3763e8d9c3084c8598ff4c6aeb6e48dad7ff61a6
Ben W. Brumfield <benwbrum@gmail.com>
2012-04-12 16:10:02 -0500

    Basic rails app framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyopicVicar::Application.config.secret_token = '940b54dd709a887760ef74dd584de3ec4c691ab1b8c5c480442687932b59e8cec26cef8f8f659d5a423c1fa78418007982577c9897dbbc0fb027deca3533b123'
