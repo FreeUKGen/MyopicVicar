0f220eb46e97c6a75c3d02e20a87e1a18b1d806e
Ben W. Brumfield <benwbrum@gmail.com>
2012-04-26 18:35:41 -0500

    Thumbnail creation now works.

--- app/models/image_file.rb
+++ app/models/image_file.rb
@@ -2,6 +2,8 @@ class ImageFile
   require 'RMagick'
   include MongoMapper::Document         
 
+  PNG_SUFFIX = '.png'
+
 # Validations :::::::::::::::::::::::::::::::::::::::::::::::::::::
 # validates_presence_of :attribute
 
@@ -16,7 +18,7 @@ class ImageFile
 # before_create :your_model_method
 # after_create :your_model_method
 # before_update :your_model_method 
-#  before_save :initialize_image
+  before_save :initialize_image
 
 # Attribute options extras ::::::::::::::::::::::::::::::::::::::::
 # attr_accessible :first_name, :last_name, :email
@@ -32,6 +34,10 @@ class ImageFile
   
    
   key :name, String
+
+  #
+  # TODO consider whether or not we actually need this
+  #
   key :path, String
   
   key :width, Integer
@@ -54,8 +60,20 @@ class ImageFile
 
   # load up the image and initialize its metadata
   def initialize_image
-    
-    
+    image= Magick::ImageList.new(self.name)    
+    self.width = image.columns
+    self.height = image.rows
+    make_thumbnail(image)    
+#    save!
+  end
+
+  
+  THUMB_HEIGHT = 120
+  def make_thumbnail(image)
+    # figure out dimensions, but make sure they're proportional
+    thumb_width = ((THUMB_HEIGHT.to_f / self.height.to_f ) * self.width).to_i
+    thumb_image = image.thumbnail(thumb_width, THUMB_HEIGHT)
+    thumb_image.write(fq(thumbnail_name))
   end
   
 
@@ -66,6 +84,24 @@ class ImageFile
   
   def thumbnail_url
     # returns a relative path to the thumbnail file
+  end
+
+  def file_directory
+    File.dirname(self.name)
+  end
+
+  def fq(filename)
+    # make sure the full path is correct
+    File.join(File.dirname(self.name), File.basename(filename))
+  end
+
+  def filename_stub
+    ext = File.extname(self.name)
+    File.basename(self.name, ext)
+  end
+
+  def thumbnail_name
+    "#{filename_stub}_thumb#{PNG_SUFFIX}"
   end
   
 end
--- spec/models/image_file_spec.rb
+++ spec/models/image_file_spec.rb
@@ -4,6 +4,7 @@ describe ImageFile do
   ZIP_FILENAME = "/home/benwbrum/dev/freeukgen/mvuploads/heterogenoustest/Flintshire 1861.zip" 
   IMAGE_FILENAME = '/home/benwbrum/dev/freeukgen/mvuploads/heterogenoustest/4143523_01206.jpg'
   PDF_FILENAME = "/home/benwbrum/dev/freeukgen/mvuploads/heterogenoustest/SSCens Tutorial_Spread_1p.pdf"
+  THUMB_FILENAME = '/home/benwbrum/dev/freeukgen/mvuploads/heterogenoustest/4143523_01206_thumb.png'
 
 
   it "should test for zipfiles" do
@@ -15,6 +16,17 @@ describe ImageFile do
   end
   it "should test for images files" do
     ImageFile.is_image?(IMAGE_FILENAME).should eq true
-    
   end
+  
+#  it "should create a thumbnail" do
+#    i = ImageFile.create(:name => IMAGE_FILENAME)
+#    File.exists?(THUMB_FILENAME).should be_true
+#  end
+  
+  it "should read height and width" do
+    i = ImageFile.create(:name => IMAGE_FILENAME)
+    i.height.should eq 3162
+    i.width.should eq 4038
+  end
+  
 end
