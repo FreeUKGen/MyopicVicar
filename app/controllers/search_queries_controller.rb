80bb74d2b3613ed08241cf33e70a2a7736a8fcfc
Ben W. Brumfield <benwbrum@gmail.com>
2012-10-22 21:41:22 -0500

    First pass at formating the search form.

--- app/controllers/search_queries_controller.rb
+++ app/controllers/search_queries_controller.rb
@@ -1,3 +1,21 @@
 class SearchQueriesController < ApplicationController
+  def index
+    redirect_to :action => :new
+  end
+
+
+  def new
+    @search_query = SearchQuery.new
+  end
+
+  def create
+    @search_query = SearchQuery.new(params[:search_query].delete_if{|k,v| v.blank? })
+
+    @search_query.save!
+    
+    render :text => @search_query.inspect
+
+  end
+
 
 end
--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -1,5 +1,21 @@
-class SearchQuery < ActiveRecord::Base
-  # attr_accessible :title, :body
+class SearchQuery
+  require 'chapman_code'
+  # consider extracting this from entities
+  RECORD_TYPES = ['Baptism', 'Marriage', 'Death']
+  ROLES = ['Father', 'Mother', 'Son', 'Daughter', 'Groom', 'Bride']
+  
+  include MongoMapper::Document
+  key :first_name, String, :required => false
+  key :last_name, String, :required => false
+  key :fuzzy, Boolean
+  key :role, String, :required => false, :in => ROLES+[nil] # I'm not sure why in and required=>false seem incompatible; the +[nil] is a work-around
+  key :record_type, String, :required => false, :in => RECORD_TYPES+[nil]
+  key :chapman_code, String, :required => false, :in => ChapmanCode::values+[nil]
+#  key :extern_ref, String
+  key :inclusive, Boolean
 
+#  key :has_thumbnails, Boolean, :default => false
+  
+#  many :assets 
   
 end
