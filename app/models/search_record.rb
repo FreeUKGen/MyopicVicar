b470a63dd51e353916b1150b1d50c7ed58c40251
Ben W. Brumfield <benwbrum@gmail.com>
2012-11-05 11:37:21 -0600

    Fixed case obliteration

--- app/models/search_record.rb
+++ app/models/search_record.rb
@@ -111,40 +111,40 @@ class SearchRecord
 
   def populate_primary_names
     # standard names
-    primary_names << { :first_name => first_name, :last_name => last_name, :source => 'transcript' } unless first_name.blank? && last_name.blank?
+    primary_names << { :first_name => String.new(first_name), :last_name => String.new(last_name), :source => 'transcript' } unless first_name.blank? && last_name.blank?
     # marriage names
-    primary_names << { :first_name => groom_first_name, :last_name => groom_last_name } unless groom_first_name.blank? && groom_last_name.blank?
-    primary_names << { :first_name => bride_first_name, :last_name => bride_last_name } unless bride_first_name.blank? && bride_last_name.blank?
+    primary_names << { :first_name => String.new(groom_first_name), :last_name => String.new(groom_last_name) } unless groom_first_name.blank? && groom_last_name.blank?
+    primary_names << { :first_name => String.new(bride_first_name), :last_name => String.new(bride_last_name) } unless bride_first_name.blank? && bride_last_name.blank?
   end
 
 
 
   def populate_inclusive_names
     # primary names
-    inclusive_names << { :first_name => first_name, :last_name => last_name }
+    inclusive_names << { :first_name => String.new(first_name), :last_name => String.new(last_name) }
     # father
     if father_first_name || father_last_name
-      inclusive_names << { :first_name => father_first_name, :last_name => father_last_name }   
+      inclusive_names << { :first_name => String.new(father_first_name), :last_name => String.new(father_last_name) }   
     end
     # mother
     if mother_first_name || mother_last_name
-      inclusive_names << { :first_name => mother_first_name, :last_name => mother_last_name }   
+      inclusive_names << { :first_name => String.new(mother_first_name), :last_name => String.new(mother_last_name) }   
     end
     # husband
     if husband_first_name || husband_last_name
-      inclusive_names << { :first_name => husband_first_name, :last_name => husband_last_name }   
+      inclusive_names << { :first_name => String.new(husband_first_name), :last_name => String.new(husband_last_name) }   
     end
     # wife
     if wife_first_name || wife_last_name
-      inclusive_names << { :first_name => wife_first_name, :last_name => wife_last_name }   
+      inclusive_names << { :first_name => String.new(wife_first_name), :last_name => String.new(wife_last_name) }   
     end
     # groom
     if groom_first_name || groom_last_name
-      inclusive_names << { :first_name => groom_first_name, :last_name => groom_last_name }   
+      inclusive_names << { :first_name => String.new(groom_first_name), :last_name => String.new(groom_last_name) }   
     end
     # bride
     if bride_first_name || bride_last_name
-      inclusive_names << { :first_name => bride_first_name, :last_name => bride_last_name }   
+      inclusive_names << { :first_name => String.new(bride_first_name), :last_name => String.new(bride_last_name) }   
     end
   end
 
--- spec/models/search_record_spec.rb
+++ spec/models/search_record_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
+require File.dirname(__FILE__) + '/sample_people'
 
 describe SearchRecord do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should keep transcribed case" do
+    person = SamplePeople::WILLIAM_FRANKLIN
+    record = SearchRecord.new(person)
+
+    # make sure we have a copy
+    fn = String.new(person[:first_name])
+    ln = String.new(person[:last_name])
+    record.save!
+    
+    record.first_name.should eq(fn)
+    record.last_name.should eq(ln)
+  end
 end
