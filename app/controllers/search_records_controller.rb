f0d47757ce430003eeaa4021738d64104d76570d
Diego Viola <diego.viola@gmail.com>
2013-02-12 16:57:38 -0300

    Keep track of viewed records

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -1,7 +1,15 @@
 class SearchRecordsController < ApplicationController
+  before_filter :visited
   def show
     @search_record = SearchRecord.find(params[:id])
     @annotations = Annotation.find(@search_record.annotation_ids)
+    session[:visited] << params[:id]
+    puts "id visited: #{session[:visited]}"
+    #puts "array size: #{session[:visited].length}"
+  end
+
+  def visited
+    session[:visited] ||= []
   end
 
   
