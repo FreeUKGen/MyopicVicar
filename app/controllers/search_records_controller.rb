5482de4d94394a11227efdcd90b9a0b581266516
Diego Viola <diego.viola@gmail.com>
2013-02-12 18:15:11 -0300

    Remove puts

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -4,7 +4,6 @@ class SearchRecordsController < ApplicationController
     @search_record = SearchRecord.find(params[:id])
     @annotations = Annotation.find(@search_record.annotation_ids)
     session[:viewed] << params[:id]
-    #puts "id viewed: #{session[:viewed]}"
   end
 
   def viewed
