b60ba16941532982b8aae6aa936f310cc235a73c
Ben W. Brumfield <benwbrum@gmail.com>
2013-03-07 16:08:20 -0600

    Fixed UI for churches

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -5,6 +5,16 @@ class ChurchesController < InheritedResources::Base
 
   end
   
+  def edit
+    load(params[:id])
+  end
+
+  def update
+    load(params[:id])
+    @church.update_attributes(params[:church])
+    @place.save    
+    redirect_to church_path(@church)
+  end
   
   def load(church_id_string)
     church_id = BSON::ObjectId(church_id_string)
