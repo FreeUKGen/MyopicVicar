6096493b127cf9fc70e154a3aa1c139bdefefff5
Diego Viola <diego.viola@gmail.com>
2013-03-28 16:38:48 -0400

    Fix models

--- app/models/church.rb
+++ app/models/church.rb
@@ -2,7 +2,7 @@ class Church
   include Mongoid::Document
   
   field :church_name
-  has_many :registers
+  embeds_many :registers
   embedded_in :place
   
   def self.find_by_name(chapman_code, church_name)
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,7 @@ class Place
   include Mongoid::Document
 
   field :chapman_code, type: String
-  has_many :churches
+  embeds_many :churches
   field :genuki_url, type: String
   field :place_name
 end
