2e745018398a4a83fd2bd54341f7c2308bd5cabb
Ben W. Brumfield <benwbrum@gmail.com>
2013-05-01 17:06:15 -0500

    Pushed config to yml file

--- lib/freereg1_translator.rb
+++ lib/freereg1_translator.rb
@@ -27,8 +27,6 @@ module Freereg1Translator
     :burial_person_forename => :first_name,
     :burial_person_surname => :last_name,
     :county => :chapman_code,
-    :father_forename => :father_first_name,
-    :father_surname => :father_last_name,
     # what should we do about the various relatives?  They should be put into family records.
     :female_relative_forename => :mother_first_name,
     # TODO refactor parentage to arrays
@@ -41,8 +39,6 @@ module Freereg1Translator
     # what should we do about the various relatives?  They should be put into family records.
     :male_relative_forename => :father_first_name,
    # :marriage_date => , #actual date as written
-    :mother_forename => :mother_first_name,
-    :mother_surname => :mother_last_name,
     :person_forename => :first_name,
   #  :register => ,
   #  :register_type => ,
@@ -76,27 +72,40 @@ module Freereg1Translator
   
   def self.expanded_attrs(entry)
     extras = []
-    role_fields_map = {
-      NameRole::GROOM_FATHER => {
-        :groom_father_forename => :first_name,
-        :groom_father_surname => :last_name
-      },
-      NameRole::BRIDE_FATHER => {
-        :bride_father_forename => :first_name,
-        :bride_father_surname => :last_name
-      }
-    }
-
-    role_fields_map.keys.each do |role_key|
-      fields_map = role_fields_map[role_key]
-      if fields_map.keys.detect { |field_key| entry[field_key] }
-        extra_name = { :role => role_key }
-        fields_map.each_pair do |original, new|
-            extra_name[new] = entry[original]
-        end    
-        extras << extra_name
-      end
+    role_fields_map = begin
+      YAML.load(File.open("#{Rails.root}/config/csv_layout.yml"))
+    rescue ArgumentError => e
+      puts "Could not parse YAML: #{e.message}"
     end
+      # consider duplicate field keys for different formats (see relative surnames above)
+    roles=role_fields_map
+    roles.each do |role|
+      role_name = role['role']
+      fields_map = role['fields']
+
+      if fields_map.values.detect { |key| key.is_a?(Array) ? entry[key[0].to_sym] : entry[key.to_sym] }
+        extra_name = { :role => role_name }
+        fields_map.each_pair do |standard, original|
+          if original.is_a? Array
+            original.detect { |o| extra_name[standard.to_sym] = entry[o.to_sym] }
+          else
+            extra_name[standard.to_sym] = entry[original.to_sym]
+          end
+        end
+        extras << extra_name
+      end  
+    end
+# 
+    # role_fields_map.keys.each do |role_key|
+      # fields_map = role_fields_map[role_key]
+      # if fields_map.keys.detect { |field_key| entry[field_key] }
+        # extra_name = { :role => role_key }
+        # fields_map.each_pair do |original, standard|
+            # extra_name[standard] = entry[original]
+        # end    
+        # extras << extra_name
+      # end
+    # end
     extras
 
   end
