031dec18824120c4b6893179bac52296595221c4
Ben W. Brumfield <benwbrum@gmail.com>
2013-05-06 11:18:31 -0500

    Retain page number on drill-down

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -1,6 +1,7 @@
 class SearchRecordsController < ApplicationController
   before_filter :viewed
   def show
+    @page_number = params[:page_number].to_i
     @search_record = SearchRecord.find(params[:id])
     @annotations = Annotation.find(@search_record.annotation_ids) if @search_record.annotation_ids
     session[:viewed] << params[:id]
