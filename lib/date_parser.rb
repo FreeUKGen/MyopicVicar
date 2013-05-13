bb27a929cd21e5b31a91b77ffba32ce970368bfd
Ben W. Brumfield <benwbrum@gmail.com>
2013-05-13 08:48:21 -0500

    Date parsing code

---
+++ lib/date_parser.rb
@@ -0,0 +1,84 @@
+
+module DateParser
+  MONTHS = {
+    'Jan' => '01',
+    'Feb' => '02',
+    'Mar' => '03',
+    'Apr' => '04',
+    'May' => '05',
+    'Jun' => '06',
+    'Jul' => '07',
+    'Aug' => '08',
+    'Sep' => '09',
+    'Oct' => '10',
+    'Nov' => '11',
+    'Dec' => '12'
+  }
+  
+  def searchable(verbatim)
+    unless verbatim.match(/\d\d\d/) #at least most of a year
+      return verbatim
+    end
+    if verbatim.match(/(\S+)\s(\S+)\s(\S+)/)  
+      d = $1    
+      vm = $2    
+      vy = $3
+    elsif verbatim.match(/\s?(\S+)\s(\S+)/)
+      d = '*'
+      vm = $1
+      vy = $2    
+    elsif verbatim.match(/(\d\d\d\d)/)
+      d = '*'
+      vm = '*'
+      vy = $1        
+    elsif verbatim.match(/(\d\d\d_)/)
+      d = '*'
+      vm = '*'
+      vy = $1
+    else
+      return verbatim
+    end  
+  
+    # handle split years
+    if vy.match(/(\d\d\d)_/)
+      vy = $1 + '5'
+    end
+    
+    # handle split years
+    if vy.match(/(\d+)\//)
+      y = $1.to_i + 1
+    else  
+      y = vy
+    end  
+  
+    # convert month names to numbers
+    if MONTHS[vm]
+      m = MONTHS[vm]
+    else
+      m = vm
+    end
+  
+    # zero-pad
+    if d.match(/\b\d\b/)
+      d = "0"+d
+    end
+    
+    "#{y}-#{m}-#{d}"  
+  end  
+  
+  def start_search_date(year)
+    return year.to_s
+  end
+  
+  def end_search_date(year)
+    # make the year inclusive
+    next_year = year + 1
+    # calculate new year
+    if next_year < 1753
+      "#{next_year}-03-25"
+    else
+      "#{next_year}-01-01"
+    end
+  end
+  
+end
