c11a20a13decaabecb7b36c98f67e895375559c6
Kirk Dawson <kirk.dawson.bc@gmail.com>
2013-06-20 16:26:41 -0600

    Error checking of Chapman Code and Place
    On edit/create of Place checked for valid Chapman Code. On create of
    Place rejected a duplicate Place within a County.
    Added index to aid check

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,24 @@
 class Place
   include Mongoid::Document
 
-  field :chapman_code, type: String
-  field :place_name
+  require 'chapman_code'
+
+   
+  field :chapman_code, type: String, :required => true
+  field :place_name, type: String, :required => true
   embeds_many :churches
   field :genuki_url, type: String
- 
+  
+  index ([[:chapman_code, Mongo::ASCENDING],[:place_name, Mongo::ASCENDING]])
+
+  validates_inclusion_of :chapman_code, :in => ChapmanCode::values+[nil]
+  validates_presence_of :place_name
+  validate :place_does_not_exist, on: :create
+
+
+  def place_does_not_exist 
+    
+      errors.add(:place_name, "already exits") if Place.where('chapman_code' => self[:chapman_code] , 'place_name' => self[:place_name]).first
+
+  end
 end
