266d141afe715ecf1abb8b7eb7090dfb76e0565d
Kirk Dawson <kirk.dawson.bc@gmail.com>
2013-09-09 08:19:47 -0600

    Extend test

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -5,7 +5,9 @@ require 'freereg_csv_processor'
 
 SAME_REGISTER_FILES = [
     "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNBA5.CSV",
-    "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNMA2.csv"
+    "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNMA2.csv",
+    "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNBA2.CSV",
+    "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNBA3.CSV"
   ]
 
 
@@ -14,18 +16,18 @@ describe Place do
     FreeregCsvProcessor::delete_all
   end
 
-  it "should create two files with one church and one place" do
+  it "should create four files with three registers, one church and one place" do
     SAME_REGISTER_FILES.each_with_index do |file, index|
       # first, load the file
       FreeregCsvProcessor.process(file)
       # then process the place
-      CreatePlacesDocs.process(40000,"rebuild")
+      #CreatePlacesDocs.process(40000,"rebuild")
 
     end
-    Freereg1CsvFile.count.should eq 2 
+    Freereg1CsvFile.count.should eq 4
     Place.count.should eq 1
     Church.count.should eq 1
-    Register.count.should eq 1
+    Register.count.should eq 3
   end
 
 
