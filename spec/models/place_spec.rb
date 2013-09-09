0194a6332cd89091332fe7e758691006292f6b61
Ben W. Brumfield <benwbrum@gmail.com>
2013-09-09 06:40:14 -0500

    Created a test case for checking registers and churches

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
+require 'create_places_docs'
+require 'freereg_csv_processor'
+
+
+SAME_REGISTER_FILES = [
+    "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNBA5.CSV",
+    "#{Rails.root}/test_data/freereg1_csvs/place_creation/SOMMSNMA2.csv"
+  ]
+
 
 describe Place do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    FreeregCsvProcessor::delete_all
+  end
+
+  it "should create two files with one church and one place" do
+    SAME_REGISTER_FILES.each_with_index do |file, index|
+      # first, load the file
+      FreeregCsvProcessor.process(file)
+      # then process the place
+      CreatePlacesDocs.process(40000,"rebuild")
+
+    end
+    Freereg1CsvFile.count.should eq 2 
+    Place.count.should eq 1
+    Church.count.should eq 1
+    Register.count.should eq 1
+  end
+
+
+
 end
