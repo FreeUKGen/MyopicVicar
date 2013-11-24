c188dd4b88820582597fb884ff323fa9cb038d45
Ben W. Brumfield <benwbrum@gmail.com>
2013-11-24 15:04:31 -0600

    Basic geocoding

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place
   include Mongoid::Document
 
   require 'chapman_code'
+	require 'nokogiri'
 
    
   field :chapman_code, type: String#, :required => true
@@ -10,6 +11,12 @@ class Place
   field :alternate_place_name, type: String
   field :place_notes, type: String
   field :genuki_url, type: String
+
+	field :pastplace_lat, type: String
+	field :pastplace_lon, type: String
+	field :geonames_lat, type: String
+	field :geonames_lon, type: String
+
   has_many :churches
   has_many :search_records
  
@@ -25,5 +32,33 @@ class Place
     
       errors.add(:place_name, "already exits") if Place.where('chapman_code' => self[:chapman_code] , 'place_name' => self[:place_name]).first
 
+  end
+
+
+	def geocode(xml_root)
+		geocode_from_pastplace(File.join(xml_root, "pastplace", self.to_xml_basename))		
+		geocode_from_geonames(File.join(xml_root, "geonames", self.to_xml_basename))		
+		save!
+		self
+  end
+
+  def geocode_from_pastplace(filename)
+		file = File.open(filename)
+		doc = Nokogiri::XML(file)
+		self[:pastplace_lat] = doc.xpath('//place/lat').text
+		self[:pastplace_lon] = doc.xpath('//place/lon').text
+		file.close
+  end
+
+	def geocode_from_geonames(filename)
+		file = File.open(filename)
+		doc = Nokogiri::XML(file)
+		self[:geonames_lat] = doc.xpath('//geoname/lat').text
+		self[:geonames_lon] = doc.xpath('//geoname/lon').text
+		file.close
+	end
+
+  def to_xml_basename
+    "#{self[:chapman_code]}_#{self[:place_name]}.xml".gsub(" ", "_")
   end
 end
