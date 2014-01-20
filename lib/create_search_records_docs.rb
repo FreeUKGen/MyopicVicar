a81fb01fb9e5a447cf3f38d7dbc8e353c2803177
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-01-20 11:48:43 -0700

    Refactored search record creation
    Process now designed for parallel processing with its own file
    selection.

--- lib/create_search_records_docs.rb
+++ lib/create_search_records_docs.rb
@@ -18,47 +18,114 @@ include Mongoid::Document
     Mongoid.load!("#{Rails.root}/config/mongoid.yml")
     
   end
+   def self.get_all_of_the_filenames(base_directory,range)
+       
+     filenames = Array.new
+     files = Array.new
+     aplha = Array.new
+     alpha_start = 1
+     alpha_end = 2
+     alpha = range.split("-")
 
-  def self.process(type_of_build,filename) 
+     if alpha[0].length == 1
+       #deal with a-c range
+       alpha_start = ALPHA.find_index(alpha[0])
+       alpha_end =  alpha_start + 1
+       alpha_end = ALPHA.find_index(alpha[1]) + 1 unless alpha.length == 1
+       index = alpha_start
+       while index < alpha_end do 
+         #get the file names for a character 
+         pattern = base_directory + ALPHA[index] + "*/*.csv" 
+         pattern_upcase = base_directory + ALPHA[index] + "*/*.csv" 
+         files = Dir.glob(pattern, File::FNM_CASEFOLD).sort 
+         files.each do |fil|
+           filenames << fil
+         end
+         index = index + 1
+       end
+     else
+      new_alpha = Array.new
+      new_alpha = range.split("/")
+      case
+        when new_alpha[0].length > 2 && new_alpha[1].length  >= 12
+           #deals with userid/abddddxy.csv ie a specific file
+           files = base_directory + range
+           filenames << files
+        when (new_alpha[0].length == 1 || new_alpha[0].length > 2) && new_alpha[1].length < 12 
+           #deals with userid/*.csv i.e. all of a usersid files or */wry*.csv
+           pattern =  base_directory + range
+           files = Dir.glob(pattern, File::FNM_CASEFOLD).sort
+          files.each do |fil|
+           filenames << fil
+         end
+      end
+    end
+   return filenames
+   @@message_file.puts "#{filenames.length}\tselected for processing\n"
+  end
+
+  def self.setup_for_new_file(filename)
+    # turn off domain checks -- some of these email accounts may no longer work and that's okay
+    #initializes variables
+    #gets information on the file to be processed
+                  
+         
+          @@file = filename
+          standalone_filename = File.basename(filename)
+          @@filename = standalone_filename
+          full_dirname = File.dirname(filename)
+          parent_dirname = File.dirname(full_dirname)
+          user_dirname = full_dirname.sub(parent_dirname, '').gsub(File::SEPARATOR, '')
+          @@userid = user_dirname
+          print "#{user_dirname} #{standalone_filename}"
+          @@message_file.puts "#{user_dirname}\t#{standalone_filename}\n"
+          
+    
+  end
+
+  def self.process(recreate,create_search_records,base_directory,range) 
     #linm is a string with the maximum number of documents to be processed
     #type of construction; if "rebuild" then we start from scrath; anyhing else we add to existing database
     #sk is a string with the number of entry documents to be skipped before we start processing the entry documents
-    standalone_filename = File.basename(filename)
-    # get the user ID represented by the containing directory
-    full_dirname = File.dirname(filename)
-    parent_dirname = File.dirname(full_dirname)
-    user_dirname = full_dirname.sub(parent_dirname, '').gsub(File::SEPARATOR, '')
-    print "Processing #{user_dirname}\t#{standalone_filename} with #{type_of_build} option\n"
-    
+        
     database = CreateSearchRecordsDocs.new
-    SearchRecord.delete_all if type_of_build == "rebuild"
-    freereg_file = Freereg1CsvFile.where(:file_name => standalone_filename, :userid => user_dirname).first
-    if freereg_file.nil? 
-      print "No such file in the database\n"
-    else
-      puts "Found file #{freereg_file[:_id] }\n"
-      entries = Freereg1CsvEntry.where(:freereg1_csv_file_id => freereg_file[:_id] ).all.no_timeout
-      if entries.nil?
-        print "File has no entries\n"
-      else
-        puts "Found #{entries.length} entries\n"
-        lc = 0 
-        lu = 0
-        entries.each do |t|
-         unless SearchRecord.where(:freereg1_csv_entry_id => t[:_id] ).exists?
-          lc = lc + 1
-          t.transform_search_record 
-         else
-              if  type_of_build == "replace" then
-                record = SearchRecord.where(:freereg1_csv_entry_id => t[:_id] ).first
-                record.delete  
-                lu = lu + 1
-                t.transform_search_record 
-              end
-          end
-        end
-        print "#{lc} search records processed or #{lu} records updated\n"
-      end
-    end
-  end   
-end
+   
+
+    file_for_warning_messages = "log/freereg_messages.log"
+     FileUtils.mkdir_p(File.dirname(file_for_warning_messages) )  unless File.exists?(file_for_warning_messages)
+     @@message_file = File.new(file_for_warning_messages, "a")
+    file_ids = Array.new
+    entries = Array.new
+
+   
+    filenames = get_all_of_the_filenames(base_directory,range)
+ time_start = Time.now  
+nn = 0
+    filenames.each do |filename|
+
+      setup_for_new_file(filename)
+      n = 0
+
+      file_ids = Freereg1CsvFile.where({:file_name => @@filename, :userid => @@userid}).all
+     
+        file_ids.each do |file_id|
+         
+          entries = Freereg1CsvEntry.where({:freereg1_csv_file_id => file_id}).all
+            
+          entries.each do |my_entry|
+           
+            SearchRecord.where({:freereg1_csv_entry_id => my_entry}).delete if recreate == "recreate"
+
+            my_entry.transform_search_record
+          n = n + 1
+          nn = nn + 1
+          end # end entries loop
+        end   #end file id loop 
+      p "#{@@filename} Created  #{n} search records\n" 
+      @@message_file.puts  "#{@@filename} Created  #{n} search records\n"   
+    end # end filename loop
+    time = (((Time.now  - time_start )/(nn-1))*1000)
+    p "Process created  #{nn} search records at an average time of #{time}ms per record\n" 
+     @@message_file.puts  "Process created  #{nn} search records at an average time of #{time}ms per record\n"  
+  end # end method
+end # end class
