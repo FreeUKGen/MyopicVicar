d436b885ae8e26322a2918349d4a9ae42a2450dc
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-02-08 21:31:52 -0700

    Add a Manage Resources controller

---
+++ app/controllers/manage_resources_controller.rb
@@ -0,0 +1,44 @@
+class ManageResourcesController < ApplicationController
+def index
+    reset_session
+   p session
+    @manage_resources = ManageResource.new  
+   
+end
+
+def new
+  @manage_resources = ManageResource.new  
+end
+
+def create
+    reset_session
+    
+    session[:initial] = "yes"
+    session[:userid] = params[:manage_resource][:userid] 
+    @user = UseridDetail.where(:userid => session[:userid]).first
+    session[:user] = @user
+    session[:first_name] = @user.person_forename
+    session[:manager] = false
+    session[:manager] = true if (session[:user].person_role == 'system_administrator' || session[:user].person_role == 'country_coordinator'  || session[:user].person_role == 'county_coordinator'  || session[:user].person_role == 'volunteer_coordinator' || session[:user].person_role == 'syndicate_coordinator')
+    p "create"
+   p session
+    redirect_to manage_resource_path(@user)
+    
+end
+
+def show
+    load(params[:id]) 
+end
+
+def load(userid_id)
+   @first_name = session[:first_name]
+   @user = UseridDetail.find(userid_id)
+  
+  end
+
+def my_syndicates
+
+  
+end
+end
+
---
+++ app/helpers/manage_resources_helper.rb
@@ -0,0 +1,2 @@
+module ManageResourcesHelper
+end
