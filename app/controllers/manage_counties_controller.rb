54a687fa6ae74c926dd5d5e3b2cd726d79b92f79
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-02-12 18:09:19 -0700

    Selection of county required for those with multiple counties

--- app/controllers/manage_counties_controller.rb
+++ app/controllers/manage_counties_controller.rb
@@ -2,49 +2,46 @@ class ManageCountiesController < ActionController::Base
 
 	 
 def index
-	p "entering index"
+
 	p session
-  redirect_to :action => :new
-end
-
-
-def new
- if session[:initial] == "yes"
- 	p  "entering  first time setup"
- 	p session
+  p  "entering  first time setup"
+  p session
   @userid = session[:userid]
   @first_name = session[:first_name]
   @user = session[:user]
-  @counties = Syndicate.where(:syndicate_coordinator => session[:userid]).all#this needs changing to counties when that collection is set up
+  @counties = County.where(:county_coordinator => session[:userid]).all
   @number_of_counties = @counties.length
   p "number of counties"
-  p @number_of_counties
+
    redirect_to manage_resource_path(@user) if @number_of_counties == 0
    @manage_county = ManageCounty.new
-   session[:manage_county] = @manage_county
+  
     if @number_of_counties == 1 
-        @counties = Syndicate.where(:syndicate_coordinator => session[:userid]).first#this needs changing to counties when that collection is set up
+        @counties = County.where(:county_coordinator => session[:userid]).first#this needs changing to counties when that collection is set up
         session[:chapman_code] = @counties.chapman_code
         @county = ChapmanCode.has_key(@counties.chapman_code)
         session[:county] = @county
         redirect_to places_path
     else
-   	   session[:muliple] = true
-   	   synd = Array.new
-   	   @counties.each do |syn|
-   	    synd << syn.syndicate_code #this needs changing to counties when that collection is set up
-   	   end
-   	   @counties = synd
+       session[:muliple] = true
+       synd = Array.new
+       @counties.each do |syn|
+        synd << syn.chapman_code #this needs changing to counties when that collection is set up
+       end
+       @counties = synd
     end #end if
-  else
-  	p "entering second time"
+end
+
+
+def new
+
+  	
   	p session
-  	@manage_county = session[:manage_county]
+  	@manage_county = ManageCounty.new
     @first_name = session[:first_name]
     @county	= session[:county]
      redirect_to places_path
-  end #end unless
-
+ 
  end #end new
 
 
@@ -52,21 +49,17 @@ def new
   p "creating"
   p session
   p params
-  if session[:initial] == "yes"
+ 
   	session[:chapman_code] = params[:manage_county][:chapman_code]
     @county = ChapmanCode.has_key(session[:chapman_code])
     session[:county] = @county
-    session[:initial] = "no"
+   p session[:chapman_code] 
     # redirect_to :action => :new
     #redirect_to coordinators_path(params)
-    redirect_to place_path
-  else
-  	p "creating second"
-  	p session
-  	p params
+    redirect_to places_path
 
-    redirect_to :action => :new
-  end #if
+  	
+  
   end # create
 
 
--- app/controllers/manage_resources_controller.rb
+++ app/controllers/manage_resources_controller.rb
@@ -3,7 +3,7 @@ class ManageResourcesController < ApplicationController
 def index
     reset_session
     @manage_resources = ManageResource.new  
-    @coordinators = County.all.distinct(:county_coordinator)
+    @coordinators = County.all.distinct(:county_coordinator).sort
 end
 
 def new
@@ -12,16 +12,12 @@ end
 
 def create
     reset_session
-    
-    session[:initial] = "yes"
     session[:userid] = params[:manage_resource][:userid] 
     @user = UseridDetail.where(:userid => session[:userid]).first
     session[:user] = @user
     session[:first_name] = @user.person_forename
     session[:manager] = false
     session[:manager] = true if (session[:user].person_role == 'system_administrator' || session[:user].person_role == 'country_coordinator'  || session[:user].person_role == 'county_coordinator'  || session[:user].person_role == 'volunteer_coordinator' || session[:user].person_role == 'syndicate_coordinator')
-    p "create"
-   p session
     redirect_to manage_resource_path(@user)
     
 end
