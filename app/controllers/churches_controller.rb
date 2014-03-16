0aa99583a0564aeed5f62678698a75b9196c88a1
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-03-15 18:01:36 -0600

    Merge churches and registers
    When the name of a church is changed to the same as an exiting church
    then this code merges the two churches and their registers.

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -15,18 +15,15 @@ class ChurchesController < InheritedResources::Base
           session[:parameters] = params
           load(params[:id])
           @names = Array.new
-         @alternate_church_names = @church.alternatechurchnames.all
+          @alternate_church_names = @church.alternatechurchnames.all
          
-         @alternate_church_names.each do |acn|
-          name = acn.alternate_name
-          @names << name
-         end
-       
-          
+            @alternate_church_names.each do |acn|
+              name = acn.alternate_name
+              @names << name
+            end
   end
 
   def new
-   
    
       @church = Church.new
       @county = session[:county]
@@ -40,9 +37,8 @@ class ChurchesController < InheritedResources::Base
       session[:errors] = nil
       @first_name = session[:first_name]
       @user = UseridDetail.where(:userid => session[:userid]).first
-
-    
   end
+
   def create
   place = Place.where(:place_name => params[:church][:place_id]).first
   church = Church.new(params[:church])
@@ -52,12 +48,12 @@ class ChurchesController < InheritedResources::Base
     flash[:notice] = 'The addition of the Church was succsessful'
    if church.errors.any?
      session[:errors] = church.errors.messages
-     flash[:notice] = 'The addition of the Curch was unsuccsessful'
+     flash[:notice] = 'The addition of the Church was unsuccsessful'
      redirect_to :action => 'new'
      return
- else
+   else
     redirect_to places_path
- end
+   end
 end
   
   def edit
@@ -78,27 +74,20 @@ end
     @church.church_name = params[:church][:church_name]
     @church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name] == ''
     @church.alternatechurchnames_attributes = params[:church][:alternatechurchnames_attributes] unless params[:church][:alternatechurchnames_attributes].nil?
-    @church.save
-     flash[:notice] = 'The update the Church was succsessful'
-   if @church.errors.any? then
-     session[:form] =  @church
-     session[:errors] = @church.errors.messages
-     flash[:notice] = 'The update of the Church was unsuccsessful'
-     render :action => 'edit'
-     return 
-  end
+  
 #update registers
     @church.registers.each do |register|
-        register.alternate_register_name = @church.church_name.to_s + " " + register.register_type.to_s
-         #update files   
+        register.alternate_register_name = params[:church][:church_name].to_s + " " + register.register_type.to_s
+        register.church_name = params[:church][:church_name]
+#update files   
     my_files = Freereg1CsvFile.where(:register_id => register._id).all
-    if my_files
+    if my_files then
       my_files.each do |myfile|
       
         myfile.church_name = params[:church][:church_name]
         myfile.save!
 
-# save place name change in Freereg_csv_entry
+# update entry
         myfile_id = myfile._id
        
         my_entries = Freereg1CsvEntry.where(:freereg1_csv_file_id => myfile_id).to_a
@@ -107,15 +96,87 @@ end
             myentries.save!
         end
       end
-    else
     end
+    # This saves registers, files and entries
         register.save!
-      end
+   end
+         @church.save
+   # we need to deal with merging of identical church and register names
+   
+        place = @church.place
+        churches = place.churches
+        churches_names = Array.new
+        
+          if churches.length >1 then
+               churches.each do |church|
+                 churches_names << church.church_name
+               end # number of churches do
+         
+               duplicate_churches = churches_names.select{|element| churches_names.count(element) > 1 }
+               duplicate_church_names = duplicate_churches.uniq
+        
+                  if duplicate_churches.length >= 1 then
+                    #have duplicate church asume there is only one duplicate
+                     duplicate_church_names.each do |duplicate_church_name|
 
+                       first_church = churches[churches_names.index(duplicate_church_name)]
+                       second_church = churches[churches_names.rindex(duplicate_church_name)]
+                       second_church_registers =  second_church.registers
+                         second_church_registers.each do |reg|
+                            first_church.registers << reg
+                         end # reg do
+              
+                 first_church.save
+                 second_church.save
 
-      
-    redirect_to :action => 'show'
-  end
+        #we now need to merge registers within the church
+            
+                  registers = first_church.registers
+                
+                  if  registers.length > 1
+                    register_names = Array.new
+                      registers.each do |register|
+                         register_names << register.alternate_register_name
+                      end #register do
+
+                    duplicate_registers = register_names.select{|element| register_names.count(element) > 1 }
+                    duplicate_register_names = duplicate_registers.uniq
+                 
+                      if duplicate_registers.length >= 1 then
+                          duplicate_register_names.each do |duplicate_register_name|
+
+                            first_register = registers[register_names.index(duplicate_register_name)]
+                            second_register = registers[register_names.rindex(duplicate_register_name)]
+                            second_register_files =  second_register.freereg1_csv_files
+                               second_register_files.each do |file|
+                                   first_register.freereg1_csv_files << file
+                               end # file do
+
+                       # first_register.save
+                           second_register.delete 
+                          end #duplicate register do
+                      end # duplicate_registers.length
+                  
+                    second_church.delete 
+
+                  end #no registers to merge
+
+               end # duplicate church name
+
+        end # no duplicate churches
+               
+      end #only one church
+  
+     flash[:notice] = 'The update the Church was succsessful'
+   if @church.errors.any? then
+     session[:form] =  @church
+     session[:errors] = @church.errors.messages
+     flash[:notice] = 'The update of the Church was unsuccsessful'
+     render :action => 'edit'
+     return 
+   end       
+    redirect_to place_path(place)
+  end # end of update
   
   def load(church_id)
     @first_name = session[:first_name]   
