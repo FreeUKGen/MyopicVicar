90e7f63ef68ccd50884bcc5a7e1d5dba33733e74
Ben W. Brumfield <benwbrum@gmail.com>
2014-05-18 21:10:37 -0500

    Changed refinery mounting to bottom

--- config/routes.rb
+++ config/routes.rb
@@ -116,7 +116,7 @@ MyopicVicar::Application.routes.draw do
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
-  mount Refinery::Core::Engine, :at => '/freereg'
+  mount Refinery::Core::Engine, :at => '/'
 
 
 
