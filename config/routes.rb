c283afd5974d9e51935e2adf11fb62207a9c77bb
Ben W. Brumfield <benwbrum@gmail.com>
2014-05-18 20:08:45 -0500

    Changed refinery mounting to bottom

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,6 @@
 # 
 MyopicVicar::Application.routes.draw do
 
-  # This line mounts Refinery's routes at the root of your application.
-  # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
-  # If you would like to change where this extension is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
-  mount Refinery::Core::Engine, :at => '/'
-
 
 
  
@@ -117,6 +110,13 @@ MyopicVicar::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  # This line mounts Refinery's routes at the root of your application.
+  # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
+  # If you would like to change where this extension is mounted, simply change the :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
+  mount Refinery::Core::Engine, :at => '/freereg'
 
 
 
