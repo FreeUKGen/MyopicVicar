03f3bb12f6f182a3d7ea4e9ceb5db3af753b26f4
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-05-24 16:43:42 -0600

    Try to force devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -180,6 +180,7 @@ Devise.setup do |config|
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
+  
   config.reset_password_within = 6.hours
 
   # ==> Configuration for :encryptable
