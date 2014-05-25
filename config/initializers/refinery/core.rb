ee5048e926ad3e3c28a96da998131e2e15165d42
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-05-25 11:28:17 -0600

    Change header

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -20,7 +20,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "FreeREG"
+  config.site_name = "Free UK Genealogy - FreeREG"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
