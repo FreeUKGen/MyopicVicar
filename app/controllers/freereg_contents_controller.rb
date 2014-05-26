376d4b05d1fa3c81f814deb4332b24f05a162ed2
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-05-26 14:26:21 -0600

    Make DAP available

--- app/controllers/freereg_contents_controller.rb
+++ app/controllers/freereg_contents_controller.rb
@@ -8,84 +8,53 @@ class FreeregContentsController < InheritedResources::Base
   end
   def new
     
-    if params[:freereg_contents_id]
-      old_query = FreeregContent.find(params[:freereg_contents_id])
-#      old_fields = old_query.attributes.delete('_id')
-#      binding.pry
-      @search_query = FreeregContent.new(old_query.attributes)
-    else
-      @search_query = FreeregContent.new  
-   
-    end
+    
   end
 
   def create
-    unless params[:freereg_content].nil? 
-    @search_query = FreeregContent.new(params[:freereg_content].delete_if{|k,v| v.blank? }) 
-    else
-    @search_query = FreeregContent.new  
-    end
-    @search_query.save!
-
-    # find the search record result
-    # redirect to search records for that search_query ID?
-        
+            
     redirect_to freereg_content_path(@search_query)
 
   end
 
   def show
-    if params[:page_number]
-      @page_number = params[:page_number].to_i
-      @search_query = FreeregContent.find(params[:id])
-      @search_results = @search_query.search.skip(@page_number*RECORDS_PER_PAGE).limit(RECORDS_PER_PAGE)
-     
-    else
-      @page_number = 0
-      @search_query = FreeregContent.find(params[:id])
-      @search_results = @search_query.search.skip(@page_number*RECORDS_PER_PAGE).limit(RECORDS_PER_PAGE)
-     
-    end
-     @county = ChapmanCode.has_key(@search_results[0].chapman_code) unless @search_results[0].nil?
-     session[:page_number] = @page_number 
+   
+     session[:county_id] = params[:id]
+     @county = ChapmanCode.values_at(params[:id]) 
      session[:county] = @county
-     session[:search_query] = @search_query
-     
-
-#    binding.pry
+     @places = Place.where(:chapman_code => @county)
+ 
   end
+
   def show_church
      @church = Church.find(params[:id])
      @county = session[:county]
-     @search_query = session[:search_query]
      @place_name = @church.place.place_name
      @place = @church.place
      @place = @place.id
      @church = @church.church_name
+     @county_id =  session[:county_id]
      session[:church] = @church
      session[:place] = @place_name
      session[:place_id] = @place
-     @page_number = session[:page_number]
+    
+    
      @registers = Register.where(:church_id => params[:id]).order_by(:record_types.asc, :register_type.asc, :start_year.asc).all
   end
 
   def show_register
      @files_id = Array.new
-     @search_query = session[:search_query]
-     @page_number = session[:page_number]
      @church  = session[:church]
      @place_name = session[:place]
      @county = session[:county]
      @place = session[:place_id]
+      @county_id =  session[:county_id]
      session[:register_id] = params[:id]
      @register = Register.find(params[:id])
-
-
     @register_name = @register.register_name 
     @register_name = @register.alternate_register_name if @register_name.nil?
-
      session[:register_name] = @register_name
-     individual_files = Freereg1CsvFile.where(:register_id =>params[:id]).order_by(:record_types.asc, :start_year.asc).all
+    individual_files = Freereg1CsvFile.where(:register_id =>params[:id]).order_by(:record_types.asc, :start_year.asc).all
      @files = Freereg1CsvFile.combine_files(individual_files)
     
 
@@ -95,33 +64,24 @@ class FreeregContentsController < InheritedResources::Base
     @files_id = session[:files]
     @register_id = session[:register_id]
      @register_name =  session[:register_name] 
-     
-    
+       @county_id =  session[:county_id]
        individual_files = Freereg1CsvFile.where(:register_id => @register_id).order_by(:record_types.asc, :start_year.asc).all
        @files = Freereg1CsvFile.combine_files(individual_files)
        puts  @files.inspect
         @files.each do |my_file|
-        puts  my_file.inspect
+       
          @record_type = RecordType.display_name(my_file.record_type)
-         puts @record_type.inspect
-         puts params[:id].inspect
+        
          if @record_type == params[:id] then
          
           @decade = my_file.daterange
-          
-
+        
          end
         
-       
-
         end
-      puts  @decade.inspect
-
+     
      @record_type = params[:id]  
-    
      @place = session[:place_id]
-     @search_query = session[:search_query]
-     @page_number = session[:page_number]
      @church  = session[:church]
      @place_name = session[:place]
      @county = session[:county]
