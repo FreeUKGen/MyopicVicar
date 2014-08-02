07090e1996903104a4bb4a3a84d569921b9eb5fa
Ben W. Brumfield <benwbrum@gmail.com>
2014-08-02 15:24:27 -0500

    Further progress on the about page

--- app/helpers/search_queries_helper.rb
+++ app/helpers/search_queries_helper.rb
@@ -1,2 +1,15 @@
+require 'text'
 module SearchQueriesHelper
+  def fuzzy(verbatim)
+    Text::Soundex.soundex(verbatim)
+  end
+
+  def format_start_date(year)
+    "January 1, "+DateParser::start_search_date(year)
+  end
+
+  def format_end_date(year)
+    Date.parse(DateParser::end_search_date(year)).strftime("%B %d, %Y")
+  end
+
 end
