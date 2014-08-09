215eac9d62168e23689d82730a370f04fc7da55e
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-08-09 09:00:39 -0600

    Correct error

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
+
 ActionMailer::Base.default_url_options[:host] = "http://xanthus.freereg2.zomo.co.uk"
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
