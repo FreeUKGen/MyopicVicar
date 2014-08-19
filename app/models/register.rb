8ad5a4545b8135d3442a74cca85fcb45514359a6
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-08-19 17:14:22 -0600

    Put some diagnostic in to detect issues in Place

--- app/models/register.rb
+++ app/models/register.rb
@@ -27,15 +27,19 @@ class Register
  
  
   def self.update_or_create_register(freereg1_csv_file)
+    p 'registering file'
+    p freereg1_csv_file
     @@result = nil
     # find if register exists
    register = find_register(freereg1_csv_file.to_register)
     if register
+      p 'updating register'
       #update register
       register.freereg1_csv_files << freereg1_csv_file
       #freereg1_csv_file.save
     else 
     # create the register  
+    p 'creating register'
      register = create_register_for_church(freereg1_csv_file.to_register, freereg1_csv_file)   
     end
     
@@ -45,36 +49,41 @@ class Register
   def self.create_register_for_church(args,freereg1_csv_file)
     # look for the church
     if @@my_church
+      p 'locating place as church exists'
       # locate place
      my_place = @@my_church.place
     else
+      p 'no church lets find if place exists'
       #church does not exist so see if Place exists with another church
-      my_place = Place.where('chapman_code' => args[:chapman_code], 'place_name' => args[:place_name]).first
+      my_place = Place.where('chapman_code' => args[:chapman_code], 'place_name' => args[:place_name],:disabled => 'false').first
       unless my_place
+      p 'no place '
        #place does not exist so lets create new place first
        my_place = Place.new(:chapman_code => args[:chapman_code], :place_name => args[:place_name], :disabled => 'false', :grid_reference => 'TQ336805') 
-           
+       p   "Place name is not approved"   
        my_place.error_flag = "Place name is not approved" 
 
        end
       #now create the church entry
+      p 'now create church entry'
       @@my_church = Church.new(:church_name => args[:church_name])
       my_place.churches << @@my_church
     end
     #now create the register
+    p 'now create register'
     register = Register.new(args) 
     register.freereg1_csv_files << freereg1_csv_file
-   
+   p register
     @@my_church.registers << register
     @@my_church.save
     #and save everything
-
+ p @@my_church
     my_place.data_present = true
       
-    my_place.save
+    my_place.save!
     #freereg1_csv_file.save
    
-   
+   p my_place
     register
   end
 
@@ -85,9 +94,11 @@ class Register
     
     @@my_church = Church.find_by_name_and_place(args[:chapman_code], args[:place_name], args[:church_name])
     if @@my_church
+      p "found church"
       my_church_id = @@my_church[:_id]
       register = Register.where(:church_id =>my_church_id, :alternate_register_name=> args[:alternate_register_name] ).first
       unless register then
+        p 'found register'
         register = Register.where(:church_id =>my_church_id, :register_name=> args[:alternate_register_name] ).first
         unless register
           register = nil
