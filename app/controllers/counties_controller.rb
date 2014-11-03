e84ee3da3671c7ffd480499c280bb29327cb94f9
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-11-03 15:41:47 -0700

    Server object to nil + but mine does not

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -123,7 +123,7 @@ def show
 	person = UseridDetail.where(:userid => @county.county_coordinator).first
     @person = person.person_forename + ' ' + person.person_surname unless person.nil? 
     person = UseridDetail.where(:userid => @county.previous_county_coordinator).first
-    @previous_person = person.person_forename + ' ' + person.person_surname unless person.nil? 
+    @previous_person = person.person_forename + ' ' + person.person_surname unless person.nil? || person.person_forename.nil?
 end
 
 def load(id)
