426483de8c4b25acfeffeb646e84d278154d3565
Kirk Dawson <kirk.dawson.bc@gmail.com>
2014-12-15 17:50:22 -0700

    Eliminate the countries and modern counties
    Both in search and DAP

--- lib/freereg_options_constants.rb
+++ lib/freereg_options_constants.rb
@@ -1,6 +1,8 @@
 module FreeregOptionsConstants
 
-CHAPMAN_CODE_ELIMINATIONS = ['England','Scotland', 'Wales','Unknown']
+CHAPMAN_CODE_ELIMINATIONS = ['England','Scotland', 'Wales','Unknown', 'Clwyd','Dyfed','Gwent','Gwynedd','Powys','Mid Glamorgan',
+    'South Glamorgan','West Glamorgan','Borders','Central','Dumfries and Galloway','Grampian','Highland','Lothian','Orkney Isles',
+    'Shetland Isles','Strathclyde','Tayside','Western Isles']
 MAXIMUM_NUMBER_OF_RESULTS = 250
 
 end
