706c1415c543149165f1fb92917144b03ab1fc69
Julian Cheal <julian.cheal@gmail.com>
2015-01-08 19:14:24 +0200

    Cleaning up configuration files. You will need to create mongoid.yml and add the settings you've been using. Created secrets.example.yml. You should create secrets.yml, but you shouldn't need to add anything unless you're deploying to production. This is a security vulnerability having the secret token visible in Github. This is actually removed in Rails 4. So this change is a stopgap until we upgrade to Rails 4. For development and production random strings are generated for the security_token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,21 +1,30 @@
 # Copyright 2012 Trustees of FreeBMD
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 # Be sure to restart your server when you modify this file.
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyopicVicar::Application.config.secret_token = '940b54dd709a887760ef74dd584de3ec4c691ab1b8c5c480442687932b59e8cec26cef8f8f659d5a423c1fa78418007982577c9897dbbc0fb027deca3533b123'
+MyopicVicar::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+	secret_token = "#{Rails.root}/config/secrets.yml"
+	if File.exists?(secret_token)
+		YAML.load_file(secret_token)[Rails.env].each do |key, value|
+      MyopicVicar::Application.config.secret_token = value
+		end
+	end
+end
