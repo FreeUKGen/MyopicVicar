a372bbdaae162c0bc0454691574ba50f74eee324
Kirk <kirk.dawson.bc@gmail.com>
2015-01-13 17:24:42 -0700

    Merge, Relocate and Rename for Churches

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -2,111 +2,178 @@ class ChurchesController < InheritedResources::Base
   rescue_from Mongoid::Errors::DeleteRestriction, :with => :record_cannot_be_deleted
   rescue_from Mongoid::Errors::Validations, :with => :record_validation_errors
 
- require 'chapman_code'
+  require 'chapman_code'
 
   def show
-   
+
     if session[:userid].nil?
       redirect_to '/', notice: "You are not authorised to use these facilities"
     end
-          @chapman_code = session[:chapman_code] 
-          @places = Place.where( :chapman_code => @chapman_code ,:disabled.ne => "true").all.order_by( place_name: 1)
-          @county = session[:county]
-          @first_name = session[:first_name]
-         
-         
-          session[:parameters] = params
-          load(params[:id])
-          @names = Array.new
-          @alternate_church_names = @church.alternatechurchnames.all
-         
-            @alternate_church_names.each do |acn|
-              name = acn.alternate_name
-              @names << name
-            end
+    @chapman_code = session[:chapman_code]
+    @places = Place.where( :chapman_code => @chapman_code ,:disabled.ne => "true").all.order_by( place_name: 1)
+    @county = session[:county]
+    @first_name = session[:first_name]
 
+
+    session[:parameters] = params
+    load(params[:id])
+    @names = Array.new
+    @alternate_church_names = @church.alternatechurchnames.all
+
+    @alternate_church_names.each do |acn|
+      name = acn.alternate_name
+      @names << name
+    end
   end
 
   def new
-      @church = Church.new
-      @county = session[:county]
-      @place = Place.where(:chapman_code => ChapmanCode.values_at(@county),:disabled.ne => "true").all.order_by( place_name: 1)
-      @places = Array.new
-          @place.each do |place|
-            @places << place.place_name
-          end
-      @place = Place.find(session[:place_id]).place_name
-      @county = session[:county]
-      @first_name = session[:first_name]
-      @user = UseridDetail.where(:userid => session[:userid]).first
+    @church = Church.new
+    @county = session[:county]
+    @place = Place.where(:chapman_code => ChapmanCode.values_at(@county),:disabled.ne => "true").all.order_by( place_name: 1)
+    @places = Array.new
+    @place.each do |place|
+      @places << place.place_name
+    end
+    @place = Place.find(session[:place_id]).place_name
+    @county = session[:county]
+    @first_name = session[:first_name]
+    @user = UseridDetail.where(:userid => session[:userid]).first
 
   end
 
   def create
-   if params[:church][:place_name].nil?
-    #Only data_manager has ability at this time to change Place so need to use the cuurent place
-    place = Place.find(session[:place_id])
-   else
-    place = Place.where(:chapman_code => ChapmanCode.values_at(session[:county]),:place_name => params[:church][:place_name]).first
-   end
-   place.churches.each do |church|
-    if church.church_name == params[:church][:church_name]
-     flash[:notice] = "A church with that name already exists in this place #{place.place_name}"
-     redirect_to new_church_path
-     return
+    if params[:church][:place_name].nil?
+      #Only data_manager has ability at this time to change Place so need to use the cuurent place
+      place = Place.find(session[:place_id])
+    else
+      place = Place.where(:chapman_code => ChapmanCode.values_at(session[:county]),:place_name => params[:church][:place_name]).first
     end
-   end
-   church = Church.new(params[:church])
-   church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name] == ''
-   place.churches << church
-   place.save
-  # church.save
-   if church.errors.any?
+    place.churches.each do |church|
+      if church.church_name == params[:church][:church_name]
+        flash[:notice] = "A church with that name already exists in this place #{place.place_name}"
+        redirect_to new_church_path
+        return
+      end
+    end
+    church = Church.new(params[:church])
+    church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name] == ''
+    place.churches << church
+    place.save
+    # church.save
+    if church.errors.any?
       flash[:notice] = 'The addition of the Church was unsuccessful'
       redirect_to new_church_path
-     return
-   else
-     flash[:notice] = 'The addition of the Church was successful'
-    redirect_to places_path
-   end
-end
-  
- def edit
-   
-          load(params[:id])
-          @chapman_code = session[:chapman_code]
-          @place = Place.where(:chapman_code => ChapmanCode.values_at(@county),:disabled.ne => "true").all.order_by( place_name: 1)
-          @places = Array.new
-          @place.each do |place|
-            @places << place.place_name
-          end
-          @county = session[:county]
-          @first_name = session[:first_name]
-          @user = UseridDetail.where(:userid => session[:userid]).first
-          #set default place name
-         # @church.update_attributes(:place_name => @place_name)
+      return
+    else
+      flash[:notice] = 'The addition of the Church was successful'
+      redirect_to places_path
+    end
+  end
+
+  def edit
+    get_user_info_from_userid
+    load(params[:id])
+    @county = session[:county]
+  end
+
+
+   def rename
+    get_user_info_from_userid
+    load(params[:id])
+    @county = session[:county]
+    @first_name = session[:first_name]
+    @user = UseridDetail.where(:userid => session[:userid]).first
+  end
+
+   def relocate
+     get_user_info_from_userid
+    load(params[:id])
+    @chapman_code = session[:chapman_code]
+    @place = Place.where(:chapman_code => ChapmanCode.values_at(@county),:disabled.ne => "true").all.order_by( place_name: 1)
+    @places = Array.new
+    @place.each do |place|
+      @places << place.place_name
+    end
+    @county = session[:county]
+    @first_name = session[:first_name]
+    @user = UseridDetail.where(:userid => session[:userid]).first
+  end
+
+  def merge
+    load(params[:id])
+      p 'merging into'
+      p @church
+      errors = @church.merge_churches
+      p @church
+      p errors
+      if errors[0]  then
+        flash[:notice] = "Church Merge unsuccessful; #{errors[1]}"
+        render :action => 'show'
+        return
+      end
+      flash[:notice] = 'The merge of the Church was successful'
+      redirect_to church_path(@church)
   end
 
   def update
     load(params[:id])
-    successful = true
-    successful = @church.change_name(params[:church][:church_name],params[:church][:place_name],) if  @church.are_we_changing_location?(params[:church])
-    @church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name] == ''
-    @church.alternatechurchnames_attributes = params[:church][:alternatechurchnames_attributes] unless params[:church][:alternatechurchnames_attributes].nil?
-    @church.update_attributes(params[:church])
-    if @church.errors.any? || !successful then
-     flash[:notice] = 'The update of the Church was unsuccessful'
-     redirect_to edit_church_path(@church)
-     return 
-    end 
-       flash[:notice] = 'The update the Church was successful' 
-        @current_page = session[:page]
-       session[:page] = session[:initial_page]    
-       redirect_to @current_page
+   
+    case
+    when params[:commit] == 'Submit' 
+     p 'editing church'
+    p params
+    p @church
+     @church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name].blank?
+     @church.alternatechurchnames_attributes = params[:church][:alternatechurchnames_attributes] unless params[:church][:alternatechurchnames_attributes].nil?
+     @church.update_attributes(params[:church])
+      p @church
+      if @church.errors.any?  then
+        flash[:notice] = 'The update of the Church was unsuccessful'
+        render :action => 'edit'
+        return
+      end
+      flash[:notice] = 'The update the Church was successful'
+      redirect_to church_path(@church)
+      return
+    when params[:commit] == 'Rename'
+      p 'renaming'
+      p @church
+      errors = @church.change_name(params[:church])
+      p @church
+      p errors
+      if errors  then
+        flash[:notice] = 'The rename of the Church was unsuccessful'
+        render :action => 'rename'
+        return
+      end
+      flash[:notice] = 'The rename the Church was successful'
+      redirect_to church_path(@church)
+      return
+     when params[:commit] == 'Relocate' 
+       p 'relocating church'
+      p @church
+       errors = @church.relocate_church(params[:church])
+      p @church
+      p errors
+      if errors[0]  then
+        flash[:notice] = "Merge unsuccessful; #{errors[1]}"
+        render :action => 'show'
+        return
+      end
+      flash[:notice] = 'The relocation of the Church was successful'
+      redirect_to church_path(@church)
+      return
+    else
+      #we should never get here but just in case
+      flash[:notice] = 'The change to the Church was unsuccessful'
+      redirect_to church_path(@church)
+      
+    end
+    
   end # end of update
-  
+
   def load(church_id)
-    @first_name = session[:first_name]   
+    @first_name = session[:first_name]
     @church = Church.find(church_id)
     session[:church_id] = @church._id
     @church_name = @church.church_name
@@ -118,26 +185,25 @@ end
     session[:place_name] =  @place_name
     @county = ChapmanCode.has_key(@place.chapman_code)
     session[:county] = @county
-     @user = UseridDetail.where(:userid => session[:userid]).first
+    @user = UseridDetail.where(:userid => session[:userid]).first
   end
 
   def destroy
     load(params[:id])
     @church.destroy
-     flash[:notice] = 'The deletion of the Church was successful'
+    flash[:notice] = 'The deletion of the Church was successful'
     redirect_to places_path
- end
+  end
 
- def record_cannot_be_deleted
-   flash[:notice] = 'The deletion of the Church was unsuccessful because there were dependant documents; please delete them first'
-  
-   redirect_to :action => 'show'
- end
+  def record_cannot_be_deleted
+    flash[:notice] = 'The deletion of the Church was unsuccessful because there were dependant documents; please delete them first'
 
- def record_validation_errors
-  flash[:notice] = 'The update of the children to Church with a church name change failed'
- 
     redirect_to :action => 'show'
- end
+  end
 
+  def record_validation_errors
+    flash[:notice] = 'The update of the children to Church with a church name change failed'
+
+    redirect_to :action => 'show'
+  end
 end
--- app/models/church.rb
+++ app/models/church.rb
@@ -2,31 +2,32 @@ class Church
   include Mongoid::Document
   include Mongoid::Timestamps::Created::Short
   include Mongoid::Timestamps::Updated::Short
-   require 'register_type' 
+  require 'register_type'
   field :church_name,type: String
   field :last_amended, type: String
   field :denomination, type: String
-  field :location, type: String
+  field :location, type: String, default: ''
   field :place_name, type: String
   field :church_notes, type: String
+  field :website, type: String, default: ''
   has_many :registers, dependent: :restrict
 
-   embeds_many :alternatechurchnames
-   accepts_nested_attributes_for :alternatechurchnames
+  embeds_many :alternatechurchnames
+  accepts_nested_attributes_for :alternatechurchnames
 
   belongs_to :place, index: true
-  index({ place_id: 1, church_name: 1 }, { unique: true })
+  index({ place_id: 1, church_name: 1 })
   validates_presence_of :church_name
   validate :church_does_not_exist, on: :create
-  
-def church_does_not_exist 
-     #errors.add(:church_name, "Church of that name already exits") unless place.church.nil?
-end 
+
+  def church_does_not_exist
+    #errors.add(:church_name, "Church of that name already exits") unless place.church.nil?
+  end
 
 
-def self.find_by_name_and_place(chapman_code, place_name,church_name)
+  def self.find_by_name_and_place(chapman_code, place_name,church_name)
     #see if church exists
-   my_place = Place.where(:chapman_code => chapman_code, :place_name => place_name,:disabled => "false").first
+    my_place = Place.where(:chapman_code => chapman_code, :place_name => place_name,:disabled => "false").first
     if my_place
       my_place_id = my_place[:_id]
       my_church = Church.where(:place_id => my_place_id, :church_name => church_name).first
@@ -34,70 +35,127 @@ def self.find_by_name_and_place(chapman_code, place_name,church_name)
       my_church = nil
     end
     return my_church
-  
-end  
 
-def are_we_changing_location?(param)
-  change = false
-  change = true unless param[:church_name] ==  self.church_name
-  change = true unless param[:place_name] == self.place.place_name
-  change
-end
-
-def change_name(church_name,place_name)
-   successful = true
-   old_church_id = self._id
-   old_place_id = self.place_id
-   chapman_code = self.place.chapman_code
-   place_name = self.place.place_name if place_name.nil?
-   param = { :church_name => church_name, :place => place_name, :county => chapman_code }
-    Church.relocate_with_no_registers(self,param) unless self.registers.exists?
-
-     self.registers.each do |register|
-       param[:register_type] = register.register_type 
-       register.relocate_with_no_files(param) unless register.freereg1_csv_files.exists?
-       register.freereg1_csv_files.each do |file|
-         new_file = Freereg1CsvFile.update_location(file,param)
-         successful = false if new_file.nil? 
-       end #file
-      end #register
-    successful 
- end
-
- def self.relocate_with_no_registers(church,param)
-   old_church = church
-   old_place = church.place
-   old_church_name = church.church_name
-   new_place = Place.where(:chapman_code => param[:county],:place_name => param[:place],:disabled => 'false').first
-   new_church = Church.where(:place_id =>  new_place._id, :church_name => param[:church_name]).first
-  if  new_church.nil?
-    new_church = Church.new(:place_id =>  new_place._id,:church_name => param[:church_name],:place_name => param[:place])  if  new_church.nil?
-    new_church.save
-    church = new_church
-  else
-    church.update_attributes(:place_id =>  new_place._id, :church_name => param[:church_name],:place_name => param[:place])
   end
-    old_place.churches.where(:church_name => old_church_name).delete_all   unless old_church.registers.exists? || !old_church.has_no_input?
-    new_place.save
-    church
-  end
-
- def has_no_input?
-  value = true
-  value = false if self.denomination.present? || self.church_notes.present? || self.location.present? 
-  value 
- end
- def data_contents
-  min = Time.new.year
-     max = 1500
-     records = 0
-       self.registers.each do |register|
+  def change_name(param)
+    p 'updating church name'
+    p self
+    p param
+    unless self.church_name == param[:church_name]
+      self.update_attributes(:church_name => param[:church_name])
+      self.registers.each do |register|
+        p 'updating register'
+        p register
+         type = register.register_type
+         register.update_attributes(:alternate_register_name =>  self.church_name.to_s + " " + type.to_s )
+          p register
         register.freereg1_csv_files.each do |file|
-          min = file.datemin.to_i if file.datemin.to_i < min
-          max = file.datemax.to_i if file.datemax.to_i > max 
-          records = records + file.records.to_i unless file.records.nil?
+          p 'updating file '
+          p file
+          file.update_attributes(:church_name => param[:church_name])
+          file.update_entries_and_search_records_for_church(self.place.place_name,param[:church_name])
+        end #file
+      end
+    end
+    if self.errors.any?
+      return true
+    end
+    return false
+  end
+
+  def merge_churches
+    p 'merging'
+    p self
+    new_church_id = self._id
+    p new_church_id
+    church_name = self.church_name
+    p church_name
+    place = self.place
+    p place
+
+    place.churches.each do |church|
+      p 'looping to check we can merge churches'
+      p church
+     
+      p church.church_name
+      if (church._id == new_church_id || church.church_name != church_name)
+        p 'bypassed'
+      else
+        p 'checking merge of this church'
+        p church
+        return [true, "a church being merged has input"] if church.has_input?
+      end
+    end
+
+    place.churches.each do |church|
+      p 'looping to merge church'
+      p church
+      p church._id
+      p church.church_name
+      if (church._id == new_church_id || church.church_name != church_name)
+        p 'bypassed'
+      else
+        p 'merge of this church'
+        p church
+        church.registers.each do |register|
+          p 'loop registers'
+          p register
+          register.update_attributes(:church_id => new_church_id)
+           p register
         end
-       end
+         place.churches.delete(church)
+      end
+     
+    end
+
+    return [false, ""]
+  end
+
+  def relocate_church(param)
+    unless param[:place_name].blank? || param[:place_name] == self.place.place_name
+      p 'relocating church'
+      p param
+      old_place = self.place
+      chapman_code = place.chapman_code
+      new_place = Place.where(:chapman_code => chapman_code, :place_name => param[:place_name]).first
+      param[:county] = old_place.chapman_code if param[:county].blank?
+      self.update_attributes(:place_id => new_place._id, :place_name => param[:place_name])
+      return [true, "Error in save of church; contact the webmaster"] if self.errors.any?
+      self.registers.each do |register|
+        p 'updating register'
+        p register
+        register.freereg1_csv_files.each do |file|
+          p 'updating place name in file '
+          p file
+          file.update_attributes(:place => param[:place_name])
+          return [true, "Error in save of file; contact the webmaster"] if file.errors.any?
+          p file
+          file.update_entries_and_search_records_for_place(new_place,self.church_name)
+        end #file
+      end
+    end
+   return [false, ""]
+  end
+
+  def has_input?
+    value = false
+    value = true if (self.denomination.present? || self.church_notes.present? || self.location.present? || self.website.present?)
+    p 'has input'
+    p value
+    value
+  end
+  def data_contents
+    min = Time.new.year
+    max = 1500
+    records = 0
+    self.registers.each do |register|
+      register.freereg1_csv_files.each do |file|
+        min = file.datemin.to_i if file.datemin.to_i < min
+        max = file.datemax.to_i if file.datemax.to_i > max
+        records = records + file.records.to_i unless file.records.nil?
+      end
+    end
     stats =[records,min,max]
-end 
+    return stats
+  end
 end
