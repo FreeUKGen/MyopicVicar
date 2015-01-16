5e681b1afed90e04ded390faf57d28762998d91b
Kirk <kirk.dawson.bc@gmail.com>
2015-01-16 10:43:54 -0700

    Change return location on create

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -66,7 +66,7 @@ class ChurchesController < InheritedResources::Base
       return
     else
       flash[:notice] = 'The addition of the Church was successful'
-      redirect_to places_path
+      redirect_to church_path(church)
     end
   end
 
@@ -77,7 +77,7 @@ class ChurchesController < InheritedResources::Base
   end
 
 
-   def rename
+  def rename
     get_user_info_from_userid
     load(params[:id])
     @county = session[:county]
@@ -85,8 +85,8 @@ class ChurchesController < InheritedResources::Base
     @user = UseridDetail.where(:userid => session[:userid]).first
   end
 
-   def relocate
-     get_user_info_from_userid
+  def relocate
+    get_user_info_from_userid
     load(params[:id])
     @chapman_code = session[:chapman_code]
     @place = Place.where(:chapman_code => ChapmanCode.values_at(@county),:disabled.ne => "true").all.order_by( place_name: 1)
@@ -101,31 +101,31 @@ class ChurchesController < InheritedResources::Base
 
   def merge
     load(params[:id])
-      p 'merging into'
-      p @church
-      errors = @church.merge_churches
-      p @church
-      p errors
-      if errors[0]  then
-        flash[:notice] = "Church Merge unsuccessful; #{errors[1]}"
-        render :action => 'show'
-        return
-      end
-      flash[:notice] = 'The merge of the Church was successful'
-      redirect_to church_path(@church)
+    p 'merging into'
+    p @church
+    errors = @church.merge_churches
+    p @church
+    p errors
+    if errors[0]  then
+      flash[:notice] = "Church Merge unsuccessful; #{errors[1]}"
+      render :action => 'show'
+      return
+    end
+    flash[:notice] = 'The merge of the Church was successful'
+    redirect_to church_path(@church)
   end
 
   def update
     load(params[:id])
-   
+
     case
-    when params[:commit] == 'Submit' 
-     p 'editing church'
-    p params
-    p @church
-     @church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name].blank?
-     @church.alternatechurchnames_attributes = params[:church][:alternatechurchnames_attributes] unless params[:church][:alternatechurchnames_attributes].nil?
-     @church.update_attributes(params[:church])
+    when params[:commit] == 'Submit'
+      p 'editing church'
+      p params
+      p @church
+      @church.alternatechurchnames_attributes = [{:alternate_name => params[:church][:alternatechurchname][:alternate_name]}] unless params[:church][:alternatechurchname][:alternate_name].blank?
+      @church.alternatechurchnames_attributes = params[:church][:alternatechurchnames_attributes] unless params[:church][:alternatechurchnames_attributes].nil?
+      @church.update_attributes(params[:church])
       p @church
       if @church.errors.any?  then
         flash[:notice] = 'The update of the Church was unsuccessful'
@@ -149,10 +149,10 @@ class ChurchesController < InheritedResources::Base
       flash[:notice] = 'The rename the Church was successful'
       redirect_to church_path(@church)
       return
-     when params[:commit] == 'Relocate' 
-       p 'relocating church'
+    when params[:commit] == 'Relocate'
+      p 'relocating church'
       p @church
-       errors = @church.relocate_church(params[:church])
+      errors = @church.relocate_church(params[:church])
       p @church
       p errors
       if errors[0]  then
@@ -167,9 +167,9 @@ class ChurchesController < InheritedResources::Base
       #we should never get here but just in case
       flash[:notice] = 'The change to the Church was unsuccessful'
       redirect_to church_path(@church)
-      
+
     end
-    
+
   end # end of update
 
   def load(church_id)
