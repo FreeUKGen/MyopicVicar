50b1f98dcf056a6fbcb7200a912cb8079431f216
Kirk <kirk.dawson.bc@gmail.com>
2015-01-17 11:13:53 -0700

    Reset data present flag
    Resets the data present flag for both present and not present

---
+++ lib/unapproved_and_no_data.rb
@@ -0,0 +1,34 @@
+class UnapprovedAndNoData
+
+
+
+  require 'chapman_code'
+  require 'church'
+  require 'register'
+  require 'freereg1_csv_file'
+  require 'freereg1_csv_entry'
+
+  require "place"
+  include Mongoid::Document
+
+  def self.process(limit)
+
+    file_for_warning_messages = "log/unapproved_and_no_data.log"
+    FileUtils.mkdir_p(File.dirname(file_for_warning_messages) )
+    message_file = File.new(file_for_warning_messages, "w")
+    limit = limit.to_i
+    puts "checking places for unapproved and no data present"
+    message_file.puts "Place Name,Chapman Code,County,Original Country,Country,Original Country"
+    record_number = 0
+    number = Place.count
+     Place.all.each do |my_entry|
+      if my_entry.error_flag == "Place name is not approved" && my_entry.data_present == 'false' 
+        record_number = record_number + 1
+        message_file.puts "\" #{my_entry.place_name}\", #{my_entry.chapman_code}, #{my_entry.county}, #{my_entry.original_county},  no location"
+       
+      end #place
+     
+    end
+     puts "There were #{record_number} unapproved places with no data in #{number} places"
+  end
+end
