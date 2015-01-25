8536fe289f52101f9d7e430ac478d6be6bd7f395
Kirk <kirk.dawson.bc@gmail.com>
2015-01-25 11:53:05 -0700

    Use different traps for development and production

--- config/initializers/development_mail_interceptor.rb
+++ config/initializers/development_mail_interceptor.rb
@@ -1,7 +1,13 @@
 class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.subject = "Trapped email #{message.to} #{message.subject}"
-    message.to = "freereg.edickens@gmail.com"
-   end
-end
 
+  def self.delivering_email(message)
+    if Rails.env.production?
+      message.subject = "Trapped email #{message.to} #{message.subject}"
+      message.to = "freereg.edickens@gmail.com"
+    end
+    if Rails.env.development?
+      message.subject = "Trapped email #{message.to} #{message.subject}"
+      message.to = "kirk.dawson.bc@gmail.com"
+    end
+  end
+end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,11 @@
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-
+if Rails.env.development?
 ActionMailer::Base.default_url_options[:host] = "http://test2.freereg.org.uk"
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
+end
+if Rails.env.production?
+  ActionMailer::Base.default_url_options[:host] = "http://freereg2.freereg.org.uk"
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
+end
