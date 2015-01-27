e38885e8d9f4ad2a0ed4fa24d1d6c2e1126784e8
Kirk <kirk.dawson.bc@gmail.com>
2015-01-27 09:01:24 -0700

    Trap issues causing BSON erros
    It appears that mongodb does not like converting flash messages into
    BSON. Also insist that either subject or body has input.

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,32 +2,34 @@ class FeedbacksController < InheritedResources::Base
   skip_before_filter :require_login
   def index
     @feedbacks = Feedback.all.order_by(feedback_time: -1).page(params[:page])
-    
+
   end
-  
+
   def new
     @feedback = Feedback.new(params)
   end
 
   def create
     @feedback = Feedback.new(params[:feedback])
+    #eliminate any flash message as the conversion to bson fails
+    session[:flash] = ""
     @feedback.session_data = session
     @feedback.save
     if @feedback.errors.any?
-       flash.notice = "There was a problem with your submission #{@feedback.errors.messages}"
-       render :action => 'new'
-       return
+      flash.notice = "There was a problem with your submission #{@feedback.errors.messages}"
+      render :action => 'new'
+      return
     end
     flash.notice = "Thank you for your feedback!"
-    redirect_to @feedback.problem_page_url    
+    redirect_to @feedback.problem_page_url
   end
 
   def delete
-     Feedback.find(params[:id]).destroy
-     flash.notice = "Feedback destroyed"
-      redirect_to :action => 'index'
-   end
-  
+    Feedback.find(params[:id]).destroy
+    flash.notice = "Feedback destroyed"
+    redirect_to :action => 'index'
+  end
+
   def convert_to_issue
     @feedback = Feedback.find(params[:id])
     @feedback.github_issue
