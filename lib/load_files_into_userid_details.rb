5c07ae8fbd4b0c2c40964ec9c47cc0ec1ff025e4
Kirk <kirk.dawson.bc@gmail.com>
2015-02-02 17:05:02 -0700

    Add load rake task

---
+++ lib/load_files_into_userid_details.rb
@@ -0,0 +1,122 @@
+class LoadFilesIntoUseridDetails
+  require 'chapman_code'
+  require 'userid_detail'
+  require 'freereg1_csv_file'
+  require 'attic_file'
+  require 'get_files'
+  require 'digest/md5'
+ 
+  FIELD_NAMES = {
+    'Surname' => :person_surname,
+    'UserID' => :userid,
+    'DisabledDate' => :disabled_date,
+    'Password' => :password,
+    'EmailID' => :email_address,
+    'Disabled' => :disabled,
+    'Active' => :active,
+    'GivenName' => :person_forename,
+    'FicheReader' => :fiche_reader,
+    'DisabledReason' => :disabled_reason,
+    'Country' => :address,
+    'SubmitterNumber' => :submitter_number,
+    'SyndicateID' => :syndicate,
+    'SyndicateName' => :syndicate_name,
+    'SignUpDate' => :sign_up_date,
+    'Person' => :person_role
+  }
+
+
+
+  def self.process(len,range)
+    file_for_warning_messages = "log/load_files_into_userid_details.log"
+    FileUtils.mkdir_p(File.dirname(file_for_warning_messages) )  unless File.exists?(file_for_warning_messages)
+    @@message_file = File.new(file_for_warning_messages, "w")
+    Mongoid.load!("#{Rails.root}/config/mongoid.yml")
+    puts "Loading files into useris_details and into attic files collection"
+    #extract range of userids
+    userids = range.split("/")
+    base_directory = Rails.application.config.datafiles
+    filenames = Hash.new
+    attic_filenames = Hash.new
+    if userids.length == 2
+      pattern = File.join(base_directory,userids[0])
+      files = Dir.glob(pattern, File::FNM_CASEFOLD).sort
+      files.each do |filename|
+        userid = filename.split("/")
+        files_for = Dir.glob(File.join(base_directory,userid[2],userids[1]), File::FNM_CASEFOLD).sort
+        filenames[userid[2]] = files_for unless userid[2].nil?
+        attic_dir = File.join(base_directory,userid[2],".attic","#{userids[1]}.*")
+        attic_files_for = Dir.glob(attic_dir, File::FNM_CASEFOLD).sort
+        attic_filenames[userid[2]] = attic_files_for unless userid[2].nil?
+      end
+    else
+      @@message_file.puts "unknown range style"
+    end
+    p "There are #{filenames.length} userid files"
+    number_missed = 0
+    number_files_missed = 0
+    missing_files = Hash.new
+    filenames.each_pair do |name,value|
+      unless UseridDetail.where(:userid => name).exists?
+        number_missed = number_missed + 1
+        @@message_file.puts "#{name} missing from userids "
+     else
+       missing_files[name] = Array.new
+       value.each do |file|
+         file_parts = file.split("/")
+         unless Freereg1CsvFile.where(:userid => name,:file_name => file_parts[-1]).exists?
+           number_files_missed = number_files_missed + 1
+            missing_files[name] << file
+         else
+            my_user = UseridDetail.where(:userid => name).first
+            my_files = Freereg1CsvFile.where(:userid => name,:file_name => file_parts[-1]).all
+            my_files.each do |my_file|
+              my_file.userid_detail = my_user
+              my_file.save
+            end
+            # 
+         end 
+       end
+     end
+  end
+ attic_filenames.each_pair do |name,value|
+      if  UseridDetail.where(:userid => name).exists?
+        my_user = UseridDetail.where(:userid => name).first
+        value.each do |file|
+          file_parts = file.split("/")
+          date = file_parts[-1].split(".")
+          date_file = DateTime.strptime(date[2],'%s') 
+          attic = AtticFile.new(:name => file_parts[-1],:date_created => date_file)
+          attic.userid_detail = my_user
+          attic.save 
+        end
+      end
+  end
+ 
+
+
+    missing_userid = Array.new
+    number = 0
+    number_processed = 0
+    number_missing = 0
+    UseridDetail.each do |userid|
+      number_processed = number_processed + 1
+      if filenames.include?(userid.userid)
+        number = number + 1
+      else
+        number_missing = number_missing + 1
+        missing_userid << userid.userid
+        @@message_file.puts "Dropped #{userid.userid} missing in the files }"
+        @@message_file.puts "#{userid.inspect}"
+      end
+    end
+
+    p "Out of #{filenames.length} user files we found #{number} loaded and #{number_missed} missing userids and had #{number_missing} userids without user files "
+    @@message_file.puts missing_userid
+    p "Missing files"
+    @@message_file.puts missing_files
+    p missing_files
+
+
+  end #end process
+end
