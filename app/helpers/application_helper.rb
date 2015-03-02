bdb50fd9d169da7f36ec67daef517b27513498d8
Julian Cheal <julian.cheal@gmail.com>
2015-03-02 17:36:03 +0000

    Added banner ad. Banner add is now added to application_helper.rb. HTML code for the ad taken directly from the FreeReg 1 Perl code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 # Copyright 2012 Trustees of FreeBMD
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 module ApplicationHelper
 
   def problem_url
@@ -39,14 +39,13 @@ module ApplicationHelper
     previous_page_url=request.env['HTTP_REFERER']
     feedback_type=Feedback::FeedbackType::ISSUE
     user_id = current_refinery_user.username
-    
+
     {  :feedback_time => problem_time,
        :session_id => session_id,
        :user_id => user_id,
        :problem_page_url => problem_page_url,
        :previous_page_url => previous_page_url,
        :feedback_type => feedback_type }
-    
   end
 
 
@@ -60,7 +59,7 @@ module ApplicationHelper
     display_map["Transcriber Syndicate"] = freereg1_csv_file.transcriber_syndicate
     display_map["Comment #1"] = freereg1_csv_file.first_comment if freereg1_csv_file.first_comment
     display_map["Comment #2"] = freereg1_csv_file.second_comment if freereg1_csv_file.second_comment
-    
+
     display_map
   end
 
@@ -72,32 +71,32 @@ module ApplicationHelper
     display_map["Last Name"] = search_query.last_name.upcase if search_query.last_name
     display_map["Exact Match?"] = "Yes" unless search_query.fuzzy
      display_map["Exact Match?"] = "No" if search_query.fuzzy
-    
+
     display_map["Record Type"] = RecordType::display_name(search_query.record_type) if search_query.record_type
 
-    display_map["Start Year"] = search_query.start_year if search_query.start_year 
+    display_map["Start Year"] = search_query.start_year if search_query.start_year
     display_map["End Year"] = search_query.end_year if search_query.end_year
 
     counties = search_query.chapman_codes.map{|code| ChapmanCode::name_from_code(code)}.join(" or ")
-    display_map["Counties"] = counties if search_query.chapman_codes.size > 1 
-    display_map["County"] = counties if search_query.chapman_codes.size == 1 
+    display_map["Counties"] = counties if search_query.chapman_codes.size > 1
+    display_map["County"] = counties if search_query.chapman_codes.size == 1
 
     if search_query.places.size > 0
       place = search_query.places.first.place_name
       if search_query.all_radius_places.size > 0
-        place << 
-          " (including #{search_query.all_radius_places.size} additional places within  
-          #{search_query.all_radius_places.last.geo_near_distance.round(1)} 
+        place <<
+          " (including #{search_query.all_radius_places.size} additional places within
+          #{search_query.all_radius_places.last.geo_near_distance.round(1)}
           #{Place::MeasurementSystem::system_to_units(Place::MeasurementSystem::SI)} )"
       end
       display_map["Place"] = place if search_query.places.size > 0
-    end    
+    end
     display_map["Include Family Members"] = "Yes" if search_query.inclusive
-    
+
     display_map
   end
 
-  
+
   def time_ago(time)
     delta_seconds = (Time.new - time).floor
     delta_minutes = (delta_seconds / 60).floor
@@ -122,5 +121,19 @@ module ApplicationHelper
       "1 second ago"
     end
   end
-  
+
+  def display_banner
+    banner = <<-HTML
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:728px;height:90px"
+         data-ad-client="ca-pub-7825403497160061"
+         data-ad-slot="3235467631"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    HTML
+    banner.html_safe
+  end
+
 end
