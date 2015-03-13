6070604c5d3391038daa0046e02583931c82cacc
Kirk <kirk.dawson.bc@gmail.com>
2015-03-13 13:55:30 -0600

    Correct logic

--- lib/create_search_records_docs.rb
+++ lib/create_search_records_docs.rb
@@ -82,7 +82,7 @@ include Mongoid::Document
            
             SearchRecord.where({:freereg1_csv_entry_id => my_entry}).delete if recreate == "recreate"
             if recreate == "augment"
-             my_entry.transform_search_record unless SearchRecord.where({:freereg1_csv_entry_id => my_entry}).exists
+             my_entry.transform_search_record unless SearchRecord.where({:freereg1_csv_entry_id => my_entry}).exists?
             else
               my_entry.transform_search_record
             end
