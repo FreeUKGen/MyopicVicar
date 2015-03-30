bd72c174700ec4a99497c21367100a3a09fb99a9
Kirk <kirk.dawson.bc@gmail.com>
2015-03-30 12:48:49 -0600

    Check the delete code

--- lib/delete_entries_records_for_removed_batches.rb
+++ lib/delete_entries_records_for_removed_batches.rb
@@ -1,20 +1,26 @@
 class DeleteEntriesRecordsForRemovedBatches
   def self.process(len,fr)
-    file_for_warning_messages = "log/delete_entries_records_for_removed_batches.log"
-    FileUtils.mkdir_p(File.dirname(file_for_warning_messages) )  unless File.exists?(file_for_warning_messages)
+    file_for_warning_messages = "log/delete_entries_records_for_removed_batches"
+    time = Time.new.to_i.to_s
+    file_for_warning_messages = (file_for_warning_messages + "." + time + ".log").to_s
     @@message_file = File.new(file_for_warning_messages, "w")
+    
     Mongoid.load!("#{Rails.root}/config/mongoid.yml")
    
     #extract range of userids
-    base_directory = Rails.application.config.datafiles if fr.to_i == 2
-    base_directory = Rails.application.config.datafiles_changeset if fr.to_i == 1
-     puts "Deleting entries and records for removed batches from the files collection at #{base_directory}"
+    base_directory = Rails.application.config.datafiles 
+    change_directory = base_directory
+    change_directory = Rails.application.config.datafiles_changeset if fr.to_i == 1
+    puts "Deleting entries and records for removed batches from the base files collection at #{base_directory} using the change set at #{change_directory}"
+    @@message_file.puts "Deleting entries and records for removed batches from the base files collection at #{base_directory} using the change set at #{change_directory}"
     len =len.to_i
     
     count = 0
     userids= UseridDetail.all.order_by(userid: 1)
+
     userids.each do |user|
       userid = user.userid
+
       unless userid.nil?
         count = count + 1
         break if count == len
@@ -22,24 +28,35 @@ class DeleteEntriesRecordsForRemovedBatches
         Freereg1CsvFile.where(userid: userid).order_by(file_name: 1).each do |name| 
           process_files << name.file_name
         end
-      
-        pattern = File.join(base_directory,userid,"*.csv")
+        
+        number_of_files = 0
+        number_of_files = process_files.length unless process_files.nil?
+       
+        pattern = File.join(change_directory,userid,"*.csv")
         files = Dir.glob(pattern, File::FNM_CASEFOLD).sort
-     
+       
         files.each do |file|
          file_parts = file.split("/")
          file_name = file_parts[-1]
        
          process_files.delete_if {|name| name = file_name}
         end
+        number_deleted = 0
         unless process_files.empty?
-        p "remove files for #{userid}" 
-        p process_files  
-         process_files.each do |my_file|
+          p "remove files for #{userid}" 
+          process_files.each do |my_file|
+          number_deleted = number_deleted + 1
+          delete_file = File.join(base_directory,userid,my_file)
+          p delete_file
+          #File.delete(delete_file) if File.exists?(delete_file)
           Freereg1CsvFile.where(userid: userid,file_name: my_file).all.each do |del_file|
-            del_file.destroy
+            #del_file.destroy
+            p del_file
           end
-         end
+        end
+         @@message_file.puts "#{userid}, #{number_of_files},#{number_deleted}"
+        else
+         @@message_file.puts "#{userid}, #{number_of_files},0"
         end
       end
     end
