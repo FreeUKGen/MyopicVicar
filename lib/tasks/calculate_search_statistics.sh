35510e3d37bae1a8afe6e7493fe0eb21b454154d
Ben W. Brumfield <benwbrum@gmail.com>
2015-04-29 21:20:50 -0500

    Script for cron job for search stats

---
+++ lib/tasks/calculate_search_statistics.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This script calculates search statistics for F2
+set -uo pipefail
+IFS=$'\n\t'
+
+trace() {
+  NOW=$( date +'%Y-%m-%d %H:%M:%S' )
+  echo "[calculate_search_statistics] ${NOW} $@" >&2
+}
+
+ROOT=/home/apache/hosts/freereg2/production
+cd $ROOT
+umask 0002
+sudo -u webserv bundle exec rake RAILS_ENV=production freereg:clean_search_queries --trace
+exit
+
+
