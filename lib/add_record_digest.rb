f394bab9a3159302e25defd995c36b71fd770ec6
Kirk <kirk.dawson.bc@gmail.com>
2015-05-25 10:18:45 -0600

    Direct calculation rather than callback

--- lib/add_record_digest.rb
+++ lib/add_record_digest.rb
@@ -29,11 +29,11 @@ class AddRecordDigest
       Freereg1CsvFile.where(:file_name => file_parts[-1], :userid => file_parts[-2]).each do |file|
        
         num = Freereg1CsvEntry.where(:freereg1_csv_file_id => file._id).count
-        p "processing #{file_parts[-2]} #{file_parts[-1]} with #{num} entries"
+        p "processing userid #{file_parts[-2]} and file #{file_parts[-1]} with #{num} entries"
         records = Freereg1CsvEntry.where(:freereg1_csv_file_id => file._id).all.no_timeout
         records.each do |my_entry| 
           record_number = record_number + 1
-          my_entry.save 
+          my_entry.update_attributes(:record_digest => my_entry.cal_digest)
           break if process_records == limit
           process_records = process_records + 1
           if process_records == 100000 then
@@ -41,12 +41,12 @@ class AddRecordDigest
             process_records = 0
           end
         end
-
+        time_end = Time.new
+        time_inc = (time_end - time_start)/record_number
+        puts "Added #{record_number} record_digest entries for #{file_parts[-1]} at #{time_inc} "
+        message_file.puts "Added #{record_number} record_digest entries for #{file_parts[-1]}"  
       end
-      time_end = Time.new
-      time_inc = (time_end - time_start)/record_number
-      puts "Added #{record_number} record_digest entries for #{file_parts[-1]} at #{time_inc} "
-      message_file.puts "Added #{record_number} record_digest entries for #{file_parts[-1]}"
+      p "Finished file"
     end
   end
 end
