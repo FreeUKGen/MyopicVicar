1b081186574f484aa833abd56467deca8614caaf
Kirk <kirk.dawson.bc@gmail.com>
2015-06-04 09:40:17 -0600

    Add a sleep config parameter
    Actual parameter must be added into mongo_config.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,5 @@ MyopicVicar::Application.configure do
   config.github_password = ENV["GITHUB_WORD"]
   config.github_repo = 'FreeUKGen/FreeUKGenProductIssues'
   config.days_to_retain_search_queries = 90
+  config.sleep = MyopicVicar::MongoConfig['sleep']
 end
