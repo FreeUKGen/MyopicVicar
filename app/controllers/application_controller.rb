cbfda610453f5d88f7d6ebe2ec2e63976cc68a96
Kirk <kirk.dawson.bc@gmail.com>
2015-08-01 14:15:44 -0600

    Change manager status process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -84,8 +84,8 @@ class ApplicationController < ActionController::Base
 
   def manager?(user)
     #sets the manager flag status
-    a = false
-    a = true if (user.person_role == 'technical' || user.person_role == 'system_administrator' || user.person_role == 'country_coordinator'  || user.person_role == 'county_coordinator'  || user.person_role == 'volunteer_coordinator' || user.person_role == 'syndicate_coordinator')
+    a = true
+    a = false if (user.person_role == 'transcriber' || user.person_role == 'researcher' || user.person_role == 'data_manager' || user.person_role == 'technical')
   end
 
 
