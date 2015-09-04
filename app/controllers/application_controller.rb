78d0206653cc6650964aea1c1126500fe47a2cfc
Kirk <kirk.dawson.bc@gmail.com>
2015-09-04 10:06:40 -0600

    Avoid filter chain issue
    Redirection worked on my server set up but not t3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
   def require_cookie_directive
     if cookies[:cookiesDirective].blank?
      flash[:notice] = 'This website only works if you are willing to accept cookies. If you did not see the cookie declaration you are likely using an obsolete browser and this website will not function'
-     redirect_to new_search_query_path
+     redirect_to main_app.new_search_query_path
     end  
   end
   
