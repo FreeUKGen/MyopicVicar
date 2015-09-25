6f85d9f83d40abddc1f2b52499da75323c353f60
Kirk <kirk.dawson.bc@gmail.com>
2015-09-24 18:46:11 -0600

    Add Date and FreeReg to record
    Also recover from failed delete and issue warning message  in log

--- app/helpers/search_records_helper.rb
+++ app/helpers/search_records_helper.rb
@@ -1,2 +1,11 @@
 module SearchRecordsHelper
+  def record_type(entry)
+    if @entry.freereg1_csv_file.present?
+      field = RecordType::display_name(@entry.freereg1_csv_file.record_type)
+    else
+      field = entry.record_type
+      logger.warn("ENTRY ERROR #{entry.id} #{entry.line_id} #{entry.place_name} #{entry.church_name} #{entry.register_type} is missing}")
+    end
+    field
+  end
 end
