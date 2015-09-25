05f49b4c1a1f36af253bca56109799bba59b134d
Kirk <kirk.dawson.bc@gmail.com>
2015-09-24 19:08:55 -0600

    Translate record type

--- app/helpers/search_records_helper.rb
+++ app/helpers/search_records_helper.rb
@@ -3,7 +3,7 @@ module SearchRecordsHelper
     if @entry.freereg1_csv_file.present?
       field = RecordType::display_name(@entry.freereg1_csv_file.record_type)
     else
-      field = entry.record_type
+      field = RecordType::display_name(entry.record_type)
       logger.warn("ENTRY ERROR #{entry.id} #{entry.line_id} #{entry.place} #{entry.church_name} #{entry.register_type} #{entry.freereg1_csv_file_id} is missing}")
     end
     field
