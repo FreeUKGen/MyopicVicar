3dae9aaa7f446fb2d899d31c240f43ca8fae2676
Kirk <kirk.dawson.bc@gmail.com>
2015-09-24 18:58:44 -0600

    Its place in entries

--- app/helpers/search_records_helper.rb
+++ app/helpers/search_records_helper.rb
@@ -4,7 +4,7 @@ module SearchRecordsHelper
       field = RecordType::display_name(@entry.freereg1_csv_file.record_type)
     else
       field = entry.record_type
-      logger.warn("ENTRY ERROR #{entry.id} #{entry.line_id} #{entry.place_name} #{entry.church_name} #{entry.register_type} is missing}")
+      logger.warn("ENTRY ERROR #{entry.id} #{entry.line_id} #{entry.place} #{entry.church_name} #{entry.register_type} #{entry.freereg1_csv_file_id} is missing}")
     end
     field
   end
