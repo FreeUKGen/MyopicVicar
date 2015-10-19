2936b436b30d94a30b25322ef261d6e1b6afed61
dougkdev <dougkdev@users.noreply.github.com>
2015-10-19 13:45:22 -0600

    FreeCEN updates for contact us

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,6 +19,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "reg-web@freereg.org.uk"
+  if MyopicVicar::Application.config.template_set == 'freecen'
+    config.mailer_sender = "cen-web@freecen.org.uk"
+  end
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
