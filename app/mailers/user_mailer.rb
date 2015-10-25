4da96b247914d0a1002077caac02f5515a433d63
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2015-10-25 16:56:48 -0600

    remove attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -17,7 +17,7 @@ class UserMailer < ActionMailer::Base
   end
 
   def batch_processing_failure(file,user,batch)
-    attachments["report.txt"] = File.read(file)
+    
     @userid = UseridDetail.where(userid: user).first
    # syndicate_coordinator = Syndicate.where(syndicate_code: @userid.syndicate).first.syndicate_coordinator
     #sc = UseridDetail.where(userid: syndicate_coordinator).first
