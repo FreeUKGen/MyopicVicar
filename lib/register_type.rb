4325f8973e02e1568302d448cd97bd240f3eb291
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2015-11-17 09:04:02 -0700

    Update approved types

--- lib/register_type.rb
+++ lib/register_type.rb
@@ -3,8 +3,12 @@ module RegisterType
 UNSPECIFIED = "Unspecified"
 
 OPTIONS = {"Parish Register" => "PR", "Transcript" => 'TR', "Archdeacon's Transcripts" => "AT", "Bishop's Transcripts" => "BT",  
-	"Phillimore's Transcripts" => "PH",  "Dwellies Transcripts" => "DW", "Extract of a Register" => "EX", 
-	"Memorial Inscription" => "MI", UNSPECIFIED => " "}
+	"Phillimore's Transcripts (Original)" => "PH",  "Dwelly's Transcripts (Original)" => "DW", "Extract of a Register" => "EX", 
+	"Memorial Inscription" => "MI", "Unspecified" => " ","Phillimore's Transcript" => "PT", "Dwelly's Transcript" => "DT",
+   "Other Transcript" => "OT", "Unknown" => "UK", "Other Register" => "OR"}
+APPROVED_OPTIONS ={ "Archdeacon's Transcripts" => "AT", "Bishop's Transcripts" => "BT",  "Dwelly's Transcript" => "DT","Extract of a Register" => "EX", 
+  "Memorial Inscription" => "MI", "Parish Register" => "PR", "Phillimore's Transcript" => "PT", "Other Register" => "OR", "Other Transcript" => "OT",
+   "Unknown" => "UK"}
 
   def self.display_name(value)
     # binding.pry
@@ -12,7 +16,18 @@ OPTIONS = {"Parish Register" => "PR", "Transcript" => 'TR', "Archdeacon's Transc
   end	
   
   def self.specified?(value) 
-    !value.blank? && !value == UNSPECIFIED
+    !value.blank? && !value == "Unspecified"
   end
-
+  def self.option_values
+    OPTIONS.values
+  end
+  def self.approved_option_values
+    APPROVED_OPTIONS.values
+  end
+  def self.option_keys
+    OPTIONS.keys
+  end
+  def self.approved_option_keys
+    APPROVED_OPTIONS.keys
+  end
 end
