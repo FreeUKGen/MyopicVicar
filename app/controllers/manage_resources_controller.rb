1f5ab798c68d1a0eda030c789a12d8b4acd36534
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2015-11-21 15:32:37 -0700

    Add mongo_config option; deny login
    Added member_open If set to true people can login. If false only SA and
    Technical people allowed to login

--- app/controllers/manage_resources_controller.rb
+++ app/controllers/manage_resources_controller.rb
@@ -36,6 +36,14 @@
        redirect_to refinery.login_path
        return
       end
+      if !Rails.application.config.member_open
+        unless @user.person_role == "system_administrator"  || @user.person_role == 'technical' 
+          current_refinery_user.delete unless current_refinery_user.nil?
+          flash[:notice] = "The system is presently undergoing maintenance and is unavailable"
+          redirect_to refinery.login_path
+          return
+        end
+      end
 
       cookies.signed[:Administrator] = Rails.application.config.github_password
      
