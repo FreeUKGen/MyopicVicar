952ec679ca5dd8f2df6c3b7905d9a8216b376e24
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2015-12-02 16:03:44 -0700

    stop tracking these

--- lib/missing_place_fields.rb
+++ lib/missing_place_fields.rb
@@ -1,39 +1,43 @@
 class MissingPlaceFields
-  file_for_warning_messages = "#{Rails.root}/log/missing_place_fields.log"
-  FileUtils.mkdir_p(File.dirname(file_for_warning_messages))
-  output_file = File.new(file_for_warning_messages, "w")
-  start = Time.now
-  output_file.puts start
-  record_number = 0
-  errors = 0
-  Place.no_timeout.each do |place|
-    record_number = record_number + 1
-    break if record_number == args.limit.to_i
-    if place.genuki_url.blank? && place.not_disabled
-      errors = errors + 1
-       output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, no genuki"  )
-    end
-    if place.county.blank? && place.not_disabled
-      errors = errors + 1
-       output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, county added"  )
-       county = ChapmanCode.name_from_code(place.chapman_code)
-       place..update_attributes(:county => county)
-    end
-    if place.chapman_code.blank? && place.not_disabled
-      errors = errors + 1
-       output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, no chapman code"  )
-    end
-    if (place.latitude.nil? && place.longitude.nil?) && place.not_disabled 
-      errors = errors + 1
-      output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, no location coordinates"  )
-    end
+  require "place"
 
+  def self.process(limit)
+    file_for_warning_messages = "#{Rails.root}/log/missing_place_fields.log"
+    FileUtils.mkdir_p(File.dirname(file_for_warning_messages))
+    output_file = File.new(file_for_warning_messages, "w")
+    start = Time.now
+    output_file.puts start
+    record_number = 0
+    errors = 0
+    Place.no_timeout.each do |place|
+      record_number = record_number + 1
+      break if record_number == limit.to_i
+      if place.genuki_url.blank? && place.disabled == 'false' 
+        errors = errors + 1
+         output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, no genuki"  )
+      end
+      if place.county.blank? && place.disabled == 'false' 
+        errors = errors + 1
+         output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, county added"  )
+         county = ChapmanCode.name_from_code(place.chapman_code)
+         place.update_attributes(:county => county)
+      end
+      if place.chapman_code.blank? && place.disabled == 'false' 
+        errors = errors + 1
+         output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, no chapman code"  )
+      end
+      if (place.latitude.nil? && place.longitude.nil?) && place.disabled == 'false'  
+        errors = errors + 1
+        output_file.puts ("#{place.id}, #{place.chapman_code}, #{place.place_name}, no location coordinates"  )
+      end
+
+    end
+    puts " #{errors} errors" 
+    output_file.puts " #{errors} errors"    
+    output_file.puts Time.now 
+    elapse = Time.now - start
+    output_file.puts elapse
+    output_file.close
+    p "finished"
   end
-  puts " #{errors} errors" 
-  output_file.puts " #{errors} errors"    
-  output_file.puts Time.now 
-  elapse = Time.now - start
-  output_file.puts elapse
-  output_file.close
-  p "finished"
 end
