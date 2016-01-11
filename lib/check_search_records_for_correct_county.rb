623998b4d01085a194f39d42a4fc066e06230120
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-01-11 15:25:56 -0700

    remove record check

--- lib/check_search_records_for_correct_county.rb
+++ lib/check_search_records_for_correct_county.rb
@@ -22,7 +22,8 @@ class CheckSearchRecordsForCorrectCounty
     files = Freereg1CsvFile.count
     p "Total files: #{files}"
     start = Time.now
-    Freereg1CsvFile.each do |my_file|
+    files = Freereg1CsvFile.all
+    files.each do |my_file|
       processing = processing + 1
       number_processed = number_processed + 1
       file_number = file_number + 1
@@ -34,11 +35,7 @@ class CheckSearchRecordsForCorrectCounty
         message_file.puts "File,#{my_file.userid}, #{my_file.file_name},#{my_file.county},#{my_file.place},#{my_file.church_name},#{my_file.register_type}, #{file_ok[1]}"
         #message_file.puts my_entry
       end
-      record_ok = my_file.check_search_record_location_and_county 
-      if !record_ok[0]
-        incorrect_records = incorrect_records + my_file.freereg1_csv_entries.count
-        message_file.puts "Record,#{my_file.userid},#{my_file.file_name},has ,#{my_file.freereg1_csv_entries.count}, #{record_ok[1]}"
-      end
+      
       if processing == 100
         processed_time = Time.now
         processing_time = (processed_time - start)*1000/number_processed 
@@ -55,17 +52,7 @@ class CheckSearchRecordsForCorrectCounty
           p "Fixed File,#{my_file.userid}, #{my_file.file_name}"
         end
       end
-      if fix == "fix" &&  !record_ok[0]
-        if file_ok[1] == "No register" || file_ok[1] == "No church" || file_ok[1] == "No place" || record_ok[1] == "No entries" || record_ok[1] == "No search record"
-          message_file.puts "Unable to fix Records,#{my_file.userid}, #{my_file.file_name}, #{file_ok[1]}, #{record_ok[1]},unable to fix"
-          p "Unable to fix Records,#{my_file.userid}, #{my_file.file_name}, #{file_ok[1]}, #{record_ok[1]}"
-        else 
-          fixed = my_file.correct_record_location_fields
-          fixed_records = fixed_records + fixed
-          message_file.puts "Fixed,#{my_file.userid}, #{my_file.file_name}, #{fixed},entries and records"
-          p "Fixed Records,#{my_file.userid}, #{my_file.file_name},#{fixed},entries and records"
-        end
-      end
+      
     end
     puts "checked #{file_number} files there were #{incorrect_files} incorrect files and #{incorrect_records} incorrect search records and #{fixed_records} were fixed"
     message_file.close 
