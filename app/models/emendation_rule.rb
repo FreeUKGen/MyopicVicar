7ab9e37261f7e0384ca2fe4c514c0ad1477a7b60
dougkdev <dougkdev@users.noreply.github.com>
2016-02-24 20:16:07 -0700

    support gender-specific emendation rules (alex=>alexander/alexandra)

--- app/models/emendation_rule.rb
+++ app/models/emendation_rule.rb
@@ -3,6 +3,7 @@ class EmendationRule
   include Mongoid::Timestamps
   field :original, type: String
   field :replacement, type: String  
+  field :gender, type: String #m=male, f=female (nil if rule applies to both)
    index({ original: 1, replacement: 1}, { unique: true })
    index({emendation_type_id: 1})
   
