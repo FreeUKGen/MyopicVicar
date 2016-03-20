5b1cb452603a7c4ce3f6fe8cda513a11b598d1bb
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-03-20 11:05:30 -0600

    Change alphabet break points

--- lib/freereg_options_constants.rb
+++ lib/freereg_options_constants.rb
@@ -38,7 +38,7 @@ module FreeregOptionsConstants
   ALPHABET = ["A-C","D-F","G-I","J-L","M-N","O-Q","R-T","U-Z"]
   ALPHABET_SELECTION_LIST = ["CAM","CON","DUR","ESS","GLA","KEN","LAN","LIN","MDX","NBL","NFK","NTH","NTT","SOM","SRY","STS","WRY"]
   ALPHABETS = [
-     ["A-Z"], ["A-L","M-Z"], ["A-I","J-O", "P-Z"], ["A-F", "G-L", "M-Q", "R-Z"],["A-D","F-I","L-O","P-Z"],["A-D","F-H","I-L","M-O","P-S","T-Z"],
+     ["A-Z"], ["A-L","M-Z"], ["A-I","J-O", "P-Z"], ["A-F", "G-L", "M-Q", "R-Z"],["A-D","E-I","J-M","N-R","P-Z"],["A-D","E-H","I-L","M-O","P-S","T-Z"],
      ["A-D","E-G","H-J","K-M","N-P","Q-S","T-Z"],["A-C","D-F","G-I","J-L","M-N","O-Q","R-T","U-Z"],
      ["A-B","C-D","E-F","G-H","I-J","K-L","M-N","O-P","Q-R","S-T","U-V","W-Z"],
      ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q-R","S","T","U-V","W-X","Y-Z"]
