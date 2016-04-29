66f652e5f3954f45e9d5a854e5cc09e21c9afb9b
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-04-29 09:32:12 -0600

    Use new processor

--- lib/tasks/scripts/update_freereg2_production.sh
+++ lib/tasks/scripts/update_freereg2_production.sh
@@ -41,14 +41,10 @@ fi
 trace "disable of searches"
 sudo /root/bin/searchctl.sh disable
 cd ${ROOT}
-trace "Get the list of files waiting to be processed"
-sudo -u webserv bundle exec rake RAILS_ENV=production build:get_waiting_file_list --trace
 trace "write the REG_users file for the image servers"
 sudo -u webserv bundle exec rake RAILS_ENV=production extract_userids_passwords_for_image_server[0] --trace
-trace "process the uploads"
-sudo -u webserv bundle exec rake RAILS_ENV=production build:freereg_update[a-9,search_records,process] --trace
-trace "delete of entries and records for removed batches"
-sudo -u webserv bundle exec rake RAILS_ENV=production build:delete_entries_records_for_removed_batches --trace
+trace "process the waiting uploads"
+sudo -u webserv bundle exec rake RAILS_ENV=production build:freereg_new_update[create_search_record,waiting,no_force,a-9] --trace
 trace "update county stats"
 sudo -u webserv bundle exec rake RAILS_ENV=production extract_county_stats --trace
 trace "delete entries and records for removed files"
