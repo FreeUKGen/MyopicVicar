5541990a1d680ef1fd55f4dab67cfc3da260c404
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-06-24 16:18:29 -0600

    New version of place table

--- app/models/manage_county.rb
+++ app/models/manage_county.rb
@@ -1,26 +1,29 @@
 class ManageCounty
+
   include Mongoid::Document
   field :chapman_code, type: String
   field :action, type: Array
   field :places, type: String
-  
-   def self.records(chapman,alphabet)
+
+  require 'freereg_options_constants'
+
+  def self.records(chapman,alphabet)
     #if alphabet is present we have already been through
     number = 0
-    if alphabet.blank? 
-      county = County.chapman_code(chapman).first    
+    if alphabet.blank?
+      county = County.chapman_code(chapman).first
       total = county.total_records unless county.nil?
       if total.present?
-        number = total.to_i 
+        number = total.to_i
         number = (number/FreeregOptionsConstants::RECORDS_PER_RANGE).to_i
         number = FreeregOptionsConstants::ALPHABETS.length - 1 if number >= FreeregOptionsConstants::ALPHABETS.length
       else
-       total = 0 
+        total = 0
       end
     else
       number = alphabet
     end
     number
   end
-  
+
 end
