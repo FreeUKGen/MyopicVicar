d7a2874ee60744a72465e2b893febcfcc34673b0
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-07-03 15:33:58 -0600

    Use different way of passing userid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,7 +65,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     cookies.signed[:Administrator] = Rails.application.config.github_issues_password
     session[:userid_detail_id] = current_authentication_devise_user.userid_detail_id
-    logger.warn "FREREG::USER current  #{current_authentication_devise_user.userid_detail_id}"
+    @@userid = current_authentication_devise_user.userid_detail_id
+    logger.warn "FREEREG::USER current  #{current_authentication_devise_user.userid_detail_id}"
     #logger.warn("APP: current_refinery_user #{current_refinery_user}")
     #logger.warn("APP: current_refinery_user.userid_detail #{current_refinery_user.userid_detail.id}") unless current_refinery_user.nil? || current_refinery_user.userid_detail.nil?
     scope = Devise::Mapping.find_scope!(resource_or_scope)
@@ -81,16 +82,17 @@ class ApplicationController < ActionController::Base
   def get_userid_from_current_authentication_devise_user
     if session[:userid_detail_id].present?
       @user = UseridDetail.id(session[:userid_detail_id]).first
-      logger.warn "FREREG::USER user #{@user.userid}"
+      logger.warn "FREEREG::USER user #{@user.userid}"
     else
-      if current_authentication_devise_user.blank?
+      logger.warn "FREEREG::USER No session "
+      if @@userid.blank?
+        logger.warn "FREEREG::USER No session and no @@userid"
         flash[:notice] = 'You are not logged into the system'
-        redirect_to refinery.login_path
+        redirect_to refinery.logout_path
         return
       else
-        logger.warn "FREREG::USER current  #{current_authentication_devise_user.userid_detail_id}"
-        @user = UseridDetail.find(current_authentication_devise_user.userid_detail_id)
-        logger.warn "FREREG::USER user #{@user.userid}"
+        @user = UseridDetail.find(@@userid)
+        logger.warn "FREEREG::USER user #{@user.userid}"
       end
     end
     @user_id = @user._id
@@ -98,13 +100,12 @@ class ApplicationController < ActionController::Base
     @first_name = @user.person_forename
     @manager = manager?(@user)
     @roles = UseridRole::OPTIONS.fetch(@user.person_role)
-    u = Refinery::Authentication::Devise::User.where(:username => @user.userid).first
     session[:userid] = @userid
     session[:user_id] = @user_id
     session[:first_name] = @first_name
     session[:manager] = manager?(@user)
     session[:role] = @user.person_role
-    logger.warn "FREREG::USER  manager #{@manager}"
+    logger.warn "FREEREG::USER  manager #{@manager}"
 
   end
 
