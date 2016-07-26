771c46ac5bc43d14d81ce2ad21c297a6cb1d5c44
Kirk Dawson <kirk.dawson.bc@gmail.com>
2016-07-26 08:51:16 -0600

    Correct  mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,15 +1,19 @@
-
-ActionMailer::Base.delivery_method = :smtp
-# SMTP settings for gmail
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :user_name            => ENV['gmail_username'],
-  :password             => ENV['gmail_password'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
-
+if Rails.env.development? && Rails.application.config.mongodb_bin_location == 'd:/mongodb/bin/'
+  ActionMailer::Base.delivery_method = :smtp
+  # SMTP settings for gmail
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+else
+  ActionMailer::Base.delivery_method = :sendmail
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+end
 ActionMailer::Base.default_url_options[:host] = Rails.application.config.website
 if Rails.env.development?
   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
