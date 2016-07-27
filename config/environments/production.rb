4d34add205b2bc595a537d83c033b7ce1476e22c
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-07-27 16:57:39 -0600

    turn off rails server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ MyopicVicar::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = true
+  config.serve_static_files = false
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
@@ -59,8 +59,8 @@ MyopicVicar::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+  #config.assets.precompile += %w( search.js )
+
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
     # config.action_mailer.raise_delivery_errors = false
