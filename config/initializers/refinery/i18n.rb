1acb6a81eda5d836601e1929d76d5d8582b44992
Ben W. Brumfield <benwbrum@gmail.com>
2016-08-09 13:01:13 -0500

    Updated refinery initializers; added 'style' and 'scoped' to the element and attribute whitelists

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -9,5 +9,5 @@ Refinery::I18n.configure do |config|
 
   # config.frontend_locales = [:en]
 
-  # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
+  # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :ca=>"Català", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
 end
--- config/initializers/refinery/pages.rb
+++ config/initializers/refinery/pages.rb
@@ -6,13 +6,17 @@ Refinery::Pages.configure do |config|
   # end
 
   # Configure global page default parts
-  # config.default_parts = ["Body", "Side Body"]
+  # config.default_parts = [{:title=>"Body", :slug=>"body"}, {:title=>"Side Body", :slug=>"side_body"}]
 
   # Configure whether to allow adding new page parts
   # config.new_page_parts = false
 
   # Configure whether to enable marketable_urls
   # config.marketable_urls = true
+
+  # You can specify reserved words that won't be used as page slugs.
+  # This only applies when marketable_urls is enabled.
+  # config.friendly_id_reserved_words = ["index", "new", "session", "login", "logout", "users", "refinery", "admin", "images", "wymiframe"]
 
   # Configure how many pages per page should be displayed when a dialog is presented that contains a links to pages
   # config.pages_per_dialog = 14
@@ -36,10 +40,9 @@ Refinery::Pages.configure do |config|
   # and will no longer be nested as children.
   # config.scope_slug_by_parent = true
 
-  # Set this to true if you want backend pages to be cached
-  # config.cache_pages_backend = false
-
-  # Set this to true to activate full-page-cache
+  # Set this to true to activate full-page-cache.
+  # If you use Rails 4.0+ and set config.cache_pages_full to true, you have to add this gem in your Gemfile:
+  # gem 'actionpack-page_caching'
   # config.cache_pages_full = false
 
   # Set this to true to fully expand the page hierarchy in the admin
@@ -49,9 +52,29 @@ Refinery::Pages.configure do |config|
 
   # config.use_layout_templates = false
 
+  # Use these if you want to use layout or view templates whose filenames do not match the default pattern
+  # For more information on the patterns see Dir.glob (http://ruby-doc.org/core-2.2.0/Dir.html#method-c-glob)
+  # example: for .erb, .html and .rb view templates use
+  #         config.view_templates_pattern = 'app', 'views', '{pages,refinery/pages}', '*.{erb,html,rb}'
+
+  # config.layout_templates_pattern = 'app', 'views', '{layouts,refinery/layouts}', '*html*'
+  # config.view_templates_pattern = 'app', 'views', '{pages,refinery/pages}', '*html*'
+
   # config.page_title = {:chain_page_title=>false, :ancestors=>{:separator=>" | ", :class=>"ancestors", :tag=>"span"}, :page_title=>{:class=>nil, :tag=>nil, :wrap_if_not_chained=>false}}
 
   # config.absolute_page_links = false
 
   # config.show_title_in_body = true
+
+  # You can add new HTML elements not already supported by Loofah::HTML5::WhiteList::ALLOWED_ELEMENTS
+  # For more information on whitelist see ALLOWED_ELEMENTS 
+  # (https://github.com/flavorjones/loofah/blob/v2.0.3/lib/loofah/html5/whitelist.rb#L151)
+  # config.add_whitelist_elements = ["source", "track"]
+  config.add_whitelist_elements = ["style"]
+  
+  # You can add new HTML attributes not already supported by Loofah::HTML5::WhiteList::ALLOWED_ATTRIBUTES
+  # For more information on whitelist see ALLOWED_ATTRIBUTES 
+  # (https://github.com/flavorjones/loofah/blob/v2.0.3/lib/loofah/html5/whitelist.rb#L152)
+  # config.add_whitelist_attributes = ["kind", "srclang", "placeholder", "controls"]
+  config.add_whitelist_attributes = ["scoped"]
 end
