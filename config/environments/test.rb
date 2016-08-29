0da818be22ab38a638b55e13ebc1136fb67937a8
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2016-08-29 09:56:22 -0600

    Use specific emendation sleep

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -62,6 +62,7 @@ MyopicVicar::Application.configure do
   config.github_repo = 'FreeUKGen/FreeUKRegProductIssues'
   config.days_to_retain_search_queries = 90
   config.sleep = MyopicVicar::MongoConfig['sleep']
+  config.emmendation_sleep = MyopicVicar::MongoConfig['emmendation_sleep']
   config.processing_delta = MyopicVicar::MongoConfig['files_for_processing'] unless MyopicVicar::MongoConfig['files_for_processing'].blank?
   config.delete_list = MyopicVicar::MongoConfig['delete_list']
   config.member_open = MyopicVicar::MongoConfig['member_open']
--- lib/update_search_records.rb
+++ lib/update_search_records.rb
@@ -42,7 +42,7 @@ class UpdateSearchRecords
           if file.freereg1_csv_entries.count > 1
             Freereg1CsvEntry.where(:freereg1_csv_file_id => file.id).all.no_timeout.each do |entry|
               result = SearchRecord.update_create_search_record(entry,search_version,place.id)
-              sleep(Rails.application.config.sleep.to_f) if result == "updated" ||  result == "created"
+              sleep(Rails.application.config.emmendation_sleep.to_f) if result == "updated" ||  result == "created"
               updated_records = updated_records + 1 if result == "updated"
               file_records_update = file_records_update + 1 if result == "updated"
               file_records_not_updated = file_records_not_updated + 1 if result == "no update"
@@ -54,7 +54,7 @@ class UpdateSearchRecords
           message_file.puts "#{file.userid}/#{file.file_name}/#{recs}/#{file_records_update}/#{file_records_not_updated} and #{n} files processed (#{files_bypassed} files bypassed) with #{records}  records total, #{updated_records} updated and #{no_update} unchanged todate"
           p "#{file.userid}/#{file.file_name}/#{recs}/#{file_records_update}/#{file_records_not_updated} and #{n} files processed (#{files_bypassed} files bypassed) with #{records} records total, #{updated_records} updated and #{no_update} unchanged todate"
           file.update_attributes(:software_version => version, :search_record_version => search_version)
-          sleep(20*(Rails.application.config.sleep.to_f))
+          sleep(20*(Rails.application.config.emmendation_sleep.to_f))
         else
           message_file.puts "No entries"
         end
