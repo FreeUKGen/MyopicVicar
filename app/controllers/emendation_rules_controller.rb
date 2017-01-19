08a7628ffb5c6835bc9f6b592699e78185cd0a52
Israel Borges <israelborges@zertico.com>
2017-01-18 22:52:48 -0200

    Add new line and remove skip require login

--- app/controllers/emendation_rules_controller.rb
+++ app/controllers/emendation_rules_controller.rb
@@ -1,5 +1,5 @@
 class EmendationRulesController < ApplicationController
-  skip_before_filter :require_login
+
   def index
     @emendation_rules = EmendationRule.sort_by_initial_letter(EmendationRule.distinct('replacement'))
     @alphabet_keys = @emendation_rules.keys
