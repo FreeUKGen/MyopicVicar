effe55a04fe6b57df4d1023d72759946264241ab
Captainkirkdawson <kirk.dawson.bc@gmail.com>
2017-01-24 16:56:26 -0700

    Add place cache detection and refresh

--- app/models/place_cache.rb
+++ app/models/place_cache.rb
@@ -3,15 +3,15 @@ class PlaceCache
   field :chapman_code, type: String
   field :places_json, type: String
 
+  index({chapman_code: 1},{name: "chapman_code"})
+
   def self.refresh(county)
     PlaceCache.where(:chapman_code => county).destroy_all
     # the js library expects a certain format
     county_response = {"" => []}
-    places = Place.chapman_code(county).not_disabled.all.order_by( place_name: 1)
-    places.no_timeout.each do |place|
-      if place.churches.exists? && place.search_records.exists?
-        county_response[place.id] = "#{place.place_name} (#{ChapmanCode::name_from_code(place.chapman_code)})"
-      end
+    places = Place.chapman_code(county).data_present.not_disabled.all.order_by( place_name: 1)
+    places.each do |place|
+      county_response[place.id] = "#{place.place_name} (#{ChapmanCode::name_from_code(place.chapman_code)})"
     end
     cache = PlaceCache.new
     cache.update_attributes({ :chapman_code => county, :places_json => county_response.to_json})
@@ -31,6 +31,15 @@ class PlaceCache
   def self.refresh_cache(place)
     cache = PlaceCache.where(:chapman_code => place.chapman_code).first
     PlaceCache.refresh(place.chapman_code) if cache.blank? || !cache.places_json.include?(place.place_name)
+  end
+
+  def self.check_and_refresh_if_absent
+    ChapmanCode::values.each do |chapman_code|
+      if Place.chapman_code(chapman_code).data_present.not_disabled.present? && PlaceCache.where(:chapman_code => chapman_code).first.places_json.length <= 7
+        refresh(chapman_code)
+      end
+    end
+
   end
 
 end
