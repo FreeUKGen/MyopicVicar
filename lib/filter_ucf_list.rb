81da8baac96f4e90e8f0a223b8f1a77f39fdd2ee
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2017-03-09 15:47:14 +0000

    Added more validations

--- lib/filter_ucf_list.rb
+++ lib/filter_ucf_list.rb
@@ -1,13 +1,15 @@
-class RefreshUcfList
+class FilterUcfList
   attr_accessor :model_name, :output_directory
 
-  def initialize model_name, output_directory
+  def initialize(model_name, output_directory = nil)
     @model_name = model_name
     @output_directory = output_directory
   end
 
+  # Rules to filter the special characters
   SPECIAL_CHARACTER_LISTS = /[?{}\[\]]/
 
+  # Print the special character ID's into the output directory
   def filter_id
     retrieve_name_columns.each do |name|
       original_stdout = STDOUT.clone
@@ -24,6 +26,8 @@ class RefreshUcfList
 
   # Fetch all the column attribute names from the table
   def fetch_columns
+    raise "Model name can't be blank" if @model_name.nil?
+
     @model_name.attribute_names
   end
 
@@ -37,14 +41,28 @@ class RefreshUcfList
     @model_name.where(column_name.to_sym => SPECIAL_CHARACTER_LISTS)
   end
 
+  # Validate the directory exists or not
   def valid_directory?
-    File.directory?(@output_directory)
+    File.directory?(output_directory_path)
   end
 
   # Create a new file named as current date and time
   def new_file name
     raise "Not a Valid Directory" unless valid_directory?
+
     file_name = "#{Time.now.strftime("%Y%m%d%H%M%S")}_#{name}.txt"
-    "#{@output_directory}/#{file_name}"
+    "#{output_directory_path}#{file_name}"
+  end
+
+  # Set an output directory
+  # If there is no ouput directory, then set the default
+  # else check the trailing slash at the end of the directory
+  def output_directory_path
+    if @output_directory.nil?
+      directory = Rails.root.to_s + "/script/"
+    else
+      directory = File.join(@output_directory, "")
+    end
+    directory
   end
 end
--- lib/tasks/find_bad_ucf.rake
+++ lib/tasks/find_bad_ucf.rake
@@ -2,15 +2,17 @@ namespace :freereg do
 
   desc "Rebuild UCF and deploy"
   task :find_bad_ucf => [:environment] do
-    #require '#{Rails.root}/lib/refresh_ucf_list'
-    require 'refresh_ucf_list'
+    require 'filter_ucf_list'
+
+    output_directory = "C:/Work/FreeUKGen/refresh_ucf"
+    model_name = Freereg1CsvEntry
+
     # Print the time before start the process
     start_time = Time.now
     p "Starting at #{start_time}"
 
     # Call the RefreshUcfList library class file with passing the model name as parameter
-    entries = RefreshUcfList.new(Freereg1CsvEntry, "C:/Work/FreeUKGen/MyopicVicar/scrpt/refresh_ucf")
-    # call the filter_id method to pipe the id's into the file which has special characters
+    entries = FilterUcfList.new(model_name, output_directory)
     entries.filter_id
 
     p "Process finished"
