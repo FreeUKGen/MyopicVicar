e8cfb9c6cabb7edc947412a4bfc1883c92e647c0
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2017-03-16 13:46:27 +0000

    Added VinodhiniS to create_userid_docs

--- config/initializers/development_mail_interceptor.rb
+++
@@ -1,7 +0,0 @@
-class DevelopmentMailInterceptor
-
-  def self.delivering_email(message)
-      message.subject = "Trapped email #{message.to} #{message.subject}"
-      message.to = "kirk.dawson.bc@gmail.com"
-  end
-end
