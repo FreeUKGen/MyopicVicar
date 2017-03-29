82be84aee7aba0d01884598b52df7766e3c5958f
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2017-03-29 22:32:23 +0100

    Added country_coordinator to view incompleted registrations list

--- spec/models/userid_detail_spec.rb
+++ spec/models/userid_detail_spec.rb
@@ -4,28 +4,41 @@ describe UseridDetail do
   subject { described_class.new() }  
 
   describe '#list_incomplete_registrations' do
-    let (:current_user) {UseridDetail.where(userid: "VinodhiniS").first}
+    current_user = UseridDetail.where(person_role: "system_administrator").first
+    current_syndicate = "all"
 
     it "is a Array" do
-      expect(subject.list_incomplete_registrations(current_user)).to be_a Array
-    end
-
-    it "gives count as 103" do
-      expect(subject.list_incomplete_registrations(current_user).count).to eq 103
+      expect(subject.list_incomplete_registrations(current_user,current_syndicate)).to be_a Array
     end
   end
 
   describe '#syndicate_incomplete_registrations' do
 
-    current_user_syndicate = UseridDetail.where(userid: "Jen").first
+    current_user_syndicate = UseridDetail.where(person_role: "syndicate_coordinator").first
     current_syndicate = current_user_syndicate.syndicate
 
     it "is a Array" do
-      expect(subject.syndicate_incomplete_registrations(current_user_syndicate, current_syndicate)).to be_a Array
+      expect(subject.list_incomplete_registrations(current_user_syndicate, current_syndicate)).to be_a Array
     end
+  end
 
-    it "gives count as 9" do
-      expect(subject.syndicate_incomplete_registrations(current_user_syndicate, current_syndicate).count).to eq 9
+  describe '#syndicate_incomplete_registrations' do
+
+    current_user_syndicate = UseridDetail.where(person_role: "country_coordinator").first
+    current_syndicate = current_user_syndicate.syndicate
+
+    it "is a Array" do
+      expect(subject.list_incomplete_registrations(current_user_syndicate, current_syndicate)).to be_a Array
+    end
+  end
+
+  describe '#syndicate_incomplete_registrations' do
+
+    current_user_syndicate = UseridDetail.where(person_role: "county_coordinator").first
+    current_syndicate = current_user_syndicate.syndicate
+
+    it "is a Array" do
+      expect(subject.list_incomplete_registrations(current_user_syndicate, current_syndicate)).to be_a Array
     end
   end
 end
