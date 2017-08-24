88f2b533b99ecd7154758be30aaad6c8dc86bc79
bev-fc <bev.greg001@gmail.com>
2017-08-24 10:12:13 -0400

    correct error

--- app/controllers/image_server_groups_controller.rb
+++ app/controllers/image_server_groups_controller.rb
@@ -81,10 +81,10 @@ class ImageServerGroupsController < ApplicationController
     @user = cookies.signed[:userid]
     @first_name = @user.person_forename unless @user.blank?
 
-    case
-      when ['system_administrator', 'country_coordinator', 'data_manager'].include? @user.person_role
+    case @user.person_role
+      when 'system_administrator', 'country_coordinator', 'data_manager'
         @userids = UseridDetail.where(:active=>true).order_by(userid_lower_case: 1)
-      when  @user.person_role == 'county_coordinator'
+      when 'county_coordinator'
         @userids = UseridDetail.where(:syndicate => @user.syndicate, :active=>true).all.order_by(userid_lower_case: 1) # need to add ability for more than one county
       else
         flash[:notice] = 'Your account does not support this action'
