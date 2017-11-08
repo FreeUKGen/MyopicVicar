84cbe2ecfd2e35e6fa96d1e6563db059e810d285
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2017-11-08 13:49:06 +0000

    Changed remote_id to remote_ip in spam_check

--- app/controllers/userid_details_controller.rb
+++ app/controllers/userid_details_controller.rb
@@ -495,7 +495,7 @@ class UseridDetailsController < ApplicationController
       error_text = error_text+"REMOE ADDR: "+request.remote_addr + "\r\n" if request.present? && request.remote_addr.present?
       error_text = error_text+"REMOE ADDR: Unknown \r\n" unless request.present? && request.remote_addr.present?
       error_text = error_text+"REMOTE IP: "+request.remote_ip + "\r\n" if request.present? && request.remote_ip.present?
-      error_text = error_text+"REMOE IP: Unknown \r\n" unless request.present? && request.remote_id.present?
+      error_text = error_text+"REMOE IP: Unknown \r\n" unless request.present? && request.remote_ip.present?
       error_text = error_text+"REMOTE HOST: "+request.remote_host+"\r\n\r\n\r\n" if request.present? && request.remote_host.present?
       error_text = error_text+"REMOE HOST: Unknown \r\n" unless request.present? && request.remote_host.present?
       f.puts error_text
