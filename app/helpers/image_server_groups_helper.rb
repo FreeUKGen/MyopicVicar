fdcf39b21e93e090390a5399f42469cbb274713d
Captain Kirk <kirk.dawson.bc@gmail.com>
2018-01-02 01:30:30 +0000

    suppress send email to cc message for CC and use different technique

---
+++ app/helpers/image_server_groups_helper.rb
@@ -0,0 +1,13 @@
+module ImageServerGroupsHelper
+  def do_we_offer_mail_to_cc(group) 
+    send_email = false   
+    if !session[:county].present?
+      if  !group.summary.nil? 
+        if !group.nil? && !group.summary[:status].nil? && (group.summary[:status] - ['u','a','bt','t','br','rs']).empty? == false
+           send_email = true
+        end 
+      end 
+    end
+    send_email
+  end
+end
