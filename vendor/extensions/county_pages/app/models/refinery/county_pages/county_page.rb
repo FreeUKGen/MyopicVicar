a34aeb620f10bd5c3a670dfcef157f023a5dacd8
Captain Kirk <kirk.dawson.bc@gmail.com>
2018-03-28 22:08:31 +0000

    county pages update

--- vendor/extensions/county_pages/app/models/refinery/county_pages/county_page.rb
+++ vendor/extensions/county_pages/app/models/refinery/county_pages/county_page.rb
@@ -3,7 +3,7 @@ module Refinery
     class CountyPage < Refinery::Core::BaseModel
       self.table_name = 'refinery_county_pages'
 
-      attr_accessible :name, :chapman_code, :content, :position, :position
+      attr_accessor :name, :chapman_code, :content, :position, :position
 
       validates :name, :presence => true, :uniqueness => true
     end
