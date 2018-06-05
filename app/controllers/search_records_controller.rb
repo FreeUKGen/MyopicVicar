b705c01aa3720848c71849f70cfd0510ad416ebd
Ben W. Brumfield <benwbrum@gmail.com>
2018-06-05 11:05:58 -0500

    Conditionalized code around obsolete or missing `search_id` params for #1435
    Conflicts:
    
    	app/controllers/search_records_controller.rb
    	app/views/search_records/show.html.erb

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -52,29 +52,41 @@ class SearchRecordsController < ApplicationController
       redirect_to new_search_query_path
       return
     end
-    begin
-      @search_query = SearchQuery.find(params[:search_id])
-      if MyopicVicar::Application.config.template_set != 'freecen'
-        @previous_record = @search_query.previous_record(params[:id])
-        @next_record = @search_query.next_record(params[:id])
+    if params[:search_id]
+      begin
+        @search_query = SearchQuery.find(params[:search_id])
+        if MyopicVicar::Application.config.template_set != 'freecen'
+          @previous_record = @search_query.previous_record(params[:id])
+          @next_record = @search_query.next_record(params[:id])
+        end
+      rescue Mongoid::Errors::DocumentNotFound
+        log_possible_host_change
+        redirect_to new_search_query_path
+        return
+      rescue Mongoid::Errors::InvalidFind
+        log_missing_document("entry for search record",@search_record[:freereg1_csv_entry_id],@search_record.id)
+        flash[:notice] = "We encountered a problem locating that original entry"
+        redirect_to new_search_query_path
+        return
       end
-    rescue Mongoid::Errors::DocumentNotFound
-      log_possible_host_change
-      redirect_to new_search_query_path
-      return
-    rescue Mongoid::Errors::InvalidFind
-      log_missing_document("entry for search record",@search_record[:freereg1_csv_entry_id],@search_record.id)
-      flash[:notice] = "We encountered a problem locating that original entry"
-      redirect_to new_search_query_path
-      return
+      else
+        @search_query = nil
+        @previous_record = nil
+        @next_record = nil
+      end
     end
     @display_date = false
     @entry.display_fields(@search_record) if @entry
     @annotations = Annotation.find(@search_record.annotation_ids) if @search_record.annotation_ids
-    @search_result = @search_query.search_result
-    @viewed_records = @search_result.viewed_records
-    @viewed_records << params[:id] unless @viewed_records.include?(params[:id])
-    @search_result.update_attribute(:viewed_records, @viewed_records)
+    if @search_query
+      @search_result = @search_query.search_result
+      @viewed_records = @search_result.viewed_records
+      @viewed_records << params[:id] unless @viewed_records.include?(params[:id])
+      @search_result.update_attribute(:viewed_records, @viewed_records)
+    else
+      @search_result = nil
+      @viewed_records = nil
+    end
     #session[:viewed] << params[:id] unless  session[:viewed].length >= 10
   end
 
@@ -125,26 +137,6 @@ class SearchRecordsController < ApplicationController
         @previous_record = @search_query.previous_record(params[:id])
         @next_record = @search_query.next_record(params[:id])
       end
-# =======
-    # if params[:search_id].nil? || params[:id].nil?
-      # flash[:notice] = "Prior records no longer exist"
-      # redirect_to new_search_query_path
-      # return
-    # end
-    # begin
-      # @search_query = SearchQuery.find(params[:search_id])
-      # @search_record = SearchRecord.find(params[:id])
-      # if @search_record.nil?
-        # response, @next_record, @previous_record = @search_query.next_and_previous_records(params[:id])
-        # response ? @search_record = @search_query.locate(params[:id]) : @search_record = nil
-      # end
-      # if @search_record.nil?
-        # flash[:notice] = "Prior records no longer exist"
-        # redirect_to new_search_query_path
-        # return
-      # end
-      # @entry = Freereg1CsvEntry.find(@search_record[:freereg1_csv_entry_id])
-# >>>>>>> master
     rescue Mongoid::Errors::DocumentNotFound
       log_possible_host_change
       redirect_to new_search_query_path
@@ -155,15 +147,6 @@ class SearchRecordsController < ApplicationController
       redirect_to new_search_query_path
       return
     end
-# <<<<<<< HEAD
-# =======
-    # @entry.display_fields(@search_record)
-    # @annotations = Annotation.find(@search_record[:annotation_ids]) if @search_record[:annotation_ids]
-    # @search_result = @search_query.search_result
-    # @viewed_records = @search_result.viewed_records
-    # @viewed_records << params[:id] unless @viewed_records.include?(params[:id])
-    # @search_result.update_attribute(:viewed_records, @viewed_records)
-# >>>>>>> master
     @display_date = true
     @entry.display_fields(@search_record) if @entry
     @annotations = Annotation.find(@search_record.annotation_ids) if @search_record.annotation_ids
