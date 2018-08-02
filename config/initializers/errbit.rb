d878519fbe111a1cdb9e556b911bdd59073978ed
Scott Murray <rai.five@gmail.com>
2018-08-02 16:46:01 +0100

    Updated errbit.rb for use with Airbrake 5.0

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,10 +1,9 @@
 ERRBIT = YAML.load_file("#{Rails.root.to_s}/config/errbit.yml")[Rails.env]
 
+# Parameters below updated to be compatabile with Airbrake gem version 5.0
 
 Airbrake.configure do |config|
-  config.api_key = ERRBIT["api_key"]
-  config.host    = ERRBIT["host"]
-  config.port    = ERRBIT["port"]
-  config.secure  = config.port == ERRBIT["secure"]
-  config.ignore_only  = []
+  config.host = ERRBIT["host"]
+  config.project_id = ERRBIT["project_id"] # required, but any positive integer works
+  config.project_key = ERRBIT["project_key"]
 end
