5b1d0226f25f0f7f706d556535888778cce8dd55
Scott Murray <rai.five@gmail.com>
2018-09-03 14:45:46 +0100

    Updated Errbit to catch error if config file does not exist

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,9 +1,18 @@
-ERRBIT = YAML.load_file("#{Rails.root.to_s}/config/errbit.yml")[Rails.env]
-
 # Parameters below updated to be compatabile with Airbrake gem version 5.0
 
-Airbrake.configure do |config|
-  config.host = ERRBIT["host"]
-  config.project_id = ERRBIT["project_id"] # required, but any positive integer works
-  config.project_key = ERRBIT["project_key"]
-end
+errbit_config = "./config/errbit.yml"
+
+if File.exist?(errbit_config) 
+    ERRBIT = YAML.load_file("#{Rails.root.to_s}/config/errbit.yml")[Rails.env]
+    Airbrake.configure do |config|
+     config.host = ERRBIT["host"]
+     config.project_id = ERRBIT["project_id"] # required, but any positive integer works
+     config.project_key = ERRBIT["project_key"] # required, but any string of integers works 
+   end
+  else 
+    Airbrake.configure do |config|
+     config.host = ""
+     config.project_id = 1
+     config.project_key = "1"
+    end
+end 
