409ac54eb0473d7ef4512766e97e05cb2b04f39a
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-01-08 15:51:27 -0700

    Update delete_or_archive_old_messages_feedbacks_and_contacts.rb

--- lib/delete_or_archive_old_messages_feedbacks_and_contacts.rb
+++ lib/delete_or_archive_old_messages_feedbacks_and_contacts.rb
@@ -7,13 +7,9 @@ class DeleteOrArchiveOldMessagesFeedbacksAndContacts
     file_for_warning_messages = "log/delete_old_messages_feedbacks_and_contacts.log"
     FileUtils.mkdir_p(File.dirname(file_for_warning_messages) )
     message_file = File.new(file_for_warning_messages, "w")
-    days_until_delete = Rails.application.config.days_to_retain_message
+    days_until_delete = Rails.application.config.days_to_retain_messages
     delete_records_less_than = DateTime.now - Rails.application.config.days_to_retain_search_queries.days
     report_records_less_than = DateTime.now - (Rails.application.config.days_to_retain_search_queries + 10).days
-    p days_until_delete
-    p delete_records_less_than
-    p report_records_less_than
-    p
     p "Running message delete with an age of #{days_until_delete}"
     message_file.puts "Running message delete with an age of #{days_until_delete}"
     Feedback.no_timeout.each do |record|
