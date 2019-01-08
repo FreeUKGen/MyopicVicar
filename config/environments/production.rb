0148ee7b62bbe99f8092607ba0423fae8700572d
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-01-08 15:49:55 -0700

    add config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ MyopicVicar::Application.configure do
   config.github_issues_password = MyopicVicar::MongoConfig['github_issues_password']
   config.github_issues_repo = MyopicVicar::MongoConfig['github_issues_repo']
   config.days_to_retain_search_queries = MyopicVicar::MongoConfig['days_to_retain_search_queries']
+  config.days_to_retain_messages = MyopicVicar::MongoConfig['days_to_retain_messages']
   config.sleep = MyopicVicar::MongoConfig['sleep']
   config.emmendation_sleep = MyopicVicar::MongoConfig['emmendation_sleep']
   config.processing_delta = MyopicVicar::MongoConfig['files_for_processing'] unless MyopicVicar::MongoConfig['files_for_processing'].blank?
