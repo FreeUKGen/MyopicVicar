f275fb622e83679af6b12dab82bf54ebb79cc9e3
Scott Murray <scott.murray@freeukgenealogy.org.uk>
2019-01-24 22:09:08 +0000

    Fix for Issue 491 - remove redundant case statements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -189,14 +189,9 @@ module ApplicationHelper
 
   def google_advert
     @data_ad_slot = ""
-    case
-    when current_page?(freecen_coverage_path)
+    if current_page?(freecen_coverage_path)
       @data_ad_slot = "9056426667"
-    when current_page?('/') || ('/search_queries') || current_page?(new_search_query_path) || current_page?(new_search_query_path(:search_id => @search_query))
-      @data_ad_slot = "2003577939"
-    when current_page?(search_query_path(@search_query))
-      @data_ad_slot = "2003577939"
-    when current_page?(:action => 'show', :controller => 'search_records')
+    else
       @data_ad_slot = "2003577939"
     end
     banner = <<-HTML
