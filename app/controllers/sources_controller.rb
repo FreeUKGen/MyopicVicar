73d0a815c533e8201b31b150424cd246e43fd499
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-02-01 17:05:19 -0700

    Update sources_controller.rb

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -101,15 +101,15 @@ class SourcesController < ApplicationController
   end
 
   def index
+
+    params[:id] = session[:register_id] if params[:id].blank?
+    @source = Source.where(register_id: params[:id]).all
     display_info
     logger.warn @register
     logger.warn @church
     logger.warn @place
     logger.warn @source
-    logger.warn  params[:id]
-    params[:id] = session[:register_id] if params[:id].blank?
-    @source = Source.where(register_id: params[:id]).all
-    logger.warn @source
+    logger.warn params[:id]
     redirect_back(fallback_location: root_path, notice: 'Attempting to display an incomplete source') && return if @register.blank? ||
       @church.blank? || @place.blank? || @source.blank?
 
