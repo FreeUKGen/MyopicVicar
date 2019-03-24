33e8e3b6ac10bde3a8db5abc37d38be48e64cfed
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-03-24 13:22:26 -0600

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,20 +1,8 @@
 require 'development_mail_interceptor'
-if Rails.env.development? && !(Rails.application.config.website == 'http://test2.freereg.org.uk')
-  ActionMailer::Base.delivery_method = :smtp
-    # SMTP settings for gmail
-    ActionMailer::Base.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => ENV['gmail_username'],
-      :password             => ENV['gmail_password'],
-      :authentication       => "plain",
-      :enable_starttls_auto => true
-    }
-    ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
-else
-  ActionMailer::Base.delivery_method = :sendmail
-  ActionMailer::Base.perform_deliveries = true
-  ActionMailer::Base.raise_delivery_errors = true
+if Rails.env.development?
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
 end
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.default_url_options[:host] = Rails.application.config.website
-
