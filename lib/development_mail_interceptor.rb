ad180c717abcff51746b8c59503383983ef3bef7
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-03-24 13:19:02 -0600

    Update development_mail_interceptor.rb

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -2,6 +2,5 @@ class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "Trapped FreeCen2 message #{message.to} #{message.subject}"
     message.to = 'kirk.dawson@freeukgenealogy.org.uk'
-    message.to = 'vinodhini.subbu@freeukgenealogy.org.uk'
   end
 end
