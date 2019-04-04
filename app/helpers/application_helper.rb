2608c1a316c13482c2d0169f9c20c86591f49bee
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2019-04-04 11:26:29 +0100

    Adds FreeCEN2 survey link in search results page: Issue 636

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -322,4 +322,21 @@ module ApplicationHelper
   def freecen1_text
     ("#{freecen1_link_text.strip} #{freecen1_link.strip}").html_safe
   end
+
+  def freecen2_survey_text1
+    content_tag :span, "FreeCEN2 is a work in progress;"
+  end
+
+  def freecen2_survey_link
+    link_to "we want your feedback ", "https://docs.google.com/a/freeukgenealogy.org.uk/forms/d/e/1FAIpQLSdT2291Wot-IdsP_Y2_w82BsP9WkqXfdTnX_8V-Y7g6pClqvg/viewform",
+    class: "go", target: :_blank, title: 'link opens in a new tab'
+  end
+
+  def freecen2_survey_text2
+    content_tag :span, "on all aspects so that we can make it better."
+  end
+
+  def freecen2_survey_full_text
+    ("#{freecen2_survey_text1.strip} #{freecen2_survey_link.strip} #{freecen2_survey_text2.strip}").html_safe
+  end
 end
