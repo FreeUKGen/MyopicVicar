62174ad2b6829a1ab9c7b0f675e12df076df124b
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-04-15 13:28:27 -0600

    Format and partial layout fix

---
+++ public/public/assets/jquery.cookiesDirective-e5230d5129125126e32c6fb22eabcb3971846bb3c1feaa0a28db6612e77ecab9.js
@@ -0,0 +1,272 @@
+/* Cookies Directive - The rewrite. Now a jQuery plugin
+ * Version: 2.0.1
+ * Author: Ollie Phillips
+ * 24 October 2013
+ */
+
+
+;(function($) {
+	$.cookiesDirective = function(options) {
+			
+		// Default Cookies Directive Settings
+		var settings = $.extend({
+			//Options
+			explicitConsent: false,
+			position: 'bottom',
+			duration: 10,
+			limit: 0,
+			message: null,				
+			cookieScripts: null,
+			privacyPolicyUri: 'privacy.html',
+			scriptWrapper: function(){},	
+			// Styling
+			fontFamily: 'helvetica',
+      fontColor: '#FFFFFF',
+      fontSize: '10px',
+      backgroundColor: '#008000',
+      backgroundOpacity: '80',
+      linkColor: '#CA0000'
+    }, options);
+		// Perform consent checks
+		if(!getCookie('cookiesDirective')) {
+			if(settings.limit > 0) {
+				// Display limit in force, record the view
+				if(!getCookie('cookiesDisclosureCount')) {
+					setCookie('cookiesDisclosureCount',1,1);		
+				} else {
+					var disclosureCount = getCookie('cookiesDisclosureCount');
+					disclosureCount ++;
+					setCookie('cookiesDisclosureCount',disclosureCount,1);
+				}
+				
+				// Have we reached the display limit, if not make disclosure
+				if(settings.limit >= getCookie('cookiesDisclosureCount')) {
+					disclosure(settings);		
+				}
+			} else {
+				// No display limit
+				disclosure(settings);
+			}		
+			
+			// If we don't require explicit consent, load up our script wrapping function
+			if(!settings.explicitConsent) {
+				settings.scriptWrapper.call();
+			}	
+		} else {
+			// Cookies accepted, load script wrapping function
+			settings.scriptWrapper.call();
+		}		
+	};
+	
+	// Used to load external javascript files into the DOM
+	$.cookiesDirective.loadScript = function(options) {
+		var settings = $.extend({
+			uri: 		'', 
+			appendTo: 	'body'
+		}, options);	
+		
+		var elementId = String(settings.appendTo);
+		var sA = document.createElement("script");
+		sA.src = settings.uri;
+		sA.type = "text/javascript";
+		sA.onload = sA.onreadystatechange = function() {
+			if ((!sA.readyState || sA.readyState == "loaded" || sA.readyState == "complete")) {
+				return;
+			} 	
+		};
+		switch(settings.appendTo) {
+			case 'head':			
+				$('head').append(sA);
+			  	break;
+			case 'body':
+				$('body').append(sA);
+			  	break;
+			default: 
+				$('#' + elementId).append(sA);
+		}
+	};
+	
+	// Helper scripts
+	// Get cookie
+	window.getCookie = function(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	};
+	
+	// Set cookie
+	window.setCookie = function(name,value,days) {
+		var expires = "";
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			expires = "; expires="+date.toGMTString();
+		}
+		document.cookie = name+"="+value+expires+"; path=/";
+	};
+	
+	// Detect IE < 9
+	var checkIE = function(){
+		var version;
+		if (navigator.appName == 'Microsoft Internet Explorer') {
+	        var ua = navigator.userAgent;
+	        var re = new RegExp("MSIE ([0-9]{1,}[\\.0-9]{0,})");
+	        if (re.exec(ua) !== null) {
+	            version = parseFloat(RegExp.$1);
+			}	
+			if (version <= 8.0) {
+				return true;
+			} else {
+				if(version == 9.0) {
+					if(document.compatMode == "BackCompat") {
+						// IE9 in quirks mode won't run the script properly, set to emulate IE8	
+						var mA = document.createElement("meta");
+						mA.content = "IE=EmulateIE8";				
+						document.getElementsByTagName('head')[0].appendChild(mA);
+						return true;
+					} else {
+						return false;
+					}
+				}	
+				return false;
+			}		
+	    } else {
+			return false;
+		}
+	};
+
+	// Disclosure routines
+	var disclosure = function(options) {
+		var settings = options;
+		settings.css = 'fixed';
+		
+		// IE 9 and lower has issues with position:fixed, either out the box or in compatibility mode - fix that
+		if(checkIE()) {
+			settings.position = 'top';
+			settings.css = 'absolute';
+		}
+		
+		// Any cookie setting scripts to disclose
+		var scriptsDisclosure = '';
+		if (settings.cookieScripts) {
+			var scripts = settings.cookieScripts.split(',');
+			var scriptsCount = scripts.length;
+			var scriptDisclosureTxt = '';
+			if(scriptsCount>1) {
+				for(var t=0; t < scriptsCount - 1; t++) {
+					 scriptDisclosureTxt += scripts[t] + ', ';	
+				}	
+				scriptsDisclosure = ' We use ' +  scriptDisclosureTxt.substring(0,  scriptDisclosureTxt.length - 2) + ' and ' + scripts[scriptsCount - 1] + ' scripts, which all set cookies. ';
+			} else {
+				scriptsDisclosure = ' We use a ' + scripts[0] + ' script which sets cookies.';		
+			}
+		} 
+		
+		// Create overlay, vary the disclosure based on explicit/implied consent
+		// Set our disclosure/message if one not supplied
+		var html = ''; 
+		html += '<div id="epd">';
+		html += '<div id="cookiesdirective" style="position:'+ settings.css +';'+ settings.position + ':-300px;left:0px;width:100%;';
+		html += 'height:auto;background:' + settings.backgroundColor + ';opacity:.' + settings.backgroundOpacity + ';';
+		html += '-ms-filter: “alpha(opacity=' + settings.backgroundOpacity + ')”; filter: alpha(opacity=' + settings.backgroundOpacity + ');';
+		html += '-khtml-opacity: .' + settings.backgroundOpacity + '; -moz-opacity: .' + settings.backgroundOpacity + ';';
+		html += 'color:' + settings.fontColor + ';font-family:' + settings.fontFamily + ';font-size:' + settings.fontSize + ';';
+		html += 'text-align:center;z-index:1000;">';
+		html += '<div style="position:relative;height:auto;width:90%;padding:10px;margin-left:auto;margin-right:auto;">';
+			
+		if(!settings.message) {
+			if(settings.explicitConsent) {
+				// Explicit consent message
+				settings.message = 'Cookies are essential for this website to operate, blocking them stops the search working. ';
+				
+			} else {
+				// Implied consent message
+				settings.message = 'We use cookies to ensure you get the best experience with our website. By continuing to use our website you consent to our use of cookies. ';
+			}		
+		}	
+		html += settings.message;
+		
+		// Build the rest of the disclosure for implied and explicit consent
+		if(settings.explicitConsent) {
+			// Explicit consent disclosure
+			
+			html += scriptsDisclosure + 'Review our <a tabindex="1" style="color:'+ settings.linkColor + ';font-weight:bold;';
+			html += 'font-family:' + settings.fontFamily + ';font-size:' + settings.fontSize + ';" href="'+ settings.privacyPolicyUri + '">Cookie policy</a>.';
+			html += '<div style="margin-top:5px;"><label for="epdagree" style="font-weight: normal; display: inline;">Tick this box to accept their use &nbsp;<input type="checkbox" name="epdagree" id="epdagree" tabindex="2" />&nbsp;&nbsp;</label> and then click &nbsp;';
+			html += '<input class="btn btn--navy btn--small weight--light" tabindex="3" type="submit" name="explicitsubmit" id="explicitsubmit" value="Continue"/></div></div>';
+		
+		} else {
+			// Implied consent disclosure
+			html += scriptsDisclosure + 'Review our <a tabindex="1" style="color:'+ settings.linkColor + ';font-weight:bold;';
+      html += 'font-family:' + settings.fontFamily + ';font-size:' + settings.fontSize + ';" href="'+ settings.privacyPolicyUri + '">Cookie policy</a>.';
+      html += '<div style="margin-top:5px;"><input class="btn btn--navy btn--small weight--light" tabindex="3" type="submit" name="impliedsubmit" id="impliedsubmit" value="Do not show this message again"/></div></div>';
+		}
+		html += '</div></div>';
+		$('body').append(html);
+		
+		// Serve the disclosure, and be smarter about branching
+		var dp = settings.position.toLowerCase();
+		if(dp != 'top' && dp!= 'bottom') {
+			dp = 'top';
+		}	
+		var opts = { in: null, out: null};
+		if(dp == 'top') {
+			opts.in = {'top':'0'};
+			opts.out = {'top':'-300'};
+		} else {
+			opts.in = {'bottom':'0'};
+			opts.out = {'bottom':'-300'};
+		}		
+
+		// Start animation
+		$('#cookiesdirective').animate(opts.in, 1000, function() {
+			// Set event handlers depending on type of disclosure
+			if(settings.explicitConsent) {
+				// Explicit, need to check a box and click a button
+				$('#explicitsubmit').click(function() {
+					if($('#epdagree').is(':checked')) {	
+						// Set a cookie to prevent this being displayed again
+						setCookie('cookiesDirective',1,365);	
+						// Close the overlay
+						$('#cookiesdirective').animate(opts.out,1000,function() { 
+							// Remove the elements from the DOM and reload page
+							$('#cookiesdirective').remove();
+							location.reload(true);
+						});
+					} else {
+						// We need the box checked we want "explicit consent", display message
+						$('#epdnotick').css('display', 'block'); 
+					}	
+				});
+			} else {
+				// Implied consent, just a button to close it
+				$('#impliedsubmit').click(function() {
+					// Set a cookie to prevent this being displayed again
+					setCookie('cookiesDirective',1,365);	
+					// Close the overlay
+					$('#cookiesdirective').animate(opts.out,1000,function() { 
+						// Remove the elements from the DOM and reload page
+						$('#cookiesdirective').remove();
+					});
+				});
+			}	
+			
+			if(settings.duration > 0)
+			{
+				// Set a timer to remove the warning after 'settings.duration' seconds
+				setTimeout(function(){
+					$('#cookiesdirective').animate({
+						opacity:'0'
+					},2000, function(){
+						$('#cookiesdirective').css(dp,'-300px');
+					});
+				}, settings.duration * 1000);
+			}
+		});	
+	};
+})(jQuery);
---
+++ public/public/assets/refinery/serializelist-76784be04d6cc5acbe4dbdbad14194f020ee6def59efabe2728248e4b6abac57.js
@@ -0,0 +1,69 @@
+/**
+ * jQuery Serialize List
+ * Copyright (c) 2009 Mike Botsko, Botsko.net LLC
+ * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * Copyright notice and license must remain intact for legal use
+ * Version .2
+ *
+ * Serialize an unordered or ordered list item. Optional ability
+ * to determine which attributes are included. The serialization
+ * will be read by PHP as a multidimensional array which you may
+ * use for saving state.
+ *
+ * https://github.com/botskonet/jquery.serialize-list
+ */
+
+(function($) {
+  $.fn.serializelist = function(options) {
+    var defaults = {
+        attributes: ['id'],
+        allow_nest: true,
+        prepend: 'ul',
+        att_regex: false,
+        is_child: false
+      },
+      opts = $.extend(defaults, options),
+      attrs = opts.attributes,
+      serialStr = '',
+      val, x, att, child_base;
+
+    var att_rep = function(att, val) {
+      if(!opts.att_regex) { return val; }
+
+			for(x in opts.att_regex) {
+				if(opts.att_regex[x].att === att) {
+					return val.replace(opts.att_regex[x].regex, '');
+				}
+			}
+		};
+
+    if(!opts.is_child) { opts.prepend = '&' + opts.prepend; }
+
+    this.each(function(ul_count, ul) {
+      $(ul).children().each(function(li_count, li) {
+    		if(opts.allow_nest || attrs.length > 1) {
+    			for(var i = 0; i < attrs.length; i++) {
+    				val = att_rep(attrs[i], $(li).attr(attrs[i]));
+    				serialStr += opts.prepend+'['+ul_count+']['+li_count+']['+attrs[i]+']='+val;
+    			}
+    		} else {
+    			val = att_rep(attrs[0], $(li).attr(attrs[0]));
+    			serialStr += opts.prepend+'['+ul_count+']['+li_count+']='+val;
+    		}
+
+    		if(opts.allow_nest) {
+    			child_base = opts.prepend+'['+ul_count+']['+li_count+'][children]';
+    			$(li).children().each(function() {
+    				if(this.tagName == 'UL' || this.tagName == 'OL') {
+    					serialStr += $(this).serializelist({'prepend': child_base, 'is_child': true});
+    				}
+    			});
+    		}
+
+        li_count++;
+      });
+    });
+
+    return(serialStr);
+  };
+})(jQuery);
