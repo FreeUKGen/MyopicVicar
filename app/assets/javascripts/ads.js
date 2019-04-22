c1c6756a6986d7789f2eb9c89803d4aaa69400cb
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-04-22 11:55:37 -0600

    Create ads.js

---
+++ app/assets/javascripts/ads.js
@@ -0,0 +1,5 @@
+// This file is used to detect Adblocking software.
+// If Adblock is being used, canRunAds will return
+// undefined when called elsewhere, because this file will be blocked
+
+var canRunAds = true;
