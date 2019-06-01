d6a15dc77ee9bd1b7ec98b3591bb62927aa7f08f
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-06-01 17:19:02 -0600

    Fix issue in development setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,9 @@
 require 'development_mail_interceptor'
 if Rails.env.development?
   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.default_url_options[:host] = Rails.application.config.website
 else
   ActionMailer::Base.delivery_method = :sendmail
   ActionMailer::Base.perform_deliveries = true
