7a7524ea38138cdf394336d4625a24810b47fa42
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-06-01 17:28:36 -0600

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,7 @@
 require 'development_mail_interceptor'
 if Rails.env.development?
   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
+  ActionMailer::Base.delivery_method = :sendmail if Rails.application.config.website == 'https://test3.freereg.org.uk'
   ActionMailer::Base.perform_deliveries = true
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.default_url_options[:host] = Rails.application.config.website
