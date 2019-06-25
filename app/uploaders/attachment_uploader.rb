ff5c77f5dfdf8d1144fc77e75f464448cc150b62
Shem <42591036+Shemiroth@users.noreply.github.com>
2019-06-25 16:51:02 +0100

    Removed "_"

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -43,7 +43,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
+  def extension_whitelist
    %w(txt log rtf htm html pdf)
   end
 
