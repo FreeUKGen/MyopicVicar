e86cbd3eb246934d01496062d6ceb19b548cf2fd
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-08-16 17:27:26 -0600

    First improvment

--- app/helpers/freecen_errors_helper.rb
+++ app/helpers/freecen_errors_helper.rb
@@ -33,38 +33,4 @@ module FreecenErrorsHelper
     partsb[1].gsub(')', '')
   end
 
-  def load_error_county(error)
-    parts = error.split(' filename:')
-    parts[0]
-  end
-
-  def load_error_file(error)
-    parts = error.split(' filename:')
-    partsa = parts[1].split(' ')
-    partsa[0].strip
-  end
-
-  def load_error_year(error)
-    parts = error.split(' filename:')
-    partsa = parts[1].split(' ')
-    partsb = partsa[1].split(' ')
-    partsb[0]
-  end
-
-  def load_error_piece(error)
-    parts = error.split('piece:')
-    partsa = parts[1].split(' ')
-    partsa[0]
-  end
-
-  def load_error_suffix(error)
-    partsa = error.split('suffix:')
-    partsb = partsa[1].split('status:')
-    partsb[0]
-  end
-
-  def load_error_status(error)
-    partsb = error.split('status:')
-    partsb[1]
-  end
 end
