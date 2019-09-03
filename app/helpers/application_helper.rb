3b9e9719e398efa9dbede4e4d8b99b20dd88b0c0
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-09-03 08:07:03 -0600

    test version 3 this one for body ad

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -519,11 +519,12 @@ module ApplicationHelper
                       (adsbygoogle=window.adsbygoogle||[]).pauseAdRequests=1;
                       </script>
                       <!-- Responsive ad -->
-                      <ins class="adsbygoogle adSenseBanner"
+                      <ins class="adsbygoogle "
                       style="display:block"
                       data-ad-client="#{data_ad_client}"
                       data-ad-slot= "#{@data_ad_slot}"
-                      data-ad-format="auto"></ins>
+                      data-ad-format="auto"
+                      data-full-width-responsive="true"></ins>
                       <script>
                       window.update_personalized_adverts = function (preference) {
                         if(preference == 'accept') {
