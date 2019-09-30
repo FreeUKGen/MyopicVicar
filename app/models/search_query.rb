7945d87c91268c0c69702ade6ce007b9f1d33401
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2019-09-30 16:20:06 +0100

    test

--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -18,6 +18,7 @@ class SearchQuery
     LOCATION='location'
     NAME='transcript_names'
 
+
     ALL_ORDERS = [
       TYPE,
       BIRTH_COUNTY,
@@ -32,17 +33,25 @@ class SearchQuery
 
   field :first_name, type: String # , :required => false
   field :last_name, type: String # , :required => false
+  field :spouse_first_name, type: String # , :required => false
+  field :mother_last_name, type: String # , :required => false
+  field :age_at_death, type: String # , :required => false
+  field :volume, type: String # , :required => false
+  field :page, type: String # , :required => false
   field :fuzzy, type: Boolean
   field :role, type: String # , :required => false
   validates_inclusion_of :role, :in => NameRole::ALL_ROLES + [nil]
   field :record_type, type: String#, :required => false
   validates_inclusion_of :record_type, :in => RecordType.all_types + [nil]
+  field :bmd_record_type, type: Array#, :required => false
   field :chapman_codes, type: Array, default: [] # , :required => false
   #  validates_inclusion_of :chapman_codes, :in => ChapmanCode::values+[nil]
   #field :extern_ref, type: String
   field :inclusive, type: Boolean
   field :witness, type: Boolean
   field :start_year, type: Integer
+  field :start_quarter, type: Integer
+  field :end_quarter, type: Integer
   field :end_year, type: Integer
   field :radius_factor, type: Integer, default: 101
   field :search_nearby_places, type: Boolean
@@ -62,6 +71,7 @@ class SearchQuery
   field :use_decomposed_dates, type: Boolean, default: false
   field :all_radius_place_ids, type: Array, default: []
   field :wildcard_search, type: Boolean, default: false
+  field :first_name_exact_match, type: Boolean, default: false
 
   field :birth_chapman_codes, type: Array, default: []
   field :birth_place_name, type: String
@@ -70,7 +80,7 @@ class SearchQuery
 
   embeds_one :search_result
 
-  validate :name_not_blank
+  validate :name_not_blank unless MyopicVicar::Application.config.template_set == 'freebmd'
   validate :date_range_is_valid
   validate :radius_is_valid
   validate :county_is_valid
@@ -239,6 +249,28 @@ class SearchQuery
     params
   end
 
+  def get_date_quarter_params
+    get_quarter
+  end
+
+  def get_quarter
+    params = {}
+    params[:quarternumber] = start_quarter_number..end_quarter_number
+    params
+  end
+
+  def start_quarter_number
+    (start_year - 1837)* 4 + start_quarter
+  end
+
+  def end_quarter_number
+    (end_year - 1837)* 4 + end_quarter
+  end
+
+  def quarter_number(year:,quarter:)
+    (year - 1837)*4 + quarter
+  end
+
   def explain_plan
     SearchRecord.where(search_params).max_scan(1 + FreeregOptionsConstants::MAXIMUM_NUMBER_OF_SCANS).asc(:search_date).all.explain
   end
@@ -292,7 +324,6 @@ class SearchQuery
   def filter_name_types(search_results)
     filtered_records = Array.new { {} }
     search_results.each do |search_result|
-      raise search_result.inspect
       search_result[:search_names].each do |search_name|
         if fuzzy
           include_record = include_record_for_fuzzy_search(search_name)
@@ -322,6 +353,49 @@ class SearchQuery
       response = false
       return response
     end
+  end
+
+  def bmd_search_results
+    self.search_result.records.values
+  end
+
+  def get_bmd_search_results
+    return get_bmd_search_response, bmd_search_results.map{|h| SearchQuery.get_search_table.new(h)}, ucf_search_results, search_result_count if get_bmd_search_response
+    return get_bmd_search_response if !get_bmd_search_response
+  end
+
+  def ucf_search_results
+    []
+  end
+
+  def search_result_count
+    bmd_search_results.length
+  end
+
+  def sort_search_results
+    self.sort_results(bmd_search_results) unless bmd_search_results.nil?
+  end
+
+  def get_bmd_search_response
+    self.search_result.records.respond_to?(:values)
+  end
+
+   def filter_name_types(search_results)
+    filtered_records = Array.new { {} }
+    search_results.each do |search_result|
+      search_result[:search_names].each do |search_name|
+        if fuzzy
+          include_record = include_record_for_fuzzy_search(search_name)
+        elsif wildcard_search
+          include_record = include_record_for_wildcard_search(search_name)
+        else
+          include_record = include_record_for_standard_search(search_name)
+        end
+        filtered_records << search_result if include_record
+        break filtered_records if include_record
+      end
+    end
+    filtered_records
   end
 
   def include_record_for_fuzzy_search(search_name)
@@ -425,13 +499,67 @@ class SearchQuery
       else
         name_params['first_name'] = first_name.downcase if first_name
         name_params['last_name'] = last_name.downcase if last_name.present?
-        name_params[:surname] = name_params.delete('last_name')
         params['search_names'] =  { '$elemMatch' => name_params}
         params =   name_params if SearchQuery.app_template == 'freebmd'
       end
     end
     params
   end
+
+  def name_search_params_bmd
+    params = Hash.new
+    name_params = Hash.new
+    #type_array = [SearchRecord::PersonType::PRIMARY]
+    #type_array << SearchRecord::PersonType::FAMILY if inclusive
+    #type_array << SearchRecord::PersonType::WITNESS if witness
+    #search_type = type_array.size > 1 ? { '$in' => type_array } : SearchRecord::PersonType::PRIMARY
+    #name_params['type'] = search_type
+    if query_contains_wildcard?
+
+    else
+      name_params['first_name'] = first_name.downcase if first_name
+      name_params['last_name'] = last_name.downcase if last_name.present?
+      name_params['SurnameSx'] = Text::Soundex.soundex(last_name) if fuzzy
+      params =   name_params 
+    end
+    params
+  end
+
+  def surname_params
+    if fuzzy
+      surname_param = Text::Soundex.soundex(last_name)
+    else
+      surname_param = last_name.downcase
+    end
+    surname_param
+  end
+
+  def refresh_name_params(name:, replacement_name:, params_hash:)
+    if name.present?
+      params_hash["#{name}"] = name.downcase
+      params_hash["#{replacement_name}"] = params_hash.delete("#{name}") if SearchQuery.app_template == 'freebmd'
+    end
+  end
+
+  def bmd_fields_name
+    { first_name: 'GivenName', last_name: 'Surname', bmd_record_type: 'RecordTypeID', SurnameSx: 'SurnameSx'}
+  end
+
+  def symbolize_search_params_keys
+    bmd_search_params.symbolize_keys
+  end
+
+  def fields_needs_name_update
+    bmd_fields_name.keys & symbolize_search_params_keys.keys
+  end
+
+  def bmd_adjust_field_names
+    symbolize_search_params_keys.deep_transform_keys do |key| 
+       (fields_needs_name_update.include?key) ? key = bmd_fields_name[key].to_sym : key =key
+    end
+  end
+
+  
 
   def next_and_previous_records(current)
     if search_result.records.respond_to?(:values)
@@ -593,15 +721,52 @@ class SearchQuery
     params
   end
 
+  def bmd_record_type_params
+    #raise bmd_record_type.inspect
+    params = {}
+    params[:RecordTypeID] = bmd_record_type.map(&:to_i) if bmd_record_type.present?
+    params[:RecordTypeID] = RecordType.all_types if bmd_record_type.blank? || bmd_record_type == '0'
+    params
+  end
+
+  def first_name_filteration
+      "GivenName like '#{bmd_adjust_field_names[:GivenName]}%'"
+  end
+
+  def bmd_params_hash
+    self.first_name_exact_match ? bmd_adjust_field_names : bmd_adjust_field_names.except(:GivenName)
+  end
+
   def search
     @search_parameters = search_params
+    @search_parameters = bmd_params_hash if SearchQuery.app_template == 'freebmd'
     @search_index = SearchQuery.get_search_table.index_hint(@search_parameters)
     # @search_index = 'place_rt_sd_ssd' if query_contains_wildcard?
     logger.warn("#{App.name_upcase}:SEARCH_HINT: #{@search_index}")
     update_attribute(:search_index, @search_index)
-    records = SearchQuery.get_search_table.where(@search_parameters)#.hint(@search_index.to_s).max_time_ms(Rails.application.config.max_search_time).limit(FreeregOptionsConstants::MAXIMUM_NUMBER_OF_RESULTS)
+    records = SearchQuery.get_search_table.where(@search_parameters).hint(@search_index.to_s).max_time_ms(Rails.application.config.max_search_time).limit(FreeregOptionsConstants::MAXIMUM_NUMBER_OF_RESULTS)
+    records = records.where(first_name_filteration) unless self.first_name_exact_match
     persist_results(records)
     persist_additional_results(secondary_date_results) if App.name == 'FreeREG' && (result_count < FreeregOptionsConstants::MAXIMUM_NUMBER_OF_RESULTS)
+    #search_ucf if can_query_ucf? && result_count < FreeregOptionsConstants::MAXIMUM_NUMBER_OF_RESULTS
+    records
+  end
+
+  def search_records
+    if MyopicVicar::Application.config.template_set = 'freebmd'
+      self.freebmd_search_records
+    else
+      self.search
+    end
+  end
+
+  def freebmd_search_records
+    @search_index = SearchQuery.get_search_table.index_hint(bmd_adjust_field_names)
+    logger.warn("#{App.name_upcase}:SEARCH_HINT: #{@search_index}")
+    update_attribute(:search_index, @search_index)
+    records = SearchQuery.get_search_table.where(bmd_params_hash).limit(FreeregOptionsConstants::MAXIMUM_NUMBER_OF_RESULTS)
+    records = records.where(first_name_filteration) unless self.first_name_exact_match
+    persist_results(records)
     #search_ucf if can_query_ucf? && result_count < FreeregOptionsConstants::MAXIMUM_NUMBER_OF_RESULTS
     records
   end
@@ -621,8 +786,16 @@ class SearchQuery
     params = {}
     params.merge!(name_search_params)
     #params.merge!(place_search_params)
-    #params.merge!(record_type_params)
-    #params.merge!(date_search_params)
+    params.merge!(record_type_params) unless MyopicVicar::Application::config.template_set == 'freebmd'
+    params.merge!(date_search_params)
+    params
+  end
+
+  def bmd_search_params
+    params = {}
+    params.merge!(name_search_params_bmd)
+    params.merge!(bmd_record_type_params)
+    params.merge!(get_date_quarter_params)
     params
   end
 
