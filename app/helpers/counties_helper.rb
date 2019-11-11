008be917e123192c840998641ce9f46ca0655f89
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-11-11 12:15:30 -0700

    Added display of open status to coordinators

--- app/helpers/counties_helper.rb
+++ app/helpers/counties_helper.rb
@@ -1,7 +1,27 @@
 module CountiesHelper
   def email_address(coordinator)
-    person = UseridDetail.where(:userid => coordinator).first
+    person = UseridDetail.where(userid: coordinator).first
     email_address = person.email_address if person.present?
+  end
+
+  def county_coordinator_agreement(coordinator)
+    person = UseridDetail.where(userid: coordinator).first
+    if person.present?
+      status = person.new_transcription_agreement
+      case status
+      when 'Accepted'
+        result = 'A'
+      when 'Unknown'
+        result = 'U'
+      when 'Declined'
+        result = 'D'
+      when 'Pending'
+        result = 'P'
+      end
+    else
+      result = 'U'
+    end
+    result
   end
 
 end
