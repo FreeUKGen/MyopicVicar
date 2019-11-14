df41d1862e60c1922cf2b529a1f93f2a09efea24
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-11-13 17:09:03 -0700

    Try to catch county coordinators

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
-    message.subject = "Trapped FreeCen2 message #{message.to} #{message.subject}"
+    message.subject = "Trapped #{appname} message #{message.to} #{message.subject}"
     message.to = 'kirk.dawson@freeukgenealogy.org.uk'
   end
 end
