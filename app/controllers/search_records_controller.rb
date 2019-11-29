8776170b878c96509d9e5e68655128a6f838ba14
Kirk Dawson <kirk.dawson.bc@gmail.com>
2019-11-29 16:23:52 -0700

    Code for 2077 added

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -146,7 +146,7 @@ class SearchRecordsController < ApplicationController
       end
       @image_id = @entry.get_the_image_id(@church, @user, session[:manage_user_origin], session[:image_server_group_id], session[:chapman_code])
       @order, @array_of_entries, @json_of_entries = @entry.order_fields_for_record_type(@search_record[:record_type], @entry.freereg1_csv_file.def, current_authentication_devise_user.present?)
-      @embargoed = @search_record.embargoed
+      @embargoed = @search_record[:embargoed]
     end
   end
 
