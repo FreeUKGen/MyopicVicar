4d1135011e551d9ed9d5ee905149ee901ab6632e
kirkdawson <kirk.dawson.bc@gmail.com>
2019-12-20 11:32:46 -0700

    embargo code

--- app/models/emendation_rule.rb
+++ app/models/emendation_rule.rb
@@ -2,12 +2,12 @@ class EmendationRule
   include Mongoid::Document
   include Mongoid::Timestamps
   field :original, type: String
-  field :replacement, type: String  
+  field :replacement, type: String
   field :gender, type: String #m=male, f=female (nil if rule applies to both)
-   index({ original: 1, replacement: 1}, { unique: true })
-   index({emendation_type_id: 1})
-  
-  belongs_to :emendation_type
+  index({ original: 1, replacement: 1}, { unique: true })
+  index({emendation_type_id: 1})
+
+  belongs_to :emendation_type, index: true
 
   class << self
     def all_original_by_replacement(replacement)
@@ -19,11 +19,11 @@ class EmendationRule
     end
 
     def sort_by_initial_letter(replacement_array)
-       hash_letter = Hash.new { |hash, key| hash[key] = [] }
-       replacement_array.each do |replacement|
-         hash_letter[replacement[0]].push(replacement)
-       end
-       hash_letter.sort.to_h
+      hash_letter = Hash.new { |hash, key| hash[key] = [] }
+      replacement_array.each do |replacement|
+        hash_letter[replacement[0]].push(replacement)
+      end
+      hash_letter.sort.to_h
     end
   end
 
