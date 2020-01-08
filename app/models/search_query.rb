413b8ffca7a467374fef80e1498834528dbeec97
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2020-01-08 17:52:39 +0000

    test

--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -1191,7 +1191,7 @@ class SearchQuery
       #quarter_array = quarters_months.select{|i| i.include?month}
       #quarter = quarters_months.index(quarter_array)
       #dob_quarter = quarter_number(year: year, quarter: quarter)
-      #logger.warn("#{dob_quarter}")
+      logger.warn("#{difference}")
       #logger.warn("#{(self.max_age_at_death.to_i..self.min_age_at_death.to_i).include?(r.QuarterNumber.to_i - dob_quarter.to_i)}")
       (self.max_age_at_death.to_i..self.min_age_at_death.to_i).include?(difference) if check_age_range?
       (r.QuarterNumber.to_i - dob_quarter.to_i) == self.age_at_death if self.age_at_death.present?
