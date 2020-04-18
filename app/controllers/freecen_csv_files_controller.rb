50dd1705633442e1bec3bab65e685161ba4eb5db
kirkdawson <kirk.dawson.bc@gmail.com>
2020-04-18 13:23:27 -0600

    View file for userid

--- app/controllers/freecen_csv_files_controller.rb
+++ app/controllers/freecen_csv_files_controller.rb
@@ -391,16 +391,15 @@ class FreecenCsvFilesController < ApplicationController
     redirect_back(fallback_location: freecen_csv_file_path(@freecen_csv_file), notice: 'File is currently awaiting processing and should not be edited') && return unless @freecen_csv_file.can_we_edit?
     controls(@freecen_csv_file)
     proceed, message = @freecen_csv_file.remove_batch
-    proceed ? flash[:notice] = 'The removal of the batch entry was successful' : flash[:notice] = message
-
+    flash[:notice] = proceed ? 'The removal of the batch was successful' : message
     if session[:my_own]
       redirect_to my_own_freecen_csv_file_path
-    elsif session[:current_page]
-      redirect_to session[:current_page]
-    elsif @return_location.blank?
-      redirect_to manage_resource_path(@user)
+    elsif session[:page]
+      redirect_to session[:page]
+    elsif session[:return_to].present?
+      redirect_to session[:return_to]
     else
-      redirect_to piece_path(@return_location)
+      redirect_to manage_resource_path(@user)
     end
   end
 
--- app/helpers/userid_details_helper.rb
+++ app/helpers/userid_details_helper.rb
@@ -23,4 +23,13 @@ module UseridDetailsHelper
     end
     result
   end
+
+  def list_userid_files
+    case appname_downcase
+    when 'freereg'
+      link_to 'List Batches', by_userid_freereg1_csv_file_path(@userid), method: :get, class: 'btn   btn--small'
+    when 'freecen'
+      link_to 'List Batches', by_userid_freecen_csv_file_path(@userid), method: :get, class: 'btn   btn--small'
+    end
+  end
 end
