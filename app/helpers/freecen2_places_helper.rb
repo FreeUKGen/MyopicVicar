b2e1af61e7ffc2f47bb3b9b1ef1a6e76e1b62644
kirkdawson <kirk.dawson.bc@gmail.com>
2020-08-17 18:26:48 -0600

    Update freecen2_places_helper.rb

--- app/helpers/freecen2_places_helper.rb
+++ app/helpers/freecen2_places_helper.rb
@@ -3,6 +3,7 @@ module Freecen2PlacesHelper
     field = ''
     place.alternate_freecen2_place_names.each_with_index do |place_name, ind|
       field += ', ' if ind > 0
+      field += place_name.alternate_name
     end
     field
   end
