cb6b7ed0d0cad780fc20533effffe5a3b9c6c620
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2020-11-18 14:54:31 +0000

    create postem

--- app/controllers/postems_controller.rb
+++ app/controllers/postems_controller.rb
@@ -4,20 +4,23 @@ class PostemsController < ApplicationController
   def create
   	@postem = Postem.new(postem_params.delete_if { |_k, v| v.blank? })
   	@record = BestGuessHash.where(Hash: postem_params[:Hash]).first.best_guess
+    @search_query = SearchQuery.where(id: params[:search_query]).first
   	@postem.QuarterNumberEvent = (@record.QuarterNumber * 3) + @record.RecordTypeID
   	@postem.RecordInfo = "#{@record.Surname}|#{@record.GivenName}|#{@record.AgeAtDeath}#{@record.AssociateName}|#{@record.District}|#{@record.Volume}|#{@record.Page}"
   	@postem.SourceInfo = request.remote_ip
   	if @postem.save
   		flash[:notice] = "Added Postem successfully"
-  		redirect_to :back
   	else
   		flash[:notice] = "Unsuccessful. Please Retry"
   		redirect_to :back
   	end
+    redirect_to friendly_bmd_record_details_path(@search_query.id,@record.RecordNumber, @record.friendly_url)
   end
+
   private
 
   def postem_params
     params.require(:postem).permit!
   end
+
 end
