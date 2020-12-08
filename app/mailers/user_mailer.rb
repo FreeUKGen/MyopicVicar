44eab4d3019280f1c9ff79dc31e04db784c39005
kirkdawson <kirk.dawson.bc@gmail.com>
2020-12-08 15:18:53 -0700

    Update user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -195,7 +195,7 @@ class UserMailer < ActionMailer::Base
     coordinator = UseridDetail.userid(userid).first
     @appname = appname
     @message = message
-    manager = UseridDetail.role('Captkirk').first
+    manager = UseridDetail.userid('Captkirk').first
     mail(to: "#{coordinator.person_forename} <#{coordinator.email_address}>", cc: "#{manager.person_forename} <#{manager.email_address}>", subject: "Record removal failure report for #{file} in #{county}") if coordinator.present?
   end
 
