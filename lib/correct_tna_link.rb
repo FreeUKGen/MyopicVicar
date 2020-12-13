47d1c5e43b10c93fd0b8ea06741f8dc0e9470e05
kirkdawson <kirk.dawson.bc@gmail.com>
2020-12-13 13:57:39 -0700

    Update correct_tna_link.rb

--- lib/correct_tna_link.rb
+++ lib/correct_tna_link.rb
@@ -17,5 +17,19 @@ class CorrectTnaLink
     end
 
     p "Processed #{number} districts and corrected #{corrected}"
+    number = 0
+    corrected = 0
+    Freecen2Piece.where(year: '1841').order_by(name: 1).each do |piece|
+      number += 1
+      break if number >= limit
+
+      tna_parts = piece.tnaid.split('.')
+      next if tna_parts.length == 1
+
+      piece.update_attributes(tnaid: tna_parts[0])
+
+      corrected += 1
+    end
+    p "Processed #{number} pieces and corrected #{corrected}"
   end
 end
