78dfd96aebcbd3b151357132577ddb18d9d320c6
kirkdawson <kirk.dawson.bc@gmail.com>
2021-01-21 18:47:43 -0700

    Add :

--- lib/edit_freecen2_name.rb
+++ lib/edit_freecen2_name.rb
@@ -2,10 +2,11 @@ class EditFreecen2Name
 
   def self.process(type, chapman_code, limit, fix)
     #The purpose of this clean up utility is to eliminate search records that are unconnected with an entry. Or an entry without a batch
-    bad_text = /[-_,'"]/
-    bad_text_less = /[-_,"]/
+    bad_text = /[-_,'":]/
+    bad_text_less = /[-_,":]/
     bad_s = /'/
     limit = limit.to_i
+    type = type.titleize
     chapman_code = chapman_code.upcase
     file_for_warning_messages = "log/edit_freecen2_name_#{chapman_code}_#{type}.txt"
     FileUtils.mkdir_p(File.dirname(file_for_warning_messages))
@@ -16,28 +17,28 @@ class EditFreecen2Name
     records = 0
     fixed = 0
     time_start = Time.now
-    case type.titleize
+    case type
     when 'District'
       if chapman_code == 'ALL'
-        documents = Freecen2District.all
+        documents = Freecen2District
       else
-        documents = Freecen2District.where(chapman_code: chapman_code).all
+        documents = Freecen2District.where(chapman_code: chapman_code)
       end
     when 'Piece'
       if chapman_code == 'ALL'
-        documents = Freecen2Piece.all
+        documents = Freecen2Piece
       else
-        documents = Freecen2Piece.where(chapman_code: chapman_code).all
+        documents = Freecen2Piece.where(chapman_code: chapman_code)
       end
     when 'Civil Parish'
       if chapman_code == 'ALL'
-        documents = Freecen2CivilParish.all
+        documents = Freecen2CivilParish
       else
-        documents = Freecen2CivilParish.where(chapman_code: chapman_code).all
+        documents = Freecen2CivilParish.where(chapman_code: chapman_code)
       end
     end
 
-    documents.each do |document|
+    documents.no_timeout.each do |document|
       records += 1
       fix_name = document.name =~ bad_text ? true : false
       next unless fix_name
@@ -48,6 +49,11 @@ class EditFreecen2Name
       new_name = document.name.gsub(bad_text_less, ' ').gsub(bad_s, '').gsub(/ +/, ' ')
       message_file.puts "#{document.chapman_code},#{document.year},\"#{document.name}\",\"#{new_name}\""
       document.update_attributes(name: new_name) if fixit
+      next unless type == 'Civil Parish'
+
+      piece = document.freecen2_piece
+      civil_parish_names = piece.add_update_civil_parish_list
+      piece.update(civil_parish_names: civil_parish_names) unless civil_parish_names == piece.civil_parish_names
     end
     time_diff = Time.now - time_start
     average_record = time_diff * 1000 / records
