0c9176156e71dbab915d29214aeba39a006f8192
Anne Vandervord <72746727+AnneV-Learn@users.noreply.github.com>
2021-02-18 17:31:48 +0000

    FreeCen #1127 - updates as per KD comments R1

--- app/controllers/freecen1_vld_files_controller.rb
+++ app/controllers/freecen1_vld_files_controller.rb
@@ -27,6 +27,16 @@ class Freecen1VldFilesController < ApplicationController
       @freecen1_vld_file = Freecen1VldFile.find(params[:id])
       @chapman_code = session[:chapman_code]
     end
+    unless Freecen1VldFile.valid_freecen1_vld_file?(params[:id])
+      message = 'The file was not correctly linked. Have your coordinator contact the web master'
+      redirect_back(fallback_location: new_manage_resource_path, notice: message) && return
+    end
+    load_people
+    if params[:type].present?
+      @type = params[:type]
+    else
+      @type = 'all'
+    end
     redirect_to manage_resources_path && return
   end
 
@@ -41,28 +51,20 @@ class Freecen1VldFilesController < ApplicationController
   end
 
   def update
-    # update the transcriber name
     @freecen1_vld_file = Freecen1VldFile.find(params[:id])
     unless Freecen1VldFile.valid_freecen1_vld_file?(params[:id])
       message = 'The file was not correctly linked. Have your coordinator contact the web master'
       redirect_back(fallback_location: new_manage_resource_path, notice: message) && return
     end
-    case params[:commit]
-    when 'Submit'
-      if @freecen1_vld_file.errors.any?
-        flash[:notice] = "The update was unsuccessful: #{message}"
-        return
-      end
-      @freecen1_vld_file.update_attributes(freecen1_vld_file_params)
-      if @freecen1_vld_file.errors.any?
-        flash[:notice] = "The update was unsuccessful: #{message}"
-        render action: 'edit'
-        return
-      else
-        session[:type] = 'edit'
-        flash[:notice] = 'The update was successful'
-        redirect_to(action: 'show') && return
-      end
+    @freecen1_vld_file.update_attributes(freecen1_vld_file_params)
+    if @freecen1_vld_file.errors.any?
+      load_people
+      flash[:notice] = "The update was unsuccessful: #{message}"
+      render action: 'edit'
+      return
+    else
+      flash[:notice] = 'The update was successful'
+      redirect_to(action: 'show') && return
     end
   end
 
@@ -73,6 +75,14 @@ class Freecen1VldFilesController < ApplicationController
       @chapman_code = session[:chapman_code]
     end
     redirect_to manage_resources_path && return
+  end
+
+  def load_people
+    @people = []
+    userids = UseridDetail.all.order_by(userid_lower_case: 1)
+    userids.each do |ids|
+      @people << ids.userid
+    end
   end
 
   private
--- app/helpers/freecen1_vld_files_helper.rb
+++ app/helpers/freecen1_vld_files_helper.rb
@@ -1,6 +1,6 @@
 module Freecen1VldFilesHelper
 
   def edit_freecen1_vld_file
-    link_to 'Edit Transcriber', edit_freecen1_vld_file_path(@freecen1_vld_file), method: :get, class: 'btn   btn--small', title: 'Allows you to enter/edit the name of the person who transcribed the file.'
+    link_to 'Edit Transcriber', edit_freecen1_vld_file_path(@freecen1_vld_file), type: 'transcriber', method: :get, class: 'btn   btn--small', title: 'Allows you to enter/edit the name of the person who transcribed the file.'
   end
 end
