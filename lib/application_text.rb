3ec97c74fb1bfe0e1c7ec96bbf9c9f480d4fb59a
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2021-03-17 23:23:28 +0000

    help text

--- lib/application_text.rb
+++ lib/application_text.rb
@@ -1,4 +1,5 @@
 module ApplicationText
 	NEW_TAB_LINK_TEXT = "Link opens in new tab."
 	SAME_PAGE_ENTRIES_TEXT = "Click to view other entries on this page"
+	VIEW_MARRIAGE_TEXT = "View Marriage"
 end
