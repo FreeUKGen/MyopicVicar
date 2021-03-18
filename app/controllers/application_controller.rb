90562b193422dc366552070c7f2f7cdd95313901
kirkdawson <kirk.dawson.bc@gmail.com>
2021-03-17 18:19:08 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,6 +55,11 @@ class ApplicationController < ActionController::Base
         session[:site_stats] = @site_stat
       when 'freecen'
         site_stat = Freecen2SiteStatistic.find_by(interval_end: last_midnight)
+        if site_stat.blank?
+          last_midnight = Time.new(time.year, time.month, time.day) - 1.day
+          site_stat = Freecen2SiteStatistic.find_by(interval_end: last_midnight)
+        end
+
         session[:site_stats] = {}
         session[:site_stats][:searches] = site_stat.searches
         session[:site_stats][:records] = site_stat.records[:total][:total][:search_records]
