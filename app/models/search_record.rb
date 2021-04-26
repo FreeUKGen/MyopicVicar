1ccc5dfcf3c9cbce65f2c2f16fffacfef3933130
kirkdawson <kirk.dawson.bc@gmail.com>
2021-04-26 13:47:56 -0600

    Revert "remove sleep and indexes"
    This reverts commit 1445ce73095ed0ce0ddda70da4f90eb14fb04334.

--- app/models/search_record.rb
+++ app/models/search_record.rb
@@ -135,7 +135,7 @@ class SearchRecord
   elsif MyopicVicar::Application.config.template_set == 'freecen'
     INDEXES = NEW_INDEXES
   elsif MyopicVicar::Application.config.template_set == 'freereg'
-    INDEXES = []
+    INDEXES = MERGED_INDEXES
   end
 
   INDEXES.each_pair do |name, fields|
