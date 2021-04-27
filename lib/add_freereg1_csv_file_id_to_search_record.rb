9032eab3143789dcb05697ec8f28cb4b69976d9e
kirkdawson <kirk.dawson.bc@gmail.com>
2021-04-27 09:28:50 -0600

    add freereg1_csv_file_id to search record

---
+++ lib/add_freereg1_csv_file_id_to_search_record.rb
@@ -0,0 +1,23 @@
+class AddFreereg1CsvFileIdToSearchRecord
+  def self.process(limit)
+    limit = limit.to_i
+    file_for_warning_messages = "log/add_freereg1_csv_file_id_to_search_record.txt"
+    FileUtils.mkdir_p(File.dirname(file_for_warning_messages))
+    message_file = File.new(file_for_warning_messages, 'w')
+    process_files = 0
+    record_numbers = 0
+    Freereg1CsvFile.each do |file|
+      process_files += 1
+      next if process_files > limit
+
+      file_id = file.id
+      file.freereg1_csv_entries.each do |entry|
+        record_numbers += 1
+        entry.search_record.update_attributes(freereg1_csv_file_id: file_id)
+      end
+      p "Finished #{file.file_name}"
+      message_file.puts "#{file.file_name}"
+    end
+    p "Finished #{process_files} files with #{record_numbers} records"
+  end
+end
