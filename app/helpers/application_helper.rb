d999bf2663933262686497939a036b2b90990b03
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2021-05-16 15:32:36 +0100

    update advert personalization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -740,6 +740,17 @@ module ApplicationHelper
       data-ad-client = "#{data_ad_client}"
       data-ad-slot = "#{data_ad_slot_header}">
       </ins>
+      <script>
+        (adsbygoogle=window.adsbygoogle||[]).pauseAdRequests=1;
+        window.update_personalized_header_adverts = function (preference) {
+              if(preference == 'accept') {
+                (adsbygoogle = window.adsbygoogle || []).requestNonPersonalizedAds=0;
+              } else if(preference == 'deny') {
+                (adsbygoogle = window.adsbygoogle || []).requestNonPersonalizedAds=1;
+              }
+            };
+        (adsbygoogle=window.adsbygoogle||[]).pauseAdRequests=0;
+      </script>
       <script type="text/javascript">
         (adsbygoogle = window.adsbygoogle || []).push({});
       </script>
