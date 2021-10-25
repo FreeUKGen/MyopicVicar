45c78a0731382b8e72e9539e4fb8607811d3bd06
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2021-10-25 15:11:41 +0100

    TEST

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -10,14 +10,14 @@ class DistrictsController < ApplicationController
 
 	def unique_district_names
 		@record_type = params[:record_type] || "birth"
-		record_type_id = RecordType::FREEBMD_OPTIONS[:@record_type.capitalize]
+		record_type_id = RecordType::FREEBMD_OPTIONS[@record_type.upcase]
 		@name_type = params[:name_type] || 1
 		@district_number = params[:id]
 		@district = District.where(DistrictNumber: @district_number)
 		if @name_type == 0
-			@unique_name = DistrictUniqueName.where(district_number: @district_number, record_type: record_type_id).unique_surnames
+			@unique_name = DistrictUniqueName.where(district_number: @district_number, record_type: record_type_id).first.unique_surnames
 		else
-			@unique_name = DistrictUniqueName.where(district_number: @district_number, record_type: record_type_id).unique_forenames
+			@unique_name = DistrictUniqueName.where(district_number: @district_number, record_type: record_type_id).first.unique_forenames
 		end
 	end
 
