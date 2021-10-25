62f3ac3e475b13887bc0223bdd3f4df6e2812b76
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2021-10-25 16:02:16 +0100

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -653,7 +653,7 @@ module ApplicationHelper
       github: 'https://github.com/FreeUKGen/FreeBMD2',
       pinterest: 'https://www.pinterest.co.uk/FreeUKGenealogy/',
       instagram: 'https://www.instagram.com/freeukgenealogy/',
-      bmd1_coverage: districts_path
+      bmd1_coverage: '/districts'
     }
   end
 
