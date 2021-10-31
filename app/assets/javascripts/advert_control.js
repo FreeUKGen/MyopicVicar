c9b279f38b9a5fda17b1529625e26d8097289d8d
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2021-10-31 20:21:38 +0000

    add advert preference script

--- app/assets/javascripts/advert_control.js
+++ app/assets/javascripts/advert_control.js
@@ -2,6 +2,42 @@ $(document).ready(function() {
   var app_name = window.location.hostname;
   // advert control for freereg application
   if (app_name.includes('freereg')) {
+        if ( $( ".reg_unit" ).length ) {
+    //Default set Non personalized Adverts
+      if ((getCookie('userAdPersonalization') === null) || (getCookie('userAdPersonalization') == 'unknown')) {
+        setCookie('userAdPersonalization', 'unknown', 365 );
+        if ( $( ".reg_header" ).length ) {
+          //update_personalized_page_adverts('deny');
+          update_personalized_header_adverts('deny');
+        };
+        if ( $( ".reg_side_advert" ).length ) {
+          //update_personalized_page_adverts('deny');
+          update_personalized_header_adverts('deny');
+        };
+       };
+    // Personalized Advert
+      if (getCookie('userAdPersonalization') == 1) {
+         //update_personalized_google_adverts('accept');
+        if ( $( ".reg_header" ).length ) {
+          //update_personalized_page_adverts('accept');
+          update_personalized_header_adverts('accept');
+        };
+        if ( $( ".reg_side_advert" ).length ) {
+          update_personalized_adverts('accept');
+        };
+       };
+    //Non Personalized Advert
+      if (getCookie('userAdPersonalization') == 0) {
+         //update_personalized_google_adverts('deny');
+        if ( $( ".reg_header" ).length ) {
+          //update_personalized_page_adverts('deny');
+          update_personalized_header_adverts('deny');
+        };
+        if ( $( ".reg_side_advert" ).length ) {
+          update_personalized_adverts('deny');
+        };
+      };
+    };
   }
 
   //------------------------------------------------------------//
@@ -22,6 +58,9 @@ $(document).ready(function() {
         if ( $( ".cen_unit_fullwidth" ).length ) {
           update_personalized_fullwidth_adverts('deny');
         };
+        if ( $( ".cen_side_advert" ).length ) {
+          update_personalized_adverts('deny');
+        };
        };
     // Personalized Advert
       if (getCookie('userAdPersonalization') == 1) {
@@ -36,6 +75,9 @@ $(document).ready(function() {
         if ( $( ".cen_unit_fullwidth" ).length ) {
           update_personalized_fullwidth_adverts('accept');
         };
+        if ( $( ".cen_side_advert" ).length ) {
+          update_personalized_adverts('deny');
+        };
        };
     //Non Personalized Advert
       if (getCookie('userAdPersonalization') == 0) {
@@ -49,6 +91,9 @@ $(document).ready(function() {
         };
         if ( $( ".cen_unit_fullwidth" ).length ) {
           update_personalized_fullwidth_adverts('deny');
+        };
+        if ( $( ".cen_side_advert" ).length ) {
+          update_personalized_adverts('deny');
         };
        };
     };
