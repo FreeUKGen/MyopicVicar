3dcf6d283f37ce38cac9206851fbafe46b787145
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2021-11-20 21:38:38 +0000

    test

--- app/controllers/saved_search_controller.rb
+++ app/controllers/saved_search_controller.rb
@@ -6,13 +6,13 @@ class SavedSearchController < ApplicationController
 
 	def save_search
     @search_id = params[:id]
-    search_results = SearchQuery.find(search_id).search_result.records.keys
+    search_results = SearchQuery.find(@search_id).search_result.records.keys
     @results_hash = BestGuess.results_hash(search_results)
 		get_saved_search
     if @saved_search.present?
     	flash[:notice] = "Search is already saved"
     else
-    	saved_search = @user.saved_searches.new(search_id: search_id, results: results_hash)
+    	saved_search = @user.saved_searches.new(search_id: search_id, results: @results_hash)
     	if saved_search.save
       	flash[:notice] = "Search Saved Successfully"
     	else
@@ -24,7 +24,7 @@ class SavedSearchController < ApplicationController
 
   def get_saved_search
   	get_user_info_from_userid
-  	@saved_search = @user.saved_searches.where(search_id: search_id, results: results_hash)
+  	@saved_search = @user.saved_searches.where(search_id: @search_id, results: @results_hash)
   end
 
 end
