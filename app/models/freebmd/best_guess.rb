414212957d8db466fb93414752f10e6b2e109d2d
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2022-02-21 21:55:07 +0530

    revert present?

--- app/models/freebmd/best_guess.rb
+++ app/models/freebmd/best_guess.rb
@@ -30,8 +30,8 @@ class BestGuess < FreebmdDbBase
     # then the record types
     particles << RecordType.display_name(self.RecordTypeID)
     # then county name
-    county_code = self.CountyCombos.County if self.CountyCombos.exists?
-    particles << ChapmanCode.name_from_code(county_code) if county_code.exists?
+    county_code = self.CountyCombos.County if self.CountyCombos.present?
+    particles << ChapmanCode.name_from_code(county_code) if county_code.present?
     # then location
     particles << self.District if self.District
     # then volume
