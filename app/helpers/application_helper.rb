94b40302d4ac4bf52fd07274c5eefa503e05ecfb
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2022-06-06 15:34:26 +0100

    initial my heritage implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1041,4 +1041,74 @@ module ApplicationHelper
                 end
                 banner.html_safe
               end
+
+  def my_heritage_720_90_first
+    banner = <<-HTML
+    <ins class='dcmads' style='display:inline-block;width:728px;height:90px'
+      data-dcm-placement='N217801.4498381FREEREG/B27882117.337217241'
+      data-dcm-rendering-mode='iframe'
+      data-dcm-https-only
+      data-dcm-gdpr-applies='gdpr=${GDPR}'
+      data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}'
+      data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}'
+      data-dcm-ltd='false'
+      data-dcm-resettable-device-id=''
+      data-dcm-app-id=''>
+      <script src='https://www.googletagservices.com/dcm/dcmads.js'></script>
+    </ins>
+    HTML
+    banner.html_safe
+  end
+
+  def my_heritage_720_90_second
+    banner = <<-HTML
+    <ins class='dcmads' style='display:inline-block;width:728px;height:90px'
+      data-dcm-placement='N217801.4498381FREEREG/B27882117.337217574'
+      data-dcm-rendering-mode='iframe'
+      data-dcm-https-only
+      data-dcm-gdpr-applies='gdpr=${GDPR}'
+      data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}'
+      data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}'
+      data-dcm-ltd='false'
+      data-dcm-resettable-device-id=''
+      data-dcm-app-id=''>
+      <script src='https://www.googletagservices.com/dcm/dcmads.js'></script>
+    </ins>
+    HTML
+    banner.html_safe
+  end
+  def my_heritage_300_250_first
+    banner = <<-HTML
+    <ins class='dcmads' style='display:inline-block;width:300px;height:250px'
+      data-dcm-placement='N217801.4498381FREEREG/B27882117.337370260'
+      data-dcm-rendering-mode='iframe'
+      data-dcm-https-only
+      data-dcm-gdpr-applies='gdpr=${GDPR}'
+      data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}'
+      data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}'
+      data-dcm-ltd='false'
+      data-dcm-resettable-device-id=''
+      data-dcm-app-id=''>
+      <script src='https://www.googletagservices.com/dcm/dcmads.js'></script>
+    </ins>
+    HTML
+    banner.html_safe
+  end
+  def my_heritage_300_250_second
+    banner = <<-HTML
+    <ins class='dcmads' style='display:inline-block;width:300px;height:250px'
+      data-dcm-placement='N217801.4498381FREEREG/B27882117.337649001'
+      data-dcm-rendering-mode='iframe'
+      data-dcm-https-only
+      data-dcm-gdpr-applies='gdpr=${GDPR}'
+      data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}'
+      data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}'
+      data-dcm-ltd='false'
+      data-dcm-resettable-device-id=''
+      data-dcm-app-id=''>
+      <script src='https://www.googletagservices.com/dcm/dcmads.js'></script>
+    </ins>
+    HTML
+    banner.html_safe
+  end
               end
