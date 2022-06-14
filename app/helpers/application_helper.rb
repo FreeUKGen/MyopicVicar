8e21e64c0ec9e81b83f8d0d4a045d17453cc63f6
Vino <vinodhini.subbu@freeukgenealogy.org.uk>
2022-06-14 09:34:39 +0100

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1112,6 +1112,42 @@ module ApplicationHelper
     banner.html_safe
   end
 
+  def my_heritage_720_90_cen
+    banner = <<-HTML
+    <ins class='dcmads' style='display:inline-block;width:728px;height:90px'
+    data-dcm-placement='N217801.4486016FREECEN2/B27887957.337083338'
+    data-dcm-rendering-mode='iframe'
+    data-dcm-https-only
+    data-dcm-gdpr-applies='gdpr=${GDPR}'
+    data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}'
+    data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}'
+    data-dcm-ltd='false'
+    data-dcm-resettable-device-id=''
+    data-dcm-app-id=''>
+  <script src='https://www.googletagservices.com/dcm/dcmads.js'></script>
+</ins>
+    HTML
+    banner.html_safe
+  end
+
+  def my_heritage_720_90_cen_detail
+    banner = <<-HTML
+    <ins class='dcmads' style='display:inline-block;width:728px;height:90px'
+    data-dcm-placement='N217801.4486016FREECEN2/B27887957.337358632'
+    data-dcm-rendering-mode='iframe'
+    data-dcm-https-only
+    data-dcm-gdpr-applies='gdpr=${GDPR}'
+    data-dcm-gdpr-consent='gdpr_consent=${GDPR_CONSENT_755}'
+    data-dcm-addtl-consent='addtl_consent=${ADDTL_CONSENT}'
+    data-dcm-ltd='false'
+    data-dcm-resettable-device-id=''
+    data-dcm-app-id=''>
+  <script src='https://www.googletagservices.com/dcm/dcmads.js'></script>
+</ins>
+    HTML
+    banner.html_safe
+  end
+
   def my_heritage_300_250_cen
     banner = <<-HTML
     <ins class='dcmads' style='display:inline-block;width:300px;height:250px'
