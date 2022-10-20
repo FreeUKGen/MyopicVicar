98a400ad77c906fb6a34f49a777bc94047e30354
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2022-10-20 11:38:20 +0100

    fix districts

--- app/models/freebmd/best_guess.rb
+++ app/models/freebmd/best_guess.rb
@@ -51,8 +51,8 @@ class BestGuess < FreebmdDbBase
   def record_hash
     surname = self.Surname.upcase
     given_name = self.GivenName.upcase
-    #district_name = self.district.DistrictName.upcase
-    district_name = self.District.upcase
+    district_name = self.district.try(:DistrictName).try(:upcase)
+    district_name = self.District.upcase unless district_name.present?
     volume = self.Volume.upcase
     page = self.Page.upcase
     year = QuarterDetails.quarter_year(self.QuarterNumber)
--- app/models/freebmd/bmd_file.rb
+++ app/models/freebmd/bmd_file.rb
@@ -1,6 +1,6 @@
 class BmdFile < FreebmdDbBase
 	self.pluralize_table_names = false
-  self.table_name = 'files'
+  self.table_name = 'Files'
    self.primary_key = 'FileNumber'
   belongs_to :submitter, foreign_key: 'SubmitterNumber', class_name: '::Submitter'#, primary_key: 'FileNumber'
   has_many :accessions, class_name: '::Accession', foreign_key: 'FileNumber', primary_key: 'FileNumber'
