2b1bb4988fa903850e45500ac48cef240702b59b
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2023-01-10 11:19:19 +0000

    update table name

--- db/migrate/20230110110548_add_new_volumes_district.rb
+++ db/migrate/20230110110548_add_new_volumes_district.rb
@@ -1,6 +1,6 @@
 class AddNewVolumesDistrict < ActiveRecord::Migration[5.1]
   def change
-    rename_column :District, :Volume1974toEnd, :Volume1974to1993_4
-    add_column :District, :Volume1993_4toEnd, :string
+    rename_column :Districts, :Volume1974toEnd, :Volume1974to1993_4
+    add_column :Districts, :Volume1993_4toEnd, :string
   end
 end
