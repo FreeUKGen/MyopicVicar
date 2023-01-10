a1df8fe02e2f589e9acc551f752ca61f2548e394
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2023-01-10 11:42:33 +0000

    test

--- db/migrate/20230110110548_add_new_volumes_district.rb
+++ db/migrate/20230110110548_add_new_volumes_district.rb
@@ -1,4 +1,5 @@
 class AddNewVolumesDistrict < ActiveRecord::Migration[5.1]
+  connects_to database: { writing: FREEBMD_DB }
   def change
     rename_column :Districts, :Volume1974toEnd, :Volume1974to1993_4
     add_column :Districts, :Volume1993_4toEnd, :string
