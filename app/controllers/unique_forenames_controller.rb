4bf9f54405c4c7860539b1206875508eff6a692b
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2023-03-21 12:56:34 +0000

    test

--- app/controllers/unique_forenames_controller.rb
+++ app/controllers/unique_forenames_controller.rb
@@ -2,14 +2,12 @@ class UniqueForenamesController < ApplicationController
   skip_before_action :require_login
 
   def index
-    require 'unique_forenames'
     @term_in_context = "^"+params[:term]
     @forenames = UniqueForename.where({"Name": {"$regex": @term_in_context, "$options": "i"}}).limit(10)
     render :json => get_search_names_hash(@forenames)
   end
 
   def show
-    require 'unique_forenames'
     @forenames = UniqueForename.where({"Name": {"$regex": @term_in_context}})
   end
 
