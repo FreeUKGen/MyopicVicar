b4278e2a80ca196c4d1356acb2bfd15a7d236932
jayto581 <joseph@freeukgenealogy.org.uk>
2023-05-31 19:17:02 +0000

    refactor: remove redundant code - remove code reference to file already part of the default assets pipeline - remove reference to ie.scss, since no longer used - remove code reference to  non-existing file - remove code duplicate stylesheet references - standardize stylesheet_link_tag command format - remove code reference to javascript files already in assets pipeline application.js - standardize javascript_include_tag command format

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,34 +4,14 @@
 
 Rails.application.configure do
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-  config.assets.paths << Rails.root.join('app', 'assets', 'images')
-  config.assets.paths << Rails.root.join('app', 'assets', 'images', 'png')
-  config.assets.paths << Rails.root.join('app', 'assets', 'images', 'svg', 'min')
-  config.assets.paths << Rails.root.join('app', 'assets', 'images', 'svg', 'src')
 
   config.assets.precompile += %w[styles/scss/lap_and_up.scss]
-  config.assets.precompile += %w[styles/scss/ie.scss]
   config.assets.precompile += %w[styles/scss/palm.scss]
   config.assets.precompile += %w[styles/scss/ladda.scss]
-  config.assets.precompile += %w[favicon.ico]
+  config.assets.precompile += %w[styles/css/donate_icon.css]
   config.assets.precompile += %w[styles/css/icons.data.svg.css]
-  config.assets.precompile += %w[styles/css/donate_icon.css]
-  config.assets.precompile += %w[styles/css/icons.data.png.css]
-  config.assets.precompile += %w[styles/css/icons.fallback.css]
   config.assets.precompile += %w[styles/css/freereg_content.css]
-  config.assets.precompile += %w[styles/css/ladda.min.css]
-  config.assets.precompile += %w[jquery.min.js]
-  config.assets.precompile += %w[jquery.chained.remote.js]
-  config.assets.precompile += %w[jquery.cookiesDirective.js]
-  config.assets.precompile += %w[html5shiv.js]
-  config.assets.precompile += %w[spin.min.js]
-  config.assets.precompile += %w[ladda.min.js]
-  config.assets.precompile += %w[ads.js]
-  config.assets.precompile += %w[adsbygoogle.js]
+  
   config.assets.precompile += %w[prebid_ads.js]
-  config.assets.precompile += %w[freecen_coverage_graph.js]
-  config.assets.precompile += %w[styles/css/donate_icon.css]
-  config.assets.precompile += %w[cookie_control.js]
-  config.assets.precompile += %w[advert_control.js]
   config.assets.precompile += %w( javascripts/freecen_gdpr.js )
 end
