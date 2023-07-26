6517b077ce1c42c057c7b15a00053d31efac632a
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2023-07-26 10:00:55 +0100

    fix contacts listing

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -150,6 +150,8 @@ class ContactsController < ApplicationController
     order = 'contact_time ASC'
     @primary_contacts = Contact.primary_results(session[:archived_contacts], order, @user)
     @secondary_contacts = Contact.secondary_results(session[:archived_contacts], order, @user)
+    @primary_contact_present = @primary_contacts.present?
+    @secondary_contact_present = @secondary_contacts.present?
     #@contacts = Contact.results(session[:archived_contacts], order, @user)
     @archived = session[:archived_contacts]
     render :index
@@ -160,6 +162,8 @@ class ContactsController < ApplicationController
     order = 'contact_time DESC'
     @primary_contacts = Contact.primary_results(session[:archived_contacts], order, @user)
     @secondary_contacts = Contact.secondary_results(session[:archived_contacts], order, @user)
+    @primary_contact_present = @primary_contacts.present?
+    @secondary_contact_present = @secondary_contacts.present?
     #@contacts = Contact.results(session[:archived_contacts], order, @user)
     @archived = session[:archived_contacts]
     render :index
@@ -170,6 +174,8 @@ class ContactsController < ApplicationController
     order = 'name ASC'
     @primary_contacts = Contact.primary_results(session[:archived_contacts], order, @user)
     @secondary_contacts = Contact.secondary_results(session[:archived_contacts], order, @user)
+    @primary_contact_present = @primary_contacts.present?
+    @secondary_contact_present = @secondary_contacts.present?
     #@contacts = Contact.results(session[:archived_contacts], order, @user)
     @archived = session[:archived_contacts]
     render :index
@@ -180,6 +186,8 @@ class ContactsController < ApplicationController
     order = 'contact_type ASC'
     @primary_contacts = Contact.primary_results(session[:archived_contacts], order, @user)
     @secondary_contacts = Contact.secondary_results(session[:archived_contacts], order, @user)
+    @primary_contact_present = @primary_contacts.present?
+    @secondary_contact_present = @secondary_contacts.present?
     #@contacts = Contact.results(session[:archived_contacts], order, @user)
     @archived = session[:archived_contacts]
     render :index
