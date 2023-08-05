212fee15a271219c8c875e334d8983e62791ba70
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2023-08-05 21:25:06 +0100

    fix cta crossover with cookie consent

---
+++ app/assets_freereg/javascripts/freereg_fuse_tag.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //donate cta
+  if ((getCookie('donate_cta_flag_new') != null)) {
+	  $('head').append('<script async src="https://cdn.fuseplatform.net/publift/tags/2/3271/fuse.js"></script>')
+}
+/*CTA code changes ends */
+});
