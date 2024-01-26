0349c9494f6b0fbf094fd451a6b6057c0d50892e
Anne Vandervord <72746727+AnneV-Learn@users.noreply.github.com>
2024-01-26 12:35:22 +0000

    Add Middlesex and Surrey to London (City) searches
    Add Middlesex and Surrey to London (City) searches

--- app/models/freecen2_place.rb
+++ app/models/freecen2_place.rb
@@ -183,6 +183,9 @@ class Freecen2Place
         county_codes = ChapmanCode::CODES['Wales'].values
         # Add Herefordshire to Wales as lots of border places - story 1617
         county_codes << ChapmanCode.values_at('Herefordshire')
+      when 'London (City)'
+        # add Middlesex and Surrey to London - story 1627
+        county_codes = %w[LND MDX SRY]
       else
         county_codes << ChapmanCode.values_at(county)
       end
