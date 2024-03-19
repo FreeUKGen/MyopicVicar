3416beb496edc4d7bff8fc67e78c8d0dd3ab058e
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-03-19 22:51:18 +0000

    update date period from the form

--- app/controllers/physical_files_controller.rb
+++ app/controllers/physical_files_controller.rb
@@ -269,12 +269,13 @@ class PhysicalFilesController < ApplicationController
   end
 
   def upload_report
-    @start_date = params[:period_from].present? ? params[:period_from].to_datetime : '01/01/2020'.to_datetime
-    @end_date = params[:period_to].present? ? params[:period_to].to_datetime : Date.today.to_datetime
+    @start_date = params['upload_report']['period_from'].present? ? params['upload_report']['period_from'].to_datetime : '01/01/2020'.to_datetime
+    @end_date = params['upload_report']['period_to'].present? ? params['upload_report']['period_to'].to_datetime : Date.today.to_datetime
     uploaded_files = PhysicalFile.where(c_at: @start_date..@end_date)
     uploaders_userid = uploaded_files.pluck(:userid).uniq.sort
     uploaders = UseridDetail.where(userid: {'$in' => uploaders_userid })
     uploders_role = uploaders.pluck(:person_role)
     @uploaders_count = uploders_role.group_by(&:itself).transform_values(&:count)
   end
+
 end
