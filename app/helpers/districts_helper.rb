f446e2f1c5be6ead592cc295aaead7dd69393447
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-03-19 14:40:02 +0000

    dynamic back button

---
+++ app/helpers/districts_helper.rb
@@ -0,0 +1,13 @@
+module DistrictsHelper
+	def show_page_back_link(link_to_hash = {})
+		link_hash = link_to_hash{}
+		case
+		when link_hash[:search_query].present?
+			link_to 'Back to search results', search_query_path(link_hash[:search_query]), class: " btn   btn--small"
+		when link_hash[:search_query].present? && link_hash[:search_record].present?
+			link_to 'Back to entry', friendly_bmd_record_details_path(link_hash[:search_query].id,link_hash[:search_record].id, link_hash[:search_record].friendly_url, search_entry: link_hash[:search_record].RecordNumber), class: " btn   btn--small"
+		when link_hash[:district].present?
+	    	link_to 'Back', "/districts/districts_list?params=#{link_hash[:district].DistrictName.first}", class: " btn   btn--small"
+	    end
+	end
+end
