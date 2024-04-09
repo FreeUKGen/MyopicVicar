02a432abbec8dc7aba78b815ea6cb96962020978
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-04-09 12:39:26 +0100

    whitelist svg

--- config/initializers/refinery/wymeditor.rb
+++ config/initializers/refinery/wymeditor.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 Refinery::Wymeditor.configure do |config|
   # Add extra tags to the wymeditor whitelist e.g. = {'tag' => {'attributes' => {'1' => 'href'}}} or just {'tag' => {}}
-  # config.whitelist_tags = {}
+  config.whitelist_tags = {'tag' => {'svg'}}
 end
