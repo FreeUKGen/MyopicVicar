31410ab6f4a4a6b9d230be53e63cf8b2454c2bff
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-05-01 00:23:42 +0100

    pick piece chapman_code

--- app/controllers/freecen2_pieces_controller.rb
+++ app/controllers/freecen2_pieces_controller.rb
@@ -201,16 +201,17 @@ class Freecen2PiecesController < ApplicationController
     redirect_back(fallback_location: new_manage_resource_path, notice: 'No Piece Number') && return if params[:number].blank?
     @number = params[:number]
     year, piece, _census_fields = Freecen2Piece.extract_year_and_piece(params[:number], '')
+    piece_chapman_code = Freecen2Piece.where(piece_number: piece).chapman_code
     session[:type] = 'locate_other_pieces'
     find_associated_pieces, piece_number = Freecen2Piece.check_piece_parts(piece)
-    @freecen2_pieces = get_pieces(piece, year)
+    @freecen2_pieces = get_pieces(piece, year,piece_chapman_code)
     @freecen2_pieces = @freecen2_pieces.reject(&:blank?)
     @associated_pieces = get_pieces(piece_number, year) if find_associated_pieces
   end
 
-  def get_pieces(piece_number, year)
+  def get_pieces(piece_number, year,piece_chapman_code)
     pieces = []
-    Freecen2Piece.year(year).order_by(number: 1).each do |test_piece|
+    Freecen2Piece.chapman_code(piece_chapman_code).year(year).order_by(number: 1).each do |test_piece|
       next unless test_piece.number.include?(piece_number)
 
       pieces << test_piece
