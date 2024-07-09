0bb666de2c3798fa17fbc545ade4ccf5333496eb
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-07-09 13:18:44 +0100

    results_per_page to int

--- app/controllers/search_queries_controller.rb
+++ app/controllers/search_queries_controller.rb
@@ -390,7 +390,7 @@ class SearchQueriesController < ApplicationController
     page_number = params[:page]
     results_per_page = params[:results_per_page]
     sorted_results = @search_query.sorted_and_paged_searched_records
-    results_per_page > 50 ? results_per_page = 50 : results_per_page = results_per_page
+    results_per_page.to_i > 50 ? results_per_page = 50 : results_per_page = results_per_page
     paginated_array = @search_query.paginate_results(sorted_results,page_number,results_per_page)
     send_data search_results_csv(paginated_array), filename: "search_results-#{Date.today}.csv"
   end
