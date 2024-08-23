490da4428e0c6bbdd922123f6e627277893a47a8
richardofsussex <richardlight399@gmail.com>
2024-08-23 17:10:24 +0100

    Added code to display coverage information.

--- config/routes.rb
+++ config/routes.rb
@@ -643,6 +643,8 @@ MyopicVicar::Application.routes.draw do
 
   resources :unique_surnames
 
+  resources :coverage, :as => "coverage"
+
 
   # This line mounts Refinery's routes at the root of your application.
   # This means, any requests to the root URL of your application will go to Refinery::PagesController#home.
