38b729209c1da5a3f154478b1e1b4ecfdc0aa7cb
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-10-08 14:47:35 +0100

    sort actions

--- app/controllers/manage_resources_controller.rb
+++ app/controllers/manage_resources_controller.rb
@@ -120,7 +120,7 @@ class ManageResourcesController < ApplicationController
     @userid = @user.userid
     @first_name = @user.person_forename if @user.present?
     @manager = manager?(@user)
-    @roles = UseridRole::OPTIONS.fetch(@current_role)
+    @roles = UseridRole::OPTIONS.fetch(@current_role).sort
     session[:userid] = @userid
     session[:user_id] = @user_id
     session[:first_name] = @first_name
