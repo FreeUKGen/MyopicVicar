0f0c0f3a16acc57381ae82bfb773cec1cc8f8d4d
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2024-12-01 22:30:54 +0000

    update cron for upload stats

--- app/models/physical_file.rb
+++ app/models/physical_file.rb
@@ -237,6 +237,7 @@ class PhysicalFile
 
   def upload_report_data(start_date, end_date)
     uploaded_files = PhysicalFile.where(c_at: start_date..end_date)
+    uploaded_files = FreecenCsvFile.where(c_at: start_date..end_date) if appname.downcase == 'freecen'
     uploaders_userid = uploaded_files.pluck(:userid).uniq.sort
     uploaders = UseridDetail.where(userid: {'$in' => uploaders_userid })
     uploders_role = uploaders.pluck(:person_role)
@@ -248,6 +249,7 @@ class PhysicalFile
 
   def upload_report_mail(start_date, end_date)
     uploaded_files = PhysicalFile.where(c_at: start_date..end_date)
+    uploaded_files = FreecenCsvFile.where(c_at: start_date..end_date) if appname.downcase == 'freecen'
     uploaders_userid = uploaded_files.pluck(:userid).uniq.sort
     uploaders = UseridDetail.where(userid: {'$in' => uploaders_userid })
     exclude_roles = ['system_administrator', 'executive_director', 'technical']
