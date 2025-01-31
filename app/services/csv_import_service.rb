c234481c24c645bfcd9d55c4f64df8b41ad79fac
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2025-01-31 13:52:58 +0000

    update church code

---
+++ app/services/csv_import_service.rb
@@ -0,0 +1,30 @@
+class CsvImportService
+	def initialize(file_path)
+		@file_path = file_path
+	end
+
+	def csv_import
+		CSV.foreach(@file_path, headers: true) do |row|
+      csv_data = row.to_hash
+      update_church(csv_data)
+    end
+	end
+
+	def find_place(data)
+		Place.find_by(place_name: data['place_name'], chapman_code: data['chapman_code'])
+	end
+
+	def find_church(place,data)
+		place.churches.find_by(church_name: data['church_name'])
+	end
+
+	def update_church(data)
+    place = find_place(data)
+    return unless place
+
+    church = find_church(place, data)
+    return unless church
+
+    church.update(church_code: data['church_code'])
+  end
+end
---
+++ lib/tasks/update_church_code.rake
@@ -0,0 +1,13 @@
+namespace :update_church do
+  desc "Update church code"
+  task church_code: :environment do
+    file_path = ENV['FILE_PATH']
+    if file_path.nil?
+      puts "provide the file path"
+      exit
+    end
+
+    CsvImportService.new(file_path).import
+    puts "Church Codes updated"
+  end
+end
