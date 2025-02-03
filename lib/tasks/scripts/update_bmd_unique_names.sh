ee9651e09d95708a50d920668e311ecaac8bf338
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2025-02-03 13:25:02 +0000

    automate uniq names

--- lib/tasks/scripts/update_bmd_unique_names.sh
+++ lib/tasks/scripts/update_bmd_unique_names.sh
@@ -1,1 +1,16 @@
-sudo -u webserv bundle exec  rake RAILS_ENV=production reports:extract_collection_unique_names --trace
+#!/usr/bin/env bash
+
+# This script populates unique names
+set -uo pipefail
+IFS=$'\n\t'
+
+trace() {
+  NOW=$( date +'%Y-%m-%d %H:%M:%S' )
+  echo "[extract_unique_names] ${NOW} $@" >&2
+}
+
+ROOT=/home/apache/hosts/freebmd2/production
+cd $ROOT
+umask 0002
+sudo -u webserv bundle exec rake RAILS_ENV=production reports:extract_collection_unique_names --trace
+exit
