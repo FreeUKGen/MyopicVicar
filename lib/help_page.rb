5ab81715a05ddea0c14d28c008e943abc00f345a
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2025-03-13 09:35:56 +0000

    help page section

--- lib/help_page.rb
+++ lib/help_page.rb
@@ -1,9 +1,12 @@
 module HelpPage
   
   SECTIONS = {
-    'Search form help' => '/cms/help#search',
-    "Search Results help" => '/cms/help#search_result',
-    'Entry onformation help' => '/messages/help#entry_info'
+    'Search form help' => 'search',
+    "Search Results help" => 'results',
+    'Entry onformation help' => 'detail'
+  }
+  SUB_SECTIONS = {
+    'Search form help' => ['wildcard','soundex','gtk']
   }
   
 end
