9fe3d925aba6c21228180f7df5d7ed57e0fc7070
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2025-06-25 12:57:19 +0100

    fix FreeCEN upload stats

--- app/models/physical_file.rb
+++ app/models/physical_file.rb
@@ -262,11 +262,15 @@ class PhysicalFile
     users_count = UseridDetail.where(c_at: start_date..end_date)
     total_records_added = SiteStatistic.where(year: start_date.year, month: start_date.month).sum(:n_records_added)
     if appname.downcase == 'freecen'
-      stats_records = Freecen2SiteStatistic.where(year: start_date.year, month: start_date.month)
-      total_csv_records_added = stats_records.sum{|f| f.records.dig(:total,:total,:added_csv_entries)}
-      total_vld_records_added = stats_records.sum{|f| f.records.dig(:total, :total,:added_vld_entries)}
-      total_vld_records_deleted = Freecen1VldFileAudit.where(created_at: start_date..end_date).sum(:num_entries)
-      total_records_added = total_csv_records_added + total_vld_records_added - total_vld_records_deleted
+      stats_records = Freecen2SiteStatistic.where(year: start_date.year, month: start_date.month).last
+      prev_stats_records = Freecen2SiteStatistic.where(year: start_date.year, month: (start_date.month - 1)).last
+      total_csv_records_added = stats_records.records.dig(:total,:total,:added_csv_entries)
+      total_vld_records_added = stats_records.records.dig(:total, :total,:added_vld_entries)
+      prev_total_csv_records_added = prev_stats_records.records.dig(:total,:total,:added_csv_entries)
+      prev_total_vld_records_added = prev_stats_records.records.dig(:total, :total,:added_vld_entries)
+      total_records = total_csv_records_added + total_vld_records_added
+      prev_total_records = prev_total_csv_records_added + prev_total_vld_records_added
+      total_records_added = total_records - prev_total_records
     end
     [uploaders_count, email_confirmed, users_count, total_records_added]
   end
