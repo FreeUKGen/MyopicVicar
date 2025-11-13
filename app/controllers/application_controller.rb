4a3a3c1a2ec6ffc16362dcedc38d28ef52a2bf7d
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2025-11-13 07:32:48 +0000

    remove refinery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,8 +83,8 @@ class ApplicationController < ActionController::Base
 
   def load_message_flag
     # This tells system there is a message to display
-      session[:message] = 'no' if session[:message].blank?
-      session[:message] = 'load' if Refinery::Page.present? && Refinery::Page.where(slug: 'message').exists?
+    #session[:message] = 'no' if session[:message].blank?
+    #session[:message] = 'load' if Refinery::Page.present? && Refinery::Page.where(slug: 'message').exists?
   end
 
   private
