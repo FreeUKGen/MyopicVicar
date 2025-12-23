20cee675fb57c6378bd02206ec31923d7845ff6e
Anne Vandervord <72746727+AnneV-Learn@users.noreply.github.com>
2025-12-23 10:27:04 +0000

    Update freecen2_county_contents_controller.rb
    def params correction

--- app/controllers/freecen2_county_contents_controller.rb
+++ app/controllers/freecen2_county_contents_controller.rb
@@ -274,7 +274,7 @@ class Freecen2CountyContentsController < ApplicationController
 
   private
 
-  def freecen2_content_params
+  def freecen2_county_content_params
     params.require(:freecen2_county_content).permit!
   end
 end
