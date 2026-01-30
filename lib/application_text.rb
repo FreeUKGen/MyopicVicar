67eec734c4993fe87866dfe258e7a55c93a5cc61
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2026-01-30 01:14:54 +0000

    add advert and refactor

--- lib/application_text.rb
+++ lib/application_text.rb
@@ -6,4 +6,5 @@ module ApplicationText
 	LATE_ENTRY_TEXT_WITHOUT_REFERENCE = "This entry appears to be a Late Entry however the entry referred to cannot be found, either because it is not identical to this entry or because it has not yet been transcribed."
 	REFERENCE_ENTRY_TEXT = "This is a late entry, that is an entry that has been registered in a quarter later than the event"
 	NO_INFORMATION = "We are afraid, we do not have this information yet."
+	SCAN_HELP_TEXT = "If the scan does not contain the entry you may be able to <%= link_to \"find the scan on our original site\", \"https://images.freebmd.org.uk/cgi/choose.pl\"%>."
 end
