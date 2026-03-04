c912d16ed37fe5f140f6ae3f497c5019dcda914f
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2026-03-04 13:54:20 +0000

    Changes applied to new model creation

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -3,3 +3,8 @@ Mongoid.configure do |config|
 #  config.database = Mongo::Connection.new.db("mvui-#{Rails.env}")
   #config.database.authenticate(username,password)
 end
+
+# Apply string trimming to all Mongoid models by default
+Rails.application.config.to_prepare do
+  Mongoid::Document.include(StripStringFields) unless Mongoid::Document.included_modules.include?(StripStringFields)
+end
