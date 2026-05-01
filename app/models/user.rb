a99b502929e78881ef7aab6fb687f9eac537e19c
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2026-05-01 13:55:21 +0100

    include devise trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User
   include Mongoid::Document
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
   devise  :encryptable, :encryptor => :freereg
   #alias devise_will_save_change_to_email? will_save_change_to_email?
   attr_writer :login
