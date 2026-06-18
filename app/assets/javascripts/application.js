031ea3b4b0261329807d01055e3d9befc3814529
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2026-06-18 13:52:19 +0100

    require jquery3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require_tree .
