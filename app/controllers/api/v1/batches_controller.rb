26420d1aea3bff90cd4b1d1dec98d2c224dcc99e
Vino-S <vinodhini.subbu@freeukgenealogy.org.uk>
2026-08-06 10:24:46 +0100

    list batches update

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BatchesController < Api::V1::BaseController
   def index
     batches = Freereg1CsvFile.where(userid: @current_user.userid).order_by(uploaded_date: :desc)
-    render json: batches.as_json(only: [:id, :file_name, :userid, :uploaded_date, :record_count, :error, :locked])
+    render json: batches.as_json(only: [:id, :file_name, :userid, :uploaded_date, :records, :error, :processed, :processed_date, :locked_by_transcriber, :locked_by_coordinator])
   end
 end
