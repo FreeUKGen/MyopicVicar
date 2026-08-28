539b4275ccdaf8fe8c44264f2cb086d39614e342
your name <you@gmail.com>
2026-08-28 16:18:22 +0000

    chore: move old spec and test folder to tmp folder

--- spec/models/freecen_dwelling_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe FreecenDwelling do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/models/freecen_piece_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe FreecenPiece do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
